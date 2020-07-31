3e054aa56487f100785436e8041c7e9b924d6afb
Nick Touran <ntouran@terrapower.com>
2020-07-31 16:05:36 -0700

    Add i,j labels to hex grid demo

--- doc/gallery-src/framework/run_grids1_hex.py
+++ doc/gallery-src/framework/run_grids1_hex.py
@@ -7,7 +7,11 @@ equal to 1.0 cm.
 
 Learn more about :py:mod:`grids <armi.reactor.grids>`.
 """
+import math
+
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
 
 from armi.reactor import grids
 
@@ -19,10 +23,16 @@ hexes = grids.HexGrid.fromPitch(1.0)
 
 fig = plt.figure()
 xyz = []
-
+polys = []
 ax = fig.add_subplot(1, 1, 1)
 for hex_i in hexes.generateSortedHexLocationList(127):
-    xyz.append(hex_i.getGlobalCoordinates())
-x, y, z = zip(*xyz)
-ax.plot(x, y, "o")
+    x, y, z = hex_i.getGlobalCoordinates()
+    ax.text(x,y,f"{hex_i.i},{hex_i.j}", ha="center",va="center")
+    polys.append(mpatches.RegularPolygon((x,y), numVertices=6, radius=1/math.sqrt(3),
+            orientation=math.pi/2))
+patches = PatchCollection(polys, fc="white", ec='k')
+ax.add_collection(patches)
+ax.set_title("(i, j) indices for a hex grid")
+ax.set_xlim([-7,7])
+ax.set_ylim([-7,7])
 plt.show()
