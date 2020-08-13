d68bc9cb1e95f4014faa25bae00453f1324d27e2
Nick Touran <ntouran@terrapower.com>
2020-08-12 18:49:11 -0700

    Add small MCNP m-card gallery example

---
+++ doc/gallery-src/analysis/run_blockMcnpMaterialCard.py
@@ -0,0 +1,29 @@
+"""
+Write MCNP Material Cards
+=========================
+
+Here we load a test reactor and write each component of one fuel block out as
+MCNP material cards.
+
+Normally, code-specific utility code would belong in a code-specific ARMI
+plugin. But in this case, the need for MCNP materials cards is so pervasive
+that it made it into the framework.
+"""
+
+from armi.reactor.tests import test_reactors
+from armi.reactor.flags import Flags
+from armi.utils.densityTools import formatMaterialCard
+from armi.nucDirectory import nuclideBases as nb
+import armi
+
+armi.configure()
+
+_o, r = test_reactors.loadTestReactor()
+
+bFuel = r.core.getBlocks(Flags.FUEL)[0]
+
+for ci, component in enumerate(bFuel, start=1):
+    ndens = component.getNumberDensities()
+    # convert nucName (str) keys to nuclideBase keys
+    ndensByBase = {nb.byName[nucName]: dens for nucName, dens in ndens.items()}
+    print("".join(formatMaterialCard(ndensByBase, matNum=ci)))
