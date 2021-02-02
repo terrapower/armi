9c73668fc690095c25027270b3c8f72525fabdac
Nick Touran <ntouran@terrapower.com>
2021-02-02 09:51:23 -0800

    Move isDepletable off of composite
    This physics-related method is better suited for a framework physics
    plugin.

--- armi/physics/neutronics/isotopicDepletion/isotopicDepletionInterface.py
+++ armi/physics/neutronics/isotopicDepletion/isotopicDepletionInterface.py
@@ -23,6 +23,44 @@ from armi.nuclearDataIO import xsLibraries
 from armi.physics.neutronics.isotopicDepletion.crossSectionTable import (
     CrossSectionTable,
 )
+from armi.reactor import composites
+from armi.reactor.flags import Flags
+
+
+def isDepletable(obj: composites.ArmiObject):
+    """
+    Return True if obj or any child is flagged as DEPLETABLE.
+
+    The DEPLETABLE flag is automatically set to true if any composition contains
+    nuclides that are in the active nuclides list, unless flags are specifically
+    set and DEPLETABLE is left out.
+
+    This is often interpreted by depletion plugins as indicating which parts of the
+    problem to apply depletion to. Analysts may want to turn on and off depletion
+    in certain problems.
+
+    For example, sometimes they want the control rods to deplete
+    to figure out how often to replace them. But in conceptual design, they may want to just
+    leave them as they are as an approximation.
+
+    .. warning:: The ``DEPLETABLE`` flag is automatically added to compositions that have
+        active nuclides. If you explicitly define any flags at all, you must also
+        manually include ``DEPLETABLE`` or else the objects will silently not deplete.
+
+    Notes
+    -----
+    The auto-flagging of ``DEPLETABLE`` happens in the construction of blueprints
+    rather than in a plugin hook because the reactor is not available at the time
+    the plugin hook runs.
+
+    See Also
+    --------
+    armi.reactor.blueprints.componentBlueprint._insertDepletableNuclideKeys
+    """
+
+    return obj.hasFlags(Flags.DEPLETABLE) or obj.containsAtLeastOneChildWithFlags(
+        Flags.DEPLETABLE
+    )
 
 
 class AbstractIsotopicDepleter:
