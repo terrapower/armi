55f9bbfb928eefdbf3a993a26b1d6d817bb57cea
Michael Jarrett <mjarrett@terrapower.com>
2021-04-05 16:55:50 -0500

    Simplify the printDensities function for LumpedFissionProducts.

--- armi/physics/neutronics/fissionProductModel/lumpedFissionProduct.py
+++ armi/physics/neutronics/fissionProductModel/lumpedFissionProduct.py
@@ -259,9 +259,7 @@ class LumpedFissionProduct(object):
 
     def printDensities(self, lfpDens):
         """Print densities of nuclides given a LFP density. """
-        nucs = list(self.keys())
-        nucs.sort()
-        for n in nucs:
+        for n in sorted(self.keys()):
             runLog.info("{0:6s} {1:.7E}".format(n.name, lfpDens * self[n]))
 
 
