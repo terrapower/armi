4015933d5ae1fcb0bff8be8a554923497fed55ae
John Stilley <thejollysin@gmail.com>
2021-10-01 19:48:40 -0700

    Basic code coverage for isotopicDepletionInterface

--- armi/physics/neutronics/tests/test_crossSectionTable.py
+++ armi/physics/neutronics/tests/test_crossSectionTable.py
@@ -1,10 +1,15 @@
 """Tests for cross section table for depletion"""
 import unittest
 
-from armi.physics.neutronics.isotopicDepletion import crossSectionTable as cst
-from armi.reactor.flags import Flags
 from armi.nuclearDataIO.cccc import isotxs
+from armi.physics.neutronics.isotopicDepletion import (
+    crossSectionTable,
+    isotopicDepletionInterface as idi,
+)
+from armi.reactor.flags import Flags
 from armi.reactor.tests.test_blocks import loadTestBlock
+from armi.reactor.tests.test_reactors import loadTestReactor
+from armi.settings import Settings
 from armi.tests import ISOAA_PATH
 
 
@@ -14,7 +19,7 @@ class TestCrossSectionTable(unittest.TestCase):
         obj.p.mgFlux = range(33)
         core = obj.getAncestorWithFlags(Flags.CORE)
         core.lib = isotxs.readBinary(ISOAA_PATH)
-        table = cst.makeReactionRateTable(obj)
+        table = crossSectionTable.makeReactionRateTable(obj)
 
         self.assertEqual(len(obj.getNuclides()), len(table))
         self.assertEqual(obj.getName(), "B0001-000")
@@ -26,6 +31,16 @@ class TestCrossSectionTable(unittest.TestCase):
         self.assertEqual(len(xSecTable), 11)
         self.assertIn("xsecs", xSecTable[0])
         self.assertIn("mcnpId", xSecTable[-1])
+
+    def test_isotopicDepletionInterface(self):
+        o, r = loadTestReactor()
+        cs = Settings()
+
+        aid = idi.AbstractIsotopicDepleter(r, cs)
+        self.assertIsNone(aid.efpdToBurn)
+        self.assertEqual(len(aid._depleteByName), 0)
+
+        self.assertEqual(len(aid.getToDeplete()), 0)
 
 
 if __name__ == "__main__":
