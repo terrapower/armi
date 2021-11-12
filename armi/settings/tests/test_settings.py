47d3cc77b3f24540fcf6ae56be6c0e34b397604e
John Stilley <1831479+john-science@users.noreply.github.com>
2021-11-12 09:13:29 -0800

    Switching form a Setting deepcopy to a copy (#476)

--- armi/settings/tests/test_settings.py
+++ armi/settings/tests/test_settings.py
@@ -284,6 +284,25 @@ assemblyRotationAlgorithm: buReducingAssemblyRotatoin
         cs3 = cs2.modified(newSettings={"numberofGenericParams": 7})
         cs4 = cs3.modified(newSettings={"somethingElse": 123})
 
+    def test_copySetting(self):
+        """Ensure that when we copy a Setting() object, the result is sound.
+        NOTE: In particuar, self.schema and self._customSchema on a Setting object are
+              removed by Setting.__getstate__, and that has been a problem in the past.
+        """
+        # get a baseline: show how the Setting object looks to start
+        s1 = setting.Setting("testCopy", 765)
+        self.assertEquals(s1.name, "testCopy")
+        self.assertEquals(s1._value, 765)
+        self.assertTrue(hasattr(s1, "schema"))
+        self.assertTrue(hasattr(s1, "_customSchema"))
+
+        # show that copy(Setting) is working correctly
+        s2 = copy.copy(s1)
+        self.assertEquals(s2._value, 765)
+        self.assertEquals(s2.name, "testCopy")
+        self.assertTrue(hasattr(s2, "schema"))
+        self.assertTrue(hasattr(s2, "_customSchema"))
+
 
 class TestSettingsConversion(unittest.TestCase):
     """Make sure we can convert from old XML type settings to new Yaml settings."""
