terrapower/armi

commit 1ee1f79c22a8fbaef361dc47b3a9e8f327682e1b

Remove temporary fix now that tips up maps work again

armi/reactor/blueprints/gridBlueprint.py
@@ -597,15 +597,6 @@ def saveToStream(stream, bluep, full=False, tryMap=False):
 
             if aMap is not None:
                 gridDesign.gridContents = None
-                if not isinstance(aMap, asciimaps.AsciiMapHexFullTipsUp):
-                    # this specific implementation of asciimap is broken, so we only
-                    # attempt to use the other implementations. If hex, full, tips-up is
-                    # being used we just preserve whatever was in the blueprints
-                    # already. this is bad, but we can do away with it when #437 is
-                    # fixed.
-                    mapString = StringIO()
-                    aMap.writeAscii(mapString)
-                    gridDesign.latticeMap = mapString.getvalue()
                 gridDesign.latticeMap = scalarstring.LiteralScalarString(
                     gridDesign.latticeMap
                 )

armi/utils/asciimaps.py
@@ -83,12 +83,14 @@ class AsciiMap:
         self._placeholder = PLACEHOLDER
         """Placeholder for blank data. Also holds the size of ascii window for each value"""
 
-        self._asciiMaxCol = 0  # max number of text columns in text representation
-        self._asciiMaxLine = 0  # max number of text lines in text representation
-        self._ijMax = (
-            0  # max num of i+j indices (max(i) + max(j)), needed mostly for hex
-        )
-        """Various dimensions of the ascii mesh and data"""
+        self._asciiMaxCol = 0
+        """max number of text columns in text representation"""
+
+        self._asciiMaxLine = 0
+        """max number of text lines in text representation"""
+
+        self._ijMax = 0
+        """max num of i+j indices (max(i) + max(j)), needed mostly for hex"""
 
         self._asciiLinesOffCorner = 0
         """Number of ascii lines chopped of corners"""

commit f7ea5855b17ff90f793de7b9fcc5e7e4a477e1b6

Fix to writing the lattice map as a string when saving the core map from the Grids GUI. This fixes a bug introduced in #440.

armi/reactor/blueprints/gridBlueprint.py
@@ -580,27 +580,32 @@ def saveToStream(stream, bluep, full=False, tryMap=False):
         if gridDesign.readFromLatticeMap or tryMap:
             geomType = geometry.GeomType.fromStr(gridDesign.geom)
             symmetry = geometry.SymmetryType.fromStr(gridDesign.symmetry)
+
+            aMap = asciimaps.asciiMapFromGeomAndDomain(
+                gridDesign.geom, symmetry.domain
+            )()
+            aMap.asciiLabelByIndices = {
+                (key[0], key[1]): val for key, val in gridDesign.gridContents.items()
+            }
             try:
-                aMap = asciimaps.asciiMapFromGeomAndDomain(
-                    gridDesign.geom, symmetry.domain
-                )()
-                aMap.asciiLabelByIndices = {
-                    (key[0], key[1]): val
-                    for key, val in gridDesign.gridContents.items()
-                }
                 aMap.gridContentsToAscii()
             except Exception as e:
                 runLog.warning(
-                    "Cannot write geometry with asciimap. Defaulting to dict. Issue: {}".format(
-                        e
-                    )
+                    f"The `lattice map` for the current assembly arrangement cannot be written. "
+                    f"Defaulting to using the `grid contents` dictionary instead. Exception: {e}"
                 )
                 aMap = None
 
             if aMap is not None:
+                # If there is an ascii map available then use it to fill out
+                # the contents of the lattice map section of the grid design.
+                # This also clears out the grid contents so there is not duplicate
+                # data.
                 gridDesign.gridContents = None
+                mapString = StringIO()
+                aMap.writeAscii(mapString)
                 gridDesign.latticeMap = scalarstring.LiteralScalarString(
-                    gridDesign.latticeMap
+                    mapString.getvalue()
                 )
             else:
                 gridDesign.latticeMap = None