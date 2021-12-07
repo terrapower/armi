72dd0724278a31fe7b843a1e56db1c7f8ac04f60
Nick Touran <ntouran@terrapower.com>
2021-12-07 06:21:11 -0800

    Remove deprecated np.float and np.int (#502)

--- armi/bookkeeping/db/database3.py
+++ armi/bookkeeping/db/database3.py
@@ -2364,7 +2364,6 @@ NONE_MAP.update(
         intType: numpy.iinfo(intType).min + 2
         for intType in (
             int,
-            numpy.int,
             numpy.int8,
             numpy.int16,
             numpy.int32,
@@ -2384,9 +2383,7 @@ NONE_MAP.update(
         )
     }
 )
-NONE_MAP.update(
-    {floatType: floatType("nan") for floatType in (numpy.float, numpy.float64)}
-)
+NONE_MAP.update({floatType: floatType("nan") for floatType in (float, numpy.float64)})
 
 
 def packSpecialData(
