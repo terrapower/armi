e8609c96402ed8397638b0ac6f41c1476cef845b
Michael Jarrett <mjarrett@terrapower.com>
2022-02-02 16:22:21 -0800

    Create a separate *.log file for the worker node *.stdouts. (#528)

--- armi/runLog.py
+++ armi/runLog.py
@@ -248,7 +248,9 @@ class _RunLog:
     def startLog(self, name):
         """Initialize the streams when parallel processing"""
         # open the main logger
-        self.logger = logging.getLogger(STDOUT_LOGGER_NAME + SEP + str(self._mpiRank))
+        self.logger = logging.getLogger(
+            STDOUT_LOGGER_NAME + SEP + name + SEP + str(self._mpiRank)
+        )
 
         # if there was a pre-existing _verbosity, use it now
         if self._verbosity != logging.INFO:
@@ -310,41 +312,58 @@ def concatenateLogs(logDir=None):
 
     info("Concatenating {0} log files".format(len(stdoutFiles)))
 
-    for stdoutName in stdoutFiles:
-        # NOTE: If the log file name format changes, this will need to change.
-        rank = int(stdoutName.split(".")[-2])
+    # default worker log name if none is found
+    caseTitle = "armi-workers"
+    for stdoutPath in stdoutFiles:
+        stdoutFile = os.path.normpath(stdoutPath).split(os.sep)[-1]
+        prefix = STDOUT_LOGGER_NAME + "."
+        if stdoutFile[0 : len(prefix)] == prefix:
+            caseTitle = stdoutFile.split(".")[-3]
+            break
 
-        # first, print the log messages for a child process
-        with open(stdoutName, "r") as logFile:
-            data = logFile.read()
-            if data:
-                # only write if there's something to write
-                rankId = "\n{0} RANK {1:03d} STDOUT {2}\n".format(
-                    "-" * 10, rank, "-" * 60
-                )
-                print(rankId, file=sys.stdout)
-                print(data, file=sys.stdout)
-        try:
-            os.remove(stdoutName)
-        except OSError:
-            warning("Could not delete {0}".format(stdoutName))
+    combinedLogName = os.path.join(logDir, "{}-mpi.log".format(caseTitle))
+    with open(combinedLogName, "w") as workerLog:
+        workerLog.write(
+            "\n{0} CONCATENATED WORKER LOG FILES {1}\n".format("-" * 10, "-" * 10)
+        )
 
-        # then print the stderr messages for that child process
-        stderrName = stdoutName[:-3] + "err"
-        if os.path.exists(stderrName):
-            with open(stderrName) as logFile:
+        for stdoutName in stdoutFiles:
+            # NOTE: If the log file name format changes, this will need to change.
+            rank = int(stdoutName.split(".")[-2])
+            with open(stdoutName, "r") as logFile:
                 data = logFile.read()
+                # only write if there's something to write
                 if data:
-                    # only write if there's something to write.
-                    rankId = "\n{0} RANK {1:03d} STDERR {2}\n".format(
+                    rankId = "\n{0} RANK {1:03d} STDOUT {2}\n".format(
                         "-" * 10, rank, "-" * 60
                     )
-                    print(rankId, file=sys.stderr)
-                    print(data, file=sys.stderr)
+                    if rank == 0:
+                        print(rankId, file=sys.stdout)
+                        print(data, file=sys.stdout)
+                    else:
+                        workerLog.write(rankId)
+                        workerLog.write(data)
             try:
-                os.remove(stderrName)
+                os.remove(stdoutName)
             except OSError:
-                warning("Could not delete {0}".format(stderrName))
+                warning("Could not delete {0}".format(stdoutName))
+
+            # then print the stderr messages for that child process
+            stderrName = stdoutName[:-3] + "err"
+            if os.path.exists(stderrName):
+                with open(stderrName) as logFile:
+                    data = logFile.read()
+                    if data:
+                        # only write if there's something to write.
+                        rankId = "\n{0} RANK {1:03d} STDERR {2}\n".format(
+                            "-" * 10, rank, "-" * 60
+                        )
+                        print(rankId, file=sys.stderr)
+                        print(data, file=sys.stderr)
+                try:
+                    os.remove(stderrName)
+                except OSError:
+                    warning("Could not delete {0}".format(stderrName))
 
 
 # Here are all the module-level functions that should be used for most outputs.
@@ -450,9 +469,10 @@ class RunLogger(logging.Logger):
 
     def __init__(self, *args, **kwargs):
         # optionally, the user can pass in the MPI_RANK by putting it in the logger name after a separator string
+        # args[0].split(SEP): 0 = "ARMI", 1 = caseTitle, 2 = MPI_RANK
         if SEP in args[0]:
             mpiRank = int(args[0].split(SEP)[-1].strip())
-            args = (args[0].split(SEP)[0],)
+            args = (".".join(args[0].split(SEP)[0:2]),)
         else:
             mpiRank = context.MPI_RANK
 
