f8057c5a9c07732e76e5740c5a24216b77b81917
John Stilley <john_science@protonmail.com>
2022-02-17 19:57:18 -0800

    Fixing a typo

--- doc/conf.py
+++ doc/conf.py
@@ -404,7 +404,7 @@ sphinx_gallery_conf = {
     "default_thumb_file": os.path.join(RES, "images", "TerraPowerLogo.png"),
 }
 
-suppress_warnings: ["autoapi.python_import_resolution"]
+suppress_warnings = ["autoapi.python_import_resolution"]
 
 # filter out this warning which shows up in sphinx-gallery builds.
 # this is suggested in the sphinx-gallery example but doesn't actually work?
