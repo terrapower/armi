a3f23ed01538aaaeb1dc58b5bf158def918901ef
Jake Hader <jhader@terrapower.com>
2022-03-01 08:48:37 -0800

    Add settings table generator in dochelpers. (#585)
    * - Add a documentation helper to auto-generate a settings table for
      software docuementation.
    
    - Add the MOVEABLE flag to the control and plenum blocks in the
      `refSmallReactorBase`.
    
    - Update the description of the crStartingHeight assembly parameter.
    
    * Add unit test for `generatePluginSettingsTable`.

--- armi/reactor/assemblyParameters.py
+++ armi/reactor/assemblyParameters.py
@@ -262,7 +262,9 @@ def getAssemblyParameterDefinitions():
             units="cm",
             description="The initial starting position of the bottom of the control "
             "material when starting control operations as measured from the 0 point in the "
-            "reactor model",
+            "reactor model. Note that the starting height is taken to be a maximum withdrawn "
+            "location for the control rod for determining the position when the control rod "
+            "is fully withdrawn.",
         )
 
     with pDefs.createBuilder() as pb:
