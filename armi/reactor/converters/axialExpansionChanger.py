a14213e33d38e790ff2519f29e5c835d52fe3ed3
aalberti <c-aalberti@terrapower.com>
2022-03-15 20:47:07 +0100

    add docstring for file

--- armi/reactor/converters/axialExpansionChanger.py
+++ armi/reactor/converters/axialExpansionChanger.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""enable component-wise axial expansion for assemblies and/or a reactor"""
 
 from statistics import mean
 from numpy import array
