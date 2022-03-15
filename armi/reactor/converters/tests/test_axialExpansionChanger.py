34ee99f5f483236485d6f9646f71d66283b1880b
aalberti <c-aalberti@terrapower.com>
2022-03-15 23:52:40 +0100

    adding and altering several tests from prototype
    Summary:
    1. Several changes from previous commit:
        - adding Base class to hold common methods and variables
        - adding Temperature class to enable code reuse between tests
        - adding class for testing axial expansion height against hand calculation
        - adjusting conservation and exception based tests to account for Base and Temperature classes.
    
    ...getting closer!

--- armi/reactor/converters/tests/test_axialExpansionChanger.py
+++ armi/reactor/converters/tests/test_axialExpansionChanger.py
@@ -14,8 +14,9 @@
 
 """Test axialExpansionChanger"""
 
+from statistics import mean
 import unittest
-from numpy import linspace, ones, array, vstack
+from numpy import linspace, ones, array, vstack, zeros
 from armi.reactor.tests.test_reactors import loadTestReactor
 from armi.tests import TEST_ROOT
 from armi.reactor.assemblies import grids
@@ -26,8 +27,8 @@ from armi.reactor.components.basicShapes import Circle, Hexagon
 from armi.reactor.converters.axialExpansionChanger import AxialExpansionChanger
 from armi.reactor.flags import Flags
 
-class TestConservation(unittest.TestCase):
-    """verify that conservation is maintained in assembly-level axial expansion"""
+class Base(unittest.TestCase):
+    """common methods and variables for unit tests"""
 
     Steel_Component_Lst = [
         Flags.DUCT,
@@ -42,40 +43,224 @@ class TestConservation(unittest.TestCase):
 
     def setUp(self):
         self._converterSettings = {}
-        self.o, self.r = loadTestReactor(TEST_ROOT)
-        self.a = self.r.core.refAssem
         self.obj = AxialExpansionChanger(self._converterSettings)
-        self.obj.setAssembly(self.a)
-
-        # initialize class variables for conservation checks
         self.massAndDens = {}
         self.steelMass = []
         self.blockHeights = {}
+    
+    def getConservationMetrics(self, a):
+        """retrieves and stores various conservation metrics
+
+        - useful for verification and unittesting
+        - Finds and stores:
+            1. mass and density of target components
+            2. mass of assembly steel
+            3. block heights
+        """
+        mass = 0.0
+        for b in a:
+            for c in b:
+                ## store mass and density of target component
+                if self.obj.expansionData.isTargetComponent(c):
+                    self._storeTargetComponentMassAndDensity(c)
+                ## store steel mass for assembly
+                if c.p.flags in self.Steel_Component_Lst:
+                    mass += c.getMass()
+
+            # store block heights
+            tmp = array([b.p.zbottom, b.p.ztop, b.p.height, b.getVolume()])
+            if b.name not in self.blockHeights:
+                self.blockHeights[b.name] = tmp
+            else:
+                self.blockHeights[b.name] = vstack((self.blockHeights[b.name], tmp))
+
+        self.steelMass.append(mass)
+    
+    def _storeTargetComponentMassAndDensity(self, c):
+        tmp = array(
+            [
+                c.getMass(),
+                c.material.getProperty("density", c.temperatureInK),
+            ]
+        )
+        if c.parent.name not in self.massAndDens:
+            self.massAndDens[c.parent.name] = tmp
+        else:
+            self.massAndDens[c.parent.name] = vstack(
+                (self.massAndDens[c.parent.name], tmp)
+            )
+
+class Temperature:
+    """create and store temperature grid/field"""
+
+    def __init__(
+        self, L, coldTemp=25.0, hotInletTemp=360.0, numTempGridPts=25, tempSteps=100, uniform=False,
+    ):
+        """
+        Parameters
+        ----------
+        L : float
+            length of self.tempGrid. Should be the height of the corresponding assembly.
+        coldTemp : float
+            component as-built temperature
+        hotInletTemp : float
+            temperature closest to bottom of assembly. Interpreted as
+            inlet temp at nominal operations.
+        numTempGridPts : integer
+            the number of temperature measurement locations along the
+            z-axis of the assembly
+        tempSteps : integer
+            the number of temperatures to create (analogous to time steps)
+        """
+        self.tempSteps = tempSteps
+        self.tempGrid = linspace(0.0, L, num=numTempGridPts)
+        self.tempField = zeros((tempSteps, numTempGridPts))
+        self.generateTempField(coldTemp, hotInletTemp, uniform)
+
+    def generateTempField(self, coldTemp, hotInletTemp, uniform):
+        """
+        generate temperature field and grid
+
+        - all temperatures are in C
+        - temperature field : temperature readings (e.g., from T/H calculation)
+        - temperature grid : physical locations in which
+                            temperature is measured
+        """
+        ## Generate temp field
+        self.tempField[0, :] = coldTemp
+        if not uniform:
+            for i in range(1, self.tempSteps):
+                self.tempField[i, :] = (
+                    coldTemp
+                    + (i + 1) / (self.tempSteps / 3) * self.tempGrid
+                    + (hotInletTemp - coldTemp) * (i + 1) / self.tempSteps
+                )
+        else:
+            tmp = linspace(coldTemp, hotInletTemp, self.tempSteps)
+            for i in range(1,self.tempSteps):
+                self.tempField[i, :] = tmp[i]
+
+class TestAxialExpansionHeight(Base,unittest.TestCase):
+    """verify that test assembly is expanded correctly"""
+
+    def setUp(self):
+        Base.setUp(self)
+        self.a = buildTestAssemblyWithFakeMaterial(name='Fake')
+        self.obj.setAssembly(self.a)
+
+        self.temp = Temperature(
+            self.a.getTotalHeight(), numTempGridPts=11, tempSteps=10
+        )
+
+        # get the right/expected answer
+        self.generateComponentWiseExpectedHeight()
+
+        # do the axial expansion
+        self.axialMeshLocs = zeros((self.temp.tempSteps, len(self.a)))
+        for idt in range(self.temp.tempSteps):
+            self.obj.mapHotTempToBlocks(self.temp.tempGrid, self.temp.tempField[idt,:])
+            self.obj.expansionData.computeThermalExpansionFactors()
+            self.obj.axiallyExpandAssembly()
+            self.getConservationMetrics(self.a)
+            self.axialMeshLocs[idt, :] = self.a.getAxialMesh()
+
+    def test_AssemblyAxialExpansionHeight(self):
+        """test the axial expansion gives correct heights for component-based expansion"""
+        for idt in range(self.temp.tempSteps):
+            for ib, b in enumerate(self.a):
+                self.assertAlmostEqual(
+                    self.trueZtop[ib, idt],
+                    self.blockHeights[b.name][idt][1],
+                    places=7,
+                    msg="Block height is not correct.\
+                         Temp Step = {0:d}, Block ID = {1:}.".format(
+                        idt, b.name
+                    ),
+                )
+
+    def test_AxialMesh(self):
+        """test that mesh aligns with block tops for component-based expansion"""
+        for idt in range(self.temp.tempSteps):
+            for ib, b in enumerate(self.a):
+                self.assertEqual(
+                    self.axialMeshLocs[idt][ib],
+                    self.blockHeights[b.name][idt][1],
+                    msg="\
+                        Axial mesh and block top do not align and invalidate the axial mesh.\
+                        Block ID = {0:s},\n\
+                            Top = {1:.12e}\n\
+                        Mesh Loc = {2:.12e}".format(
+                        str(b.name),
+                        self.blockHeights[b.name][idt][1],
+                        self.axialMeshLocs[idt][ib],
+                    ),
+                )
+
+    def generateComponentWiseExpectedHeight(self):
+        """calculate the expected height, external of AssemblyAxialExpansion()"""
+        assem = buildTestAssemblyWithFakeMaterial(name="Fake")
+        aveBlockTemp = zeros((len(assem), self.temp.tempSteps))
+        self.trueZtop = zeros((len(assem), self.temp.tempSteps))
+        self.trueHeight = zeros((len(assem), self.temp.tempSteps))
+        self.trueZtop[-1, :] = assem[-1].p.ztop
+
+        for idt in range(self.temp.tempSteps):
+            # get average block temp
+            for ib in range(len(assem)):
+                aveBlockTemp[ib, idt] = self.getAveTemp(ib, idt, assem)
+            # get block ztops
+            for ib, b in enumerate(assem[:-1]):
+                if ib > 0:
+                    b.p.zbottom = assem[ib - 1].p.ztop
+                if idt > 0:
+                    dll = (
+                        0.02 * aveBlockTemp[ib, idt] - 0.02 * aveBlockTemp[ib, idt - 1]
+                    ) / (100.0 + 0.02 * aveBlockTemp[ib, idt - 1])
+                    thermExpansionFactor = 1.0 + dll
+                    b.p.ztop = thermExpansionFactor * b.p.height + b.p.zbottom
+                self.trueZtop[ib, idt] = b.p.ztop
+            # get block heights
+            for ib, b in enumerate(assem):
+                b.p.height = b.p.ztop - b.p.zbottom
+                self.trueHeight[ib, idt] = b.p.height
+
+    def getAveTemp(self, ib, idt, assem):
+        tmpMapping = []
+        for idz, z in enumerate(self.temp.tempGrid):
+            if assem[ib].p.zbottom <= z <= assem[ib].p.ztop:
+                tmpMapping.append(self.temp.tempField[idt][idz])
+            if z > assem[ib].p.ztop:
+                break
+
+        return mean(tmpMapping)
+
+class TestConservation(Base, unittest.TestCase):
+    """verify that conservation is maintained in assembly-level axial expansion"""
+
+    def setUp(self):
+        Base.setUp(self)
+        self.o, self.r = loadTestReactor(TEST_ROOT)
+        self.a = self.r.core.refAssem
+        self.obj.setAssembly(self.a)
+
+        # initialize class variables for conservation checks
         self.oldMass = {}
         for b in self.a:
             self.oldMass[b.name] = 0.0
 
         # do the expansion and store mass and density info
-        coldTemp = 1.0
-        hotInletTemp = 1000.0
-        temp_grid = linspace(0.0, self.a.getTotalHeight())
-        temp_field = coldTemp * ones(len(temp_grid))
-        self.tempSteps = 100
-        for idt in range(self.tempSteps):
-            self.obj.mapHotTempToBlocks(temp_grid, temp_field)
+        self.temp = Temperature(
+            self.a.getTotalHeight(), coldTemp=1.0, hotInletTemp=1000.0
+        )
+        for idt in range(self.temp.tempSteps):
+            self.obj.mapHotTempToBlocks(self.temp.tempGrid, self.temp.tempField[idt,:])
             self.obj.expansionData.computeThermalExpansionFactors()
             self.obj.axiallyExpandAssembly()
-            self.getConservationMetrics()
-            # increment temperature
-            temp_field = (
-                coldTemp
-                + (idt + 1) / (self.tempSteps / 3) * temp_grid
-                + (hotInletTemp - coldTemp) * (idt + 1) / self.tempSteps
-            )
+            self.getConservationMetrics(self.a)
 
     def test_TargetComponentMassConservation(self):
         """tests mass conservation for target components"""
-        for idt in range(self.tempSteps):
+        for idt in range(self.temp.tempSteps):
             for b in self.a[:-1]:  # skip the dummy sodium block
                 if idt != 0:
                     self.assertAlmostEqual(
@@ -97,7 +282,7 @@ class TestConservation(unittest.TestCase):
 
         Component list defined by, Steel_Component_List, in GetSteelMass()
         """
-        for idt in range(self.tempSteps - 1):
+        for idt in range(self.temp.tempSteps - 1):
             self.assertAlmostEqual(
                 self.steelMass[idt],
                 self.steelMass[idt + 1],
@@ -105,56 +290,13 @@ class TestConservation(unittest.TestCase):
                 msg="Conservation of steel mass failed on time step {0:d}".format(idt),
             )
 
-    def getConservationMetrics(self):
-        """retrieves and stores various conservation metrics
 
-        - useful for verification and unittesting
-        - Finds and stores:
-            1. mass and density of target components
-            2. mass of assembly steel
-            3. block heights
-        """
-        mass = 0.0
-        for b in self.a:
-            for c in b:
-                ## store mass and density of target component
-                if self.obj.expansionData.isTargetComponent(c):
-                    self._storeTargetComponentMassAndDensity(c)
-                ## store steel mass for assembly
-                if c.p.flags in self.Steel_Component_Lst:
-                    mass += c.getMass()
-
-            # store block heights
-            tmp = array([b.p.zbottom, b.p.ztop, b.p.height, b.getVolume()])
-            if b.name not in self.blockHeights:
-                self.blockHeights[b.name] = tmp
-            else:
-                self.blockHeights[b.name] = vstack((self.blockHeights[b.name], tmp))
-
-        self.steelMass.append(mass)
-
-    def _storeTargetComponentMassAndDensity(self, c):
-        tmp = array(
-            [
-                c.getMass(),
-                c.material.getProperty("density", c.temperatureInK),
-            ]
-        )
-        if c.parent.name not in self.massAndDens:
-            self.massAndDens[c.parent.name] = tmp
-        else:
-            self.massAndDens[c.parent.name] = vstack(
-                (self.massAndDens[c.parent.name], tmp)
-            )
-
-
-class TestExceptions(unittest.TestCase):
+class TestExceptions(Base, unittest.TestCase):
     """Verify exceptions are caught"""
 
     def setUp(self):
-        self._converterSettings = {}
-        self.obj = AxialExpansionChanger(self._converterSettings)
-        self.a = buildTestAssemblyWithFakeMaterial()
+        Base.setUp(self)
+        self.a = buildTestAssemblyWithFakeMaterial(name='FakeException')
         self.obj.setAssembly(self.a)
 
     def test_setExpansionFactors(self):
@@ -166,18 +308,18 @@ class TestExceptions(unittest.TestCase):
             self.assertEqual(the_exception.error_code, 3)
 
     def test_mapHotTempToBlockValueError(self):
-        temp_grid = [5.0, 15.0, 35.0]
-        temp_field = linspace(25.0, 310.0, 3)
+        tempGrid = [5.0, 15.0, 35.0]
+        tempField = linspace(25.0, 310.0, 3)
         with self.assertRaises(ValueError) as cm:
-            self.obj.mapHotTempToBlocks(temp_grid, temp_field)
+            self.obj.mapHotTempToBlocks(tempGrid, tempField)
             the_exception = cm.exception
             self.assertEqual(the_exception.error_code, 3)
 
     def test_mapHotTempToBlockRuntimeError(self):
-        temp_grid = [5.0, 15.0, 35.0]
-        temp_field = linspace(25.0, 310.0, 10)
+        tempGrid = [5.0, 15.0, 35.0]
+        tempField = linspace(25.0, 310.0, 10)
         with self.assertRaises(RuntimeError) as cm:
-            self.obj.mapHotTempToBlocks(temp_grid, temp_field)
+            self.obj.mapHotTempToBlocks(tempGrid, tempField)
             the_exception = cm.exception
             self.assertEqual(the_exception.error_code, 3)
 
@@ -187,17 +329,17 @@ class TestExceptions(unittest.TestCase):
         hotInletTemp = 310.0
         tempSteps = 10
         numTempGridPts = 11
-        temp_grid = linspace(0.0, self.a.getTotalHeight(), numTempGridPts)
-        temp_field = coldTemp * ones(numTempGridPts)
+        tempGrid = linspace(0.0, self.a.getTotalHeight(), numTempGridPts)
+        tempField = coldTemp * ones(numTempGridPts)
         with self.assertRaises(ArithmeticError) as cm:
             for idt in range(1, tempSteps):
-                self.obj.mapHotTempToBlocks(temp_grid, temp_field)
+                self.obj.mapHotTempToBlocks(tempGrid, tempField)
                 self.obj.expansionData.computeThermalExpansionFactors()
                 self.obj.axiallyExpandAssembly()
                 # increment temperature
-                temp_field = (
+                tempField = (
                     coldTemp
-                    + (idt + 1) / (tempSteps / 3) * temp_grid
+                    + (idt + 1) / (tempSteps / 3) * tempGrid
                     + (hotInletTemp - coldTemp) * (idt + 1) / tempSteps
                 )
 
@@ -227,31 +369,40 @@ class TestExceptions(unittest.TestCase):
             self.assertEqual(the_exception.error_code, 3)
 
 
-def buildTestAssemblyWithFakeMaterial():
-    """Create test assembly consisting of list of fake material"""
+def buildTestAssemblyWithFakeMaterial(name):
+    """Create test assembly consisting of list of fake material
+    
+    Parameters
+    ----------
+    name : string
+        determines which fake material to use
+    """
     assembly = HexAssembly("testAssemblyType")
     assembly.spatialGrid = grids.axialUnitGrid(numCells=1)
     # The ArmiObject that this grid describes.
     # https://terrapower.github.io/armi/.apidocs/armi.reactor.grids.html#armi.reactor.grids.Grid
     # this feels recursive and not super clear....?
     assembly.spatialGrid.armiObject = assembly
-    assembly.add(_buildTestBlock("shield"))
-    assembly.add(_buildTestBlock("fuel"))
-    assembly.add(_buildTestBlock("fuel"))
-    assembly.add(_buildTestBlock("plenum"))
+    assembly.add(_buildTestBlock("shield", name))
+    assembly.add(_buildTestBlock("fuel", name))
+    assembly.add(_buildTestBlock("fuel", name))
+    assembly.add(_buildTestBlock("plenum", name))
     assembly.add(_buildDummySodium())
     assembly.calculateZCoords()
     assembly.reestablishBlockOrder()
     return assembly
 
 
-def _buildTestBlock(blockType):
+def _buildTestBlock(blockType, name):
     """Return a simple pin type block filled with coolant and surrounded by duct.
 
     Parameters
     ----------
     blockType : string
         determines which type of block you're building
+
+    name : string
+        determines which fake material to use
     """
     b = HexBlock(blockType, height=10.0)
 
@@ -266,9 +417,9 @@ def _buildTestBlock(blockType):
         "mult": 1.0,
     }
     coolDims = {"Tinput": 25.0, "Thot": 25.0}
-    mainType = Circle(blockType, "FakeException", **fuelDims)
-    clad = Circle("clad", "FakeException", **cladDims)
-    duct = Hexagon("duct", "FakeException", **ductDims)
+    mainType = Circle(blockType, name, **fuelDims)
+    clad = Circle("clad", name, **cladDims)
+    duct = Hexagon("duct", name, **ductDims)
 
     coolant = DerivedShape("coolant", "Sodium", **coolDims)
     intercoolant = Hexagon("intercoolant", "Sodium", **intercoolantDims)
@@ -286,18 +437,7 @@ def _buildTestBlock(blockType):
 
 
 def _buildDummySodium():
-    """Build a dummy sodium block.
-
-    Parameters
-    ----------
-    height : float
-        sets initial height of block
-
-    Returns
-    -------
-    b:
-        single block object containing sodium
-    """
+    """Build a dummy sodium block."""
     b = HexBlock("dummy", height=10.0)
 
     sodiumDims = {"Tinput": 25.0, "Thot": 25.0, "op": 17, "ip": 0.0, "mult": 1.0}
