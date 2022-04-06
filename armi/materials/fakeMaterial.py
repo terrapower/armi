9e4f607f2be57192fcc2f116b0540f4e8404ab17
aalberti <c-aalberti@terrapower.com>
2022-04-06 11:13:43 -0700

    adding more detailed docstrings
    https://github.com/terrapower/armi/pull/606#discussion_r844010511

--- armi/materials/fakeMaterial.py
+++ armi/materials/fakeMaterial.py
@@ -3,10 +3,15 @@ from armi.materials.ht9 import HT9
 from armi.utils import units
 
 
-class Fake(HT9):
-    """
-    Fake material based on HT9, used to verify
-    assembly axial expansion
+class Fake(HT9): # pylint: disable=abstract-method
+    """Fake material used to verify armi.reactor.converters.axialExpansionChanger
+
+    Notes
+    -----
+    - specifically used armi.reactor.converters.tests.test_axialExpansionChanger.py:TestAxialExpansionHeight
+      to verify axialExpansionChanger produces expected heights from hand calculation
+    - also used to verify mass and height conservation resulting from even amounts of expansion 
+      and contraction. See armi.reactor.converters.tests.test_axialExpansionChanger.py:TestConservation
     """
 
     name = "Fake"
@@ -20,10 +25,14 @@ class Fake(HT9):
         return 0.02 * Tc
 
 
-class FakeException(HT9):
-    """
-    Fake material based on HT9, used to verify exceptions
-    in assembly axial expansion
+class FakeException(HT9): # pylint: disable=abstract-method
+    """Fake material used to verify armi.reactor.converters.tests.test_axialExpansionChanger.py:TestExceptions
+
+    Notes
+    -----
+    - the only difference between this and `class Fake(HT9)` above is that the thermal expansion factor
+      is higher to ensure that a negative block height is caught in 
+      armi.reactor.converters.tests.test_axialExpansionChanger.py:TestExceptions:test_AssemblyAxialExpansionException. 
     """
 
     name = "FakeException"
