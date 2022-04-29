a8bc37cc4828aa6d64237f14a2242dc2ab730c44
aalberti <c-aalberti@terrapower.com>
2022-04-29 07:46:56 -0700

    adding additional test for blueprint copy
    - this double checks and ensures that particular attributes are indeed set on the copied blueprints.

--- armi/reactor/converters/tests/test_uniformMesh.py
+++ armi/reactor/converters/tests/test_uniformMesh.py
@@ -62,6 +62,24 @@ class TestUniformMeshComponents(unittest.TestCase):
         self.assertEqual(refMesh[0], avgMesh[0])
         self.assertNotEqual(refMesh[4], avgMesh[4], "Not equal above the fuel.")
 
+    def test_blueprintCopy(self):
+        """ensure that necessary blueprint attributes are set
+
+        see https://github.com/terrapower/armi/pull/645#discussion_r861210649
+        """
+        convReactor = self.converter.initNewReactor(self.converter._sourceReactor)
+        converted = convReactor.blueprints
+        original = self.converter._sourceReactor.blueprints
+        toCompare = [
+            "activeNuclides",
+            "allNuclidesInProblem",
+            "elementsToExpand",
+            "inertNuclides",
+        ]  # note, items within toCompare must be list or "list-like", like an ordered set
+        for attr in toCompare:
+            for c, o in zip(getattr(converted, attr), getattr(original, attr)):
+                self.assertEqual(c, o)
+
 
 def applyNonUniformHeightDistribution(reactor):
     """Modifies some assemblies to have non-uniform axial meshes"""
