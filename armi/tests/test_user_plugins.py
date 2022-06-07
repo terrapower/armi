9e2be2a697576d18b1fcf4acc03c654b2fffc9ce
John Stilley <john_science@protonmail.com>
2022-06-07 16:53:22 -0700

    WIP

--- armi/tests/test_user_plugins.py
+++ armi/tests/test_user_plugins.py
@@ -22,8 +22,10 @@ import pluggy
 from armi import context
 from armi import getApp
 from armi import getPluginManagerOrFail
+from armi import interfaces
 from armi import plugins
 from armi import utils
+from armi.bookkeeping.db.database3 import DatabaseInterface
 from armi.reactor.flags import Flags
 from armi.reactor.tests import test_reactors
 from armi.settings import caseSettings
@@ -65,6 +67,28 @@ class UserPluginOnProcessCoreLoading(plugins.UserPlugin):
     def onProcessCoreLoading(core, cs):
         fuels = core.getAssemblies(Flags.FUEL)
         fuels[0].p.buLimit = fuels[0].p.buLimit + 1000
+
+
+class UpInterface(interfaces.Interface):
+    """TODO"""
+    name = "UpInterface"
+
+    def interactEveryNode(self, cycle, node):
+        self.r.core.p.power += int(cycle + node + 100)
+        assert False
+
+
+class UserPluginWithInterface(plugins.UserPlugin):
+    """TODO"""
+    @staticmethod
+    @HOOKSPEC
+    def exposeInterfaces(cs):
+        assert False
+        return [
+            interfaces.InterfaceInfo(
+                interfaces.STACK_ORDER.PREPROCESSING, UpInterface, {"enabled": enabled}
+            )
+        ]
 
 
 class TestUserPlugins(unittest.TestCase):
@@ -155,3 +179,58 @@ class TestUserPlugins(unittest.TestCase):
         sumBuLimits = sum(f.p.buLimit for f in fuels)
         plug0.onProcessCoreLoading(core=r.core, cs=o.cs)
         self.assertEqual(sum(f.p.buLimit for f in fuels), sumBuLimits + 1000)
+
+    def test_userPluginWithInterfaces(self):
+        """TODO"""
+        # register the plugin
+        app = getApp()
+        #count = app.pluginManager.counter
+        #app.pluginManager.register(UserPluginWithInterface)
+        #self.assertEqual(app.pluginManager.counter, count + 1)
+
+        count = app.pluginManager.counter
+        plugins = ["armi.tests.test_user_plugins.UserPluginWithInterface"]
+        app.registerUserPlugins(plugins)
+        self.assertEqual(app.pluginManager.counter, count + 1)
+
+        # validate the plugins was registered
+        pluginz = app.pluginManager.list_name_plugin()
+        pluginNames = [p[0] for p in pluginz]
+        name = "UserPluginWithInterface"
+        self.assertIn(name, pluginNames)
+
+        # load a reactor and grab the fuel assemblies
+        o, r = test_reactors.loadTestReactor(TEST_ROOT)
+        fuels = r.core.getAssemblies(Flags.FUEL)
+
+        #o.interfaces = []
+        #o.createInterfaces()  # TODO: JOHN! Not fixing the problem...
+
+        breakpoint()
+
+        #for p in pluginz:
+        #    p[1].exposeInterfaces(cs=o.cs)
+        #getPluginManagerOrFail().hook.exposeInterfaces(cs=o.cs)
+        app.pluginManager.hook.exposeInterfaces(cs=o.cs)
+
+        assert False
+
+        # TODO: Validate our interface is in the stack!
+        print("\n\n")
+        print(pluginNames)
+        print(o.interfaces)
+
+        for i, interf in enumerate(o.interfaces):
+            if "history" in str(interf).lower():
+                o.interfaces = o.interfaces[:i] + o.interfaces[i + 1 :]
+                break
+
+        # TODO: Validate our interface is in the stack!
+        print(o.interfaces)
+        print("\n\n")
+
+        # TODO
+        self.assertEqual(r.core.p.power, 100000000.0)
+        o.cs["nCycles"] = 2
+        o.operate()
+        self.assertNotEqual(r.core.p.power, 100000000.0)
