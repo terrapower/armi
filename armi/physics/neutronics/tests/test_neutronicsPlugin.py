8f343489e07c532d003d973f066ef05c93d1a768
Chris Keckler <ckeckler@terrapower.com>
2022-06-06 21:35:07 -0500

    Adding validation to Settings using schema (#595)

--- armi/physics/neutronics/tests/test_neutronicsPlugin.py
+++ armi/physics/neutronics/tests/test_neutronicsPlugin.py
@@ -111,7 +111,7 @@ class NeutronicsReactorTests(unittest.TestCase):
         cs = _getModifiedSettings(
             customSettings={
                 "beta": [0.0] * 6,
-                "decayConstants": [0.0] * 6,
+                "decayConstants": [1.0] * 6,
             }
         )
         getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
@@ -148,7 +148,7 @@ class NeutronicsReactorTests(unittest.TestCase):
         # parameter.
         r = tests.getEmptyHexReactor()
         cs = _getModifiedSettings(
-            customSettings={"beta": [0.0], "decayConstants": [0.0]},
+            customSettings={"beta": [0.0], "decayConstants": [1.0]},
         )
         getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
         self.assertEqual(r.core.p.beta, sum(cs["beta"]))
@@ -159,7 +159,7 @@ class NeutronicsReactorTests(unittest.TestCase):
         # group-wise beta input.
         r = tests.getEmptyHexReactor()
         cs = _getModifiedSettings(
-            customSettings={"decayConstants": [0.0] * 6},
+            customSettings={"decayConstants": [1.0] * 6},
         )
         getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
         self.assertIsNone(r.core.p.beta)
@@ -171,7 +171,7 @@ class NeutronicsReactorTests(unittest.TestCase):
         # is still assigned.
         r = tests.getEmptyHexReactor()
         cs = _getModifiedSettings(
-            customSettings={"decayConstants": [0.0] * 6, "beta": 0.0},
+            customSettings={"decayConstants": [1.0] * 6, "beta": 0.0},
         )
         getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
         self.assertEqual(r.core.p.beta, cs["beta"])
@@ -194,7 +194,7 @@ class NeutronicsReactorTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             r = tests.getEmptyHexReactor()
             cs = _getModifiedSettings(
-                customSettings={"decayConstants": [0.0] * 6, "beta": [0.0]},
+                customSettings={"decayConstants": [1.0] * 6, "beta": [0.0]},
             )
             getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
 
@@ -203,38 +203,7 @@ class NeutronicsReactorTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             r = tests.getEmptyHexReactor()
             cs = _getModifiedSettings(
-                customSettings={"decayConstants": [0.0] * 6, "beta": [0.0] * 5},
-            )
-            getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
-
-        # The following tests check the voluptuous schema definition. This
-        # ensures that anything except NoneType, [float], float are not valid
-        # inputs.
-        with self.assertRaises(vol.AnyInvalid):
-            r = tests.getEmptyHexReactor()
-            cs = _getModifiedSettings(
-                customSettings={"decayConstants": [1]},
-            )
-            getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
-
-        with self.assertRaises(vol.AnyInvalid):
-            r = tests.getEmptyHexReactor()
-            cs = _getModifiedSettings(
-                customSettings={"beta": [1]},
-            )
-            getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
-
-        with self.assertRaises(vol.AnyInvalid):
-            r = tests.getEmptyHexReactor()
-            cs = _getModifiedSettings(
-                customSettings={"beta": 1},
-            )
-            getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
-
-        with self.assertRaises(vol.AnyInvalid):
-            r = tests.getEmptyHexReactor()
-            cs = _getModifiedSettings(
-                customSettings={"beta": (1, 2, 3)},
+                customSettings={"decayConstants": [1.0] * 6, "beta": [0.0] * 5},
             )
             getPluginManagerOrFail().hook.onProcessCoreLoading(core=r.core, cs=cs)
 
