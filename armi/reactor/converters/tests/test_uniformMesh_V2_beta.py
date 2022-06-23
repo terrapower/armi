82c428e4b373f20f21eec2c84464ed29d4c895ff
aalberti <c-aalberti@terrapower.com>
2022-06-23 15:50:13 -0700

    moving uniformMesh to armi.reactor.converters
    Summary:
    - things are waaaaay clearer and more organized if moved over into armi.reactor.converters. And sets things up a little better to eventually replace the old uniformMesher
    - adding unit tests to ensure mass conservation at the block, assembly, and core levels. also has tests for exceptions to keep coverage up.

---
+++ armi/reactor/converters/tests/test_uniformMesh_V2_beta.py
@@ -0,0 +1,184 @@
+# Copyright 2019 TerraPower, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable = invalid-name, wrong-import-position
+import unittest
+import os
+from armi.reactor.flags import Flags
+from armi.reactor.tests import test_reactors
+from armi.tests import TEST_ROOT
+from armi.utils import densityTools
+from armi.reactor.converters.tests.test_axialExpansionChanger import (
+    buildTestAssemblyWithFakeMaterial,
+)
+from armi.reactor.converters import uniformMesh_V2_beta
+from armi.reactor.converters.uniformMesh_V2_beta import UniformMeshV2
+
+class TestMassConservation(unittest.TestCase):
+    """ensure that mass is conserved"""
+
+    def setUp(self) -> None:
+        _o, self.r = test_reactors.loadTestReactor(
+            os.path.join(TEST_ROOT, "detailedAxialExpansion"),
+            customSettings={
+                "detailedAxialExpansion": True,
+                "inputHeightsConsideredHot": False,
+            },
+        )
+        self.origCoreMass = self.r.core.getMass()
+        self.origAssemblyMass = {}
+        self.newAssemblyMass = {}
+        self.origBlockMass = {}
+        self.newBlockMass = {}
+
+        self.uniMesher = UniformMeshV2(self.r.core, primaryFlag=Flags.FUEL, secondaryFlag=Flags.CONTROL)
+        self.uniMesher.getCoreWideUniformMesh()
+
+    def calculateAssemblyAndBlockMasses(self):
+        """calculates the assembly and block masses for the origin and uniform mesh assemblies
+
+        Notes
+        -----
+        - the actions contained in loop over r.core.getAssemblies() is the same as
+          uniforMesh_V2::applyCoreWideUniformMesh. However, that function can't be
+          used since assembly and block masses need to be calculated at intermediate steps
+        - uniforMesh_V2::applyCoreWideUniformMesh is directly tested in test_CoreWideUniformMesh
+        """
+        for a in self.r.core.getAssemblies():
+            # calculate uniform mesh for assembly
+            uniAssem = self.uniMesher.updateAssemblyAxialMesh(a)
+            # get spatial locator and remove original assembly from core
+            loc = a.spatialLocator
+            self.r.core.removeAssembly(a, discharge=False)
+            # stash original assembly and block masses
+            self.origAssemblyMass[a.name] = a.getMass()
+            self.getOriginalBlockMass(a)
+            # store new assembly and block masses
+            self.newAssemblyMass[a.name] = uniAssem.getMass()
+            self.newBlockMass[a.name] = []
+            for b in uniAssem.getChildren():
+                self.newBlockMass[a.name].append(b.getMass())
+            # add uniAssem to loc in core
+            self.r.core.add(uniAssem, loc)
+
+    def getOriginalBlockMass(self, a):
+        """get original block mass that corresponds to uniform mesh
+        - accounts for overlap between original and uniform mesh
+        """
+        self.origBlockMass[a.name] = []
+        zLower = 0.0
+        for zUpper in self.uniMesher.uniformMesh:
+            overlappingBlockInfo = a.getBlocksBetweenElevations(zLower, zUpper)
+            self.origBlockMass[a.name].append(getPredictedUniformMeshBlockMass(overlappingBlockInfo))
+            zLower = zUpper
+
+    def test_massConservation(self):
+        self.calculateAssemblyAndBlockMasses()
+        for key in self.origAssemblyMass.keys(): #pylint: disable=consider-iterating-dictionary
+            # check block masses
+            for i, (orig, new) in enumerate(
+                zip(self.origBlockMass[key], self.newBlockMass[key])
+            ):
+                self.assertAlmostEqual(
+                    orig,
+                    new,
+                    msg="Assembly key {0}; Block number {1}; Mass is not conserved.".format(
+                        key, i
+                    ),
+                )
+            # then check assembly masses
+            self.assertAlmostEqual(
+                self.origAssemblyMass[key],
+                self.newAssemblyMass[key],
+                msg="Assembly {0} mass is not conserved.".format(key),
+            )
+
+    def test_CoreWideUniformMesh(self):
+        self.uniMesher.applyCoreWideUniformMesh()
+        # check core mass
+        self.assertAlmostEqual(
+            self.origCoreMass,
+            self.r.core.getMass(),
+            places=5,
+            msg="Core mass is not conserved.",
+        )
+        # check core axial mesh aligns with self.uniformMesh
+        for i, _val in enumerate(self.uniMesher.uniformMesh):
+            self.assertEqual(
+                self.uniMesher.uniformMesh[i],
+                self.r.core.p.axialMesh[i],
+                msg="The core uniform mesh doesn't align with the calculated uniform mesh.",
+            )
+
+def getPredictedUniformMeshBlockMass(overlappingBlockInfo: list):
+    """calculates the mass of a hypothetical block whose contributions are determined by overlappingBlockInfo
+    
+    Parameters
+    ----------
+    overlappingBlockInfo: list
+        output from a.getBlocksBetweenElevations(zLower, zUpper)
+    
+    Returns
+    -------
+    mass : float
+        predicted block mass
+    """
+    mass = 0.0
+    for _i, (b, h) in enumerate(overlappingBlockInfo):
+        for c in b:
+            nuclideNames = c._getNuclidesFromSpecifier(None) #pylint: disable=protected-access
+            volume = (c.getArea() * h) / (b.getSymmetryFactor())
+            densities = c.getNuclideNumberDensities(nuclideNames)
+            mass += sum(
+                densityTools.getMassInGrams(nucName, volume, numberDensity)
+                for nucName, numberDensity in zip(nuclideNames, densities)
+            )
+    return mass
+
+class TestExceptions(unittest.TestCase):
+    """make sure hardcoded exceptions work as expected"""
+
+    def test_checkAxialMeshValidity_invalidMesh(self):
+        failMesh = [1.0, 2.0, 3.0, 5.0, 4.0]
+        with self.assertRaises(RuntimeError) as cm:
+            uniformMesh_V2_beta.checkAxialMeshValidity(failMesh)
+            the_exception = cm.exception
+            self.assertEqual(the_exception.error_code, 3)
+
+    def test_checkAxialMeshValidity_preservationFail(self):
+        """
+        Notes
+        ----
+        - testMesh has a "mesh discrepancy" between testMesh[2] and testMesh[1]
+        - If preservedMesh is None, the uniform mesh point would simply be an average of these values.
+        - Since preservedMesh is not None, we have to choose which ever mesh point (either 2.0 or 2.05)
+          that is in preservedMesh (the lowest gets checked first and therefore has priority).
+        - Ideally, either of these options are in preservedMesh. However, to induce the RuntimeError,
+          we do not include either.
+        """
+        testMesh = [1.0, 2.0, 2.05, 3.05, 4.05, 5.0]
+        preservedMesh = [1.0, 2.01, 3.0, 4.0, 4.95, 5.0]
+        with self.assertRaises(RuntimeError) as cm:
+            uniformMesh_V2_beta.checkAxialMeshValidity(testMesh, preservedMesh)
+            the_exception = cm.exception
+            self.assertEqual(the_exception.error_code, 3)
+
+    def test_updateAssemblyAxialMesh(self):
+        a = buildTestAssemblyWithFakeMaterial(name="FakeMat")
+        uniMesh = UniformMeshV2(None, Flags.FUEL, Flags.CONTROL)
+        uniMesh.uniformMesh = [1.0, 2.0, 2.0, 3.0, 3.0, 4.0, 5.0]
+        with self.assertRaises(ValueError) as cm:
+            uniMesh.updateAssemblyAxialMesh(a)
+            the_exception = cm.exception
+            self.assertEqual(the_exception.error_code, 3)
---
+++ armi/reactor/converters/uniformMesh_V2_beta.py
@@ -0,0 +1,483 @@
+# Copyright 2019 TerraPower, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""NOTE: This is currently in beta testing, and is not (yet) a replacement for uniformMesh.py"""
+
+# pylint: disable = anomalous-backslash-in-string, invalid-name
+
+import logging
+from armi.reactor.flags import Flags
+from armi.reactor import grids
+
+runLog = logging.getLogger(__name__)
+
+
+class UniformMeshV2:
+    """beta class for uniform mesh converter overhaul"""
+
+    def __init__(self, core, primaryFlag: Flags, secondaryFlag: Flags):
+        self.core = core
+        self.primaryFlag = primaryFlag
+        self.secondaryFlag = secondaryFlag
+        self.uniformMesh = []
+
+    def getCoreWideUniformMesh(self, printLikeBlkSmears: bool = False):
+        """calculate core wide uniform mesh
+
+        Parameters
+        ----------
+        printLikeBlkSmears : boolean, optional
+            There may be inter-block smearing between similar/like blocks. E.g., fuel to fuel, plenum to plenum, etc.
+            These smearing instances may be less important than dissimilar blocks (e.g., control to plenum,
+            shield to fuel, fuel to plenum). This boolean controls whether or not the similar/like block
+            smearing cases are printed.
+        """
+        runLog.info(
+            "Getting uniform mesh for axially disjoint core... "
+            "Inter-block material smearing report available via 'verbosity: debug'."
+        )
+        primaryAssems = []
+        secondaryAssems = []
+        otherAssems = []
+        for a in self.core.getAssemblies():
+            if a.hasFlags(self.primaryFlag):
+                primaryAssems.append(a)
+            elif a.hasFlags(self.secondaryFlag):
+                secondaryAssems.append(a)
+            else:
+                otherAssems.append(a)
+
+        # Overlay uniform meshes on each other for a core wide uniform mesh and check for smearing
+        self.uniformMesh = createUniformMeshCore(
+            primaryAssemsToPreserve=primaryAssems,
+            secondaryAssemsToPreserve=secondaryAssems,
+            otherAssems=otherAssems,
+        )
+
+        runLog.debug(
+            "Smearing Report For Primary Assemblies -- {0:s}".format(
+                str(self.primaryFlag)
+            )
+        )
+        self.checkForSmearing(primaryAssems, printLikeBlkSmears)
+
+        runLog.debug(
+            "Smearing Report For Secondary Assemblies -- {0:s}".format(
+                str(self.secondaryFlag)
+            )
+        )
+        self.checkForSmearing(secondaryAssems, printLikeBlkSmears)
+
+    def applyCoreWideUniformMesh(self):
+        """apply the core wide uniform mesh to the core"""
+        runLog.info("Applying uniform mesh to reactor.core...")
+        for assem in self.core.getAssemblies():
+            uniAssem = self.updateAssemblyAxialMesh(assem)
+            spatialLocator = assem.spatialLocator
+            self.core.removeAssembly(assem, discharge=False)
+            self.core.add(uniAssem, spatialLocator)
+
+        self.core.p.axialMesh = self.uniformMesh
+
+    def updateAssemblyAxialMesh(self, sourceAssembly):
+        """apply calculated core-wise uniform mesh to assembly
+
+        Parameters
+        ----------
+        sourceAssembly: :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object
+            ARMI assembly to be adjusted
+
+        Returns
+        -------
+        uniAssm : :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object
+            a new assembly object that has self.uniformMesh applied to sourceAssembly
+
+        Raises
+        ------
+        ValueError
+            unexpected error in assembly.py::Assembly::getBlocksBetweenElevations. Usually means
+            that zLower and zUpper are the same... Which isn't good.
+        """
+        # create a new assembly, a, that is the same type and contains the same params, grid, etc as sourceAssembly
+        uniAssem = sourceAssembly.__class__(sourceAssembly.getType())
+        uniAssem.setName(sourceAssembly.getName())
+        for param in sourceAssembly.getParamNames():
+            uniAssem.p[param] = sourceAssembly.p[param]
+        uniAssem.spatialGrid = grids.axialUnitGrid(len(sourceAssembly))
+        uniAssem.spatialGrid.armiObject = uniAssem
+
+        zLower = 0
+        for i, zUpper in enumerate(self.uniformMesh):
+            overlappingBlockInfo = sourceAssembly.getBlocksBetweenElevations(
+                zLower, zUpper
+            )
+            # This is not expected to occur given that the assembly mesh is consistent with
+            # the blocks within it, but this is added for defensive programming and to
+            # highlight a developer issue.
+            if not overlappingBlockInfo:
+                raise ValueError(
+                    f"No blocks found between {zLower:.3f} and {zUpper:.3f} in {sourceAssembly}. "
+                    f"This is a major bug that should be reported to the developers."
+                )
+            b = createNewBlock(overlappingBlockInfo, zUpper - zLower)
+            b.p.assemNum = uniAssem.p.assemNum
+            b.name = b.makeName(uniAssem.p.assemNum, i)
+            uniAssem.add(b)
+            zLower = zUpper
+
+        uniAssem.reestablishBlockOrder()
+        uniAssem.calculateZCoords()
+
+        return uniAssem
+
+    def checkForSmearing(self, aList: list, printLikeBlkSmears: bool):
+        """indicate smearing by presence of block bounds that are not aligned with uniformMesh
+
+        Parameters
+        ----------
+        aList : list, :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object
+            List of assemblies which uniformMesh would be applied; will or will not have inter-block smearing\
+        printLikeBlkSmears : boolean
+            There may be inter-block smearing between similar/like blocks. E.g., fuel to fuel, plenum to plenum, etc.
+            These smearing instances may be less important than dissimilar blocks (e.g., control to plenum,
+            shield to fuel, fuel to plenum). This boolean controls whether or not the similar/like block
+            smearing cases are printed.
+        """
+        for a in aList:
+            runLog.debug(
+                "{0}...".format(a) # pylint: disable=logging-format-interpolation
+            ) 
+            for i, _z in enumerate(self.uniformMesh):
+                if i == 0:
+                    bottom = 0.0
+                else:
+                    bottom = self.uniformMesh[i - 1]
+                top = self.uniformMesh[i]
+                overlap = a.getBlocksBetweenElevations(bottom, top)
+                if len(overlap) > 1:
+                    diffMaterialInBlocks = len(set(b.p.flags for b, _h in overlap)) > 1
+                    if printLikeBlkSmears or diffMaterialInBlocks:
+                        runLog.debug(
+                            "    uniform mesh block {0}, ({1:.3f},{2:.3f})".format(
+                                i, bottom, top
+                            )
+                        )
+                        runLog.debug("    percent contribution \t block")
+                        for b, val in overlap:
+                            runLog.debug(
+                                "    {0:9.2f}\t\t\t{1}".format(
+                                    val / (top - bottom) * 100.0, b.p.flags
+                                )
+                            )
+
+
+def createNewBlock(overlappingBlockInfo: list, uniformHeight: float):
+    """create new armi block from overlapping block info"""
+    # get source block type
+    heights = [h for _b, h in overlappingBlockInfo]
+    blocks = [b for b, _h in overlappingBlockInfo]
+    sourceBlk = blocks[heights.index(max(heights))]
+
+    # create a new block, b, that is the same type and contains the same params, grid, etc as sourceBlk
+    b = sourceBlk.__class__(sourceBlk.getType())
+    for param in sourceBlk.getParamNames():
+        b.p[param] = sourceBlk.p[param]
+    for c in sourceBlk.getChildren():
+        b.add(c.__copy__())
+    b.spatialGrid = sourceBlk.spatialGrid
+    b.p.height = uniformHeight  # other z params get set in uniAssem.calculateZCoords
+    b.clearCache()  # reset component volumes so that they are referenced to new height
+    _setNumberDensitiesFromOverlaps(b, overlappingBlockInfo)
+
+    return b
+
+
+def createUniformMeshCore(
+    primaryAssemsToPreserve: list, secondaryAssemsToPreserve: list, otherAssems: list
+):
+    """using three sets of assemblies, create a core-wide uniform mesh
+
+    Parameters
+    ----------
+    primaryAssemsToPreserve: list, :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object.
+        list of assemblies that will be given top priority for resolving axial mesh discrepancies
+    secondaryAssemsToPreserve: list, :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object.
+        list of assemblies that will be given secondary priority for resolving axial mesh discrepancies
+    otherAssems: list, :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object.
+        remaining assemblies in ARMI core that do not belong to primary or secondary categories
+
+    Returns
+    -------
+    coreUniformMesh: list
+        uniform axial mesh for reactor
+    """
+    # get uniformMesh for otherAssems
+    uniformMeshOther = otherAssems[0].getAxialMesh()
+    for assem in otherAssems:
+        uniformMeshOther = updateUniformMesh(assem.getAxialMesh(), uniformMeshOther)
+
+    # get uniformMesh for secondaryAssemsToPreserve
+    uniformMeshSecondaryPreserve = secondaryAssemsToPreserve[0].getAxialMesh()
+    for assem in secondaryAssemsToPreserve:
+        uniformMeshSecondaryPreserve = updateUniformMesh(
+            assem.getAxialMesh(), uniformMeshSecondaryPreserve
+        )
+
+    # get uniformMesh for primaryAssemsToPreserve
+    uniformMeshPrimaryPreserve = primaryAssemsToPreserve[0].getAxialMesh()
+    for assem in primaryAssemsToPreserve:
+        uniformMeshPrimaryPreserve = updateUniformMesh(
+            assem.getAxialMesh(), uniformMeshPrimaryPreserve
+        )
+
+    # overlay uniformMeshSecondaryPreserve on uniformMeshOther
+    coreUniformMesh = updateUniformMesh(
+        uniformMeshSecondaryPreserve, uniformMeshOther, preserve=True
+    )
+
+    # overlay uniformMeshPrimaryPreserve on uniformMeshSecondaryPreserve
+    coreUniformMesh = updateUniformMesh(
+        uniformMeshPrimaryPreserve, coreUniformMesh, preserve=True
+    )
+
+    return coreUniformMesh
+
+
+def updateUniformMesh(
+    assemAxialMesh: list, currentUniformMesh: list, preserve: bool = False
+):
+    """update currentUniformMesh given assembly, assem
+
+    Parameters
+    ----------
+    assemAxialMesh : list, float
+        axial mesh of :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object
+    currentUniformMesh : list, float
+        the current uniform mesh
+    preserve : bool, optional
+        boolean to engage different behavior if assemAxialMesh is to be given priority for preservation
+
+    Returns
+    -------
+    newMeshPoints : list, float
+        the updated uniform mesh based on any discrepancies between currentUniformMesh
+        and the axial mesh of assem
+    """
+    newMeshPoints = []
+    ib, ia = 0, 0
+    while ib < len(currentUniformMesh):
+        diff = abs(currentUniformMesh[ib] - assemAxialMesh[ia])
+        if 0 < diff < 1:
+            if not preserve:
+                # Average the mesh difference; will result in some inevitable material smearing.
+                newMeshPoints.append(
+                    (currentUniformMesh[ib] + assemAxialMesh[ia]) / 2.0
+                )
+            else:
+                # choose mesh point for assembly that you want to preserve
+                newMeshPoints.append(assemAxialMesh[ia])
+            ib += 1
+            ia += 1
+        elif diff >= 1:
+            # Difference is sufficient to add new mesh point.
+            meshBetweenBlk = getBlockBoundsBetweenElevation(
+                assemAxialMesh,
+                max(currentUniformMesh[ib - 1], assemAxialMesh[ia - 1]),
+                max(currentUniformMesh[ib], assemAxialMesh[ia]),
+            )
+            if not meshBetweenBlk:
+                newMeshPoints.append(currentUniformMesh[ib])
+                ib += 1
+            else:
+                for val in meshBetweenBlk:
+                    newMeshPoints.append(val)
+                    ia += 1
+        elif diff == 0:
+            # Mesh points perfectly align.
+            newMeshPoints.append(currentUniformMesh[ib])
+            ib += 1
+            ia += 1
+
+    if preserve:
+        checkAxialMeshValidity(newMeshPoints, preservedMeshPoints=assemAxialMesh)
+    else:
+        checkAxialMeshValidity(newMeshPoints)
+
+    return newMeshPoints
+
+
+def checkAxialMeshValidity(axialMesh: list, preservedMeshPoints: list = None):
+    """check validity of axialMesh (that each subsequent entry increases)
+
+    Parameters
+    ----------
+    axialMesh : float, list
+        the axialMesh to verify validity for.
+    preservedMeshPoints: float, list, optional
+        mesh points to use to resolve discrepancies (see notes for details)
+
+    Notes
+    -----
+    - Validity is accounted for by checking that each subsequent axial mesh location
+    is greater than the one before it.
+    - a "discrepancy" is defined as cases in which the mesh points within axialMesh
+    are found to be less than 1 cm. Discrepancies are only checked if preservedMeshPoints
+    is not None.
+
+    Raises
+    ------
+    RuntimeError
+        an axial mesh point is less than the one before it
+    RuntimeError
+        neither axial mesh point being resolved is found in perservedMeshPoints
+    """
+    i = 0
+    while i < (len(axialMesh) - 1):
+        check = axialMesh[i + 1] - axialMesh[i]
+        if check < 0:
+            runLog.error(axialMesh)
+            raise RuntimeError(
+                "The values in axialMesh need to increase, this is not the case!"
+            )
+        if preservedMeshPoints is None:
+            i += 1
+        else:
+            if check >= 1:
+                i += 1
+            elif 0 < check < 1:
+                try:
+                    preservedMeshPoints.index(axialMesh[i])
+                    axialMesh.pop(i + 1)
+                except ValueError:
+                    try:
+                        preservedMeshPoints.index(axialMesh[i + 1])
+                        axialMesh.pop(i)
+                    except ValueError as second:
+                        raise RuntimeError(
+                            "values {0} and {1} not found in preservedMeshPoints!".format(
+                                axialMesh[i], axialMesh[i + 1]
+                            )
+                        ) from second
+
+
+def getBlockBoundsBetweenElevation(axialGrid: list, zLower: float, zUpper: float):
+    """retrieve axial mesh values on axialGrid that fall between zLower and zUpper
+
+    Parameters
+    ----------
+    axialGrid: list, float
+        axial mesh/grid for a given :py:class:`Assembly <armi.reactor.assemblies.Assembly>` object
+    zLower: float
+        lower elevation bound (not necessarily associated with block bound)
+    zUpper: float
+        upper elevation bound (not necessarily associated with block bound)
+
+    Notes
+    -----
+    - if axialGrid is within EPS of either zLower or zUpper, it is skipped
+
+    Returns
+    -------
+    blkBndsBetweenElev: list
+        a list of axial mesh values from axialGrid that fall between zLower and zUpper
+    """
+    EPS = 1e-3  # pylint: disable=invalid-name
+    blkBndsBetweenElev = []
+    for i in range(len(axialGrid[:-1])):
+        if zLower < axialGrid[i] < zUpper:
+            if axialGrid[i] - zLower < EPS:
+                continue
+            if zUpper - axialGrid[i] < EPS:
+                continue
+            blkBndsBetweenElev.append(axialGrid[i])
+        elif axialGrid[i] > zUpper:
+            break
+
+    return blkBndsBetweenElev
+
+
+def _setNumberDensitiesFromOverlaps(block, overlappingBlockInfo):
+    """
+    Set number densities on a block based on overlapping blocks
+
+    Notes
+    -----
+    A conservation of number of atoms technique is used to map the non-uniform number densities onto the uniform
+    neutronics mesh. When the number density of a height :math:`H` neutronics mesh block :math:`N^{\prime}` is
+    being computed from one or more blocks in the ARMI mesh with number densities :math:`N_i` and
+    heights :math:`h_i`, the following formula is used:
+
+    .. math::
+
+        N^{\prime} =  \sum_i N_i \frac{h_i}{H}
+
+    NOTE: yes, this is a copy from armi.reactor.converters.uniformMesh; the intent is that it will be carried over
+          when the original uniformMesh gets replaced.
+
+    See Also
+    --------
+    _setStateFromOverlaps : does this for state other than number densities.
+    """
+    totalDensities = {}
+    block.clearNumberDensities()
+    blockHeightInCm = block.getHeight()
+    for overlappingBlock, overlappingHeightInCm in overlappingBlockInfo:
+        for nucName, numberDensity in overlappingBlock.getNumberDensities().items():
+            totalDensities[nucName] = (
+                totalDensities.get(nucName, 0.0)
+                + numberDensity * overlappingHeightInCm / blockHeightInCm
+            )
+    block.setNumberDensities(totalDensities)
+
+
+# """manually load detailedAxialExpansion test reactor and ensure mass conservation for uniform mesh"""
+# from armi.reactor.blueprints.reactorBlueprint import summarizeMaterialData
+# filePath = os.path.join(TEST_ROOT, "detailedAxialExpansion")
+# fileName = "armiRun.yaml"
+# # instantiate case settings, blueprints, reactor, core & grid designs
+# cs = settings.Settings(fName=os.path.join(filePath, fileName))
+# cs = cs.modified(newSettings={"detailedAxialExpansion": False})
+# bp = blueprints.loadFromCs(cs)
+# r = reactors.Reactor(cs.caseTitle, bp)
+# coreDesign = bp.systemDesigns["core"]
+# gridDesign = bp.gridDesigns.get(coreDesign.gridName, None)
+# core = coreDesign._resolveSystemType(coreDesign.typ)(coreDesign.name)
+# core.setOptionsFromCs(cs)
+# spatialGrid = gridDesign.construct()
+# core.spatialGrid = spatialGrid
+# core.spatialGrid.armiObject = core
+# r.add(core)
+# # add assemblies to core
+# for locationInfo, aTypeID in gridDesign.gridContents.items():
+#     newAssembly = bp.constructAssem(cs, specifier=aTypeID)
+#     i, j = locationInfo
+#     loc = core.spatialGrid[i, j, 0]
+#     core.add(newAssembly, loc)
+# summarizeMaterialData(core)
+# coreDesign._modifyGeometry(core, gridDesign)
+# # calculate the uniform mesh
+# uniformMeshCore = core.getCoreWideUniformMesh(
+#     Flags.FUEL, Flags.CONTROL
+# )
+# # apply the uniform mesh and ensure
+# for assem in core.getAssemblies():
+#     uniAssem = reactors.updateAssemblyAxialMesh(assem, uniformMeshCore)
+#     self.assertAlmostEqual(
+#         assem.getMass(),
+#         uniAssem.getMass(),
+#         msg="Assembly = {0} -- Mass is not being conserved between original and uniform axial mesh assemblies!".format(assem)
+#     )
+#     spatialLocator = assem.spatialLocator
+#     core.removeAssembly(assem, discharge=False)
+#     core.add(uniAssem, spatialLocator)
