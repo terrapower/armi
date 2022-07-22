d80419d17ce7d8a1730024d69c950910b9cb9ca2
John Stilley <john_science@protonmail.com>
2022-07-22 14:27:45 -0700

    Adding test coverage for afterLoadDB hook

--- armi/bookkeeping/tests/test_historyTracker.py
+++ armi/bookkeeping/tests/test_historyTracker.py
@@ -35,7 +35,7 @@ from armi.reactor import blocks
 from armi.reactor import grids
 from armi.reactor.flags import Flags
 from armi.tests import ArmiTestHelper
-from armi.utils import directoryChangers
+from armi.utils.directoryChangers import TemporaryDirectoryChanger
 
 CASE_TITLE = "anl-afci-177"
 THIS_DIR = os.path.dirname(__file__)  # b/c tests don't run in this folder
@@ -57,34 +57,13 @@ class TestHistoryTracker(ArmiTestHelper):
 
     @classmethod
     def setUpClass(cls):
-        # Not using a directory changer since it isn't important that we go back in the
-        # first place, and we don't want to get tangled with the directory change below.
-        # We need to be in the TUTORIAL_DIR in the first place so that for `filesToMove`
-        # to work right.
+        # We need to be in the TUTORIAL_DIR so that for `filesToMove` to work right.
         os.chdir(TUTORIAL_DIR)
 
-        # Make sure to do this work in a temporary directory to avoid race conditions
-        # when running tests in parallel with xdist.
-        cls.dirChanger = directoryChangers.TemporaryDirectoryChanger(
-            filesToMove=TUTORIAL_FILES
-        )
+        # Do this work in a temp dir, to avoid race conditions.
+        cls.dirChanger = TemporaryDirectoryChanger(filesToMove=TUTORIAL_FILES)
         cls.dirChanger.__enter__()
         runTutorialNotebook()
-
-        reloadCs = settings.Settings(f"{CASE_TITLE}.yaml")
-
-        newSettings = {}
-        newSettings["db"] = True
-        newSettings["reloadDBName"] = pathlib.Path(f"{CASE_TITLE}.h5").absolute()
-        newSettings["runType"] = "Snapshots"
-        newSettings["loadStyle"] = "fromDB"
-        newSettings["detailAssemLocationsBOL"] = ["001-001"]
-
-        reloadCs = reloadCs.modified(newSettings=newSettings)
-        reloadCs.caseTitle = "armiRun"
-
-        o = armi_init(cs=reloadCs)
-        cls.o = o
 
     @classmethod
     def tearDownClass(cls):
@@ -94,13 +73,13 @@ class TestHistoryTracker(ArmiTestHelper):
         cs = settings.Settings(f"{CASE_TITLE}.yaml")
         newSettings = {}
         newSettings["db"] = True
-        newSettings["reloadDBName"] = pathlib.Path(f"{CASE_TITLE}.h5").absolute()
-        newSettings["loadStyle"] = "fromDB"
         newSettings["detailAssemLocationsBOL"] = ["001-001"]
+        newSettings["loadStyle"] = "fromDB"
+        newSettings["reloadDBName"] = pathlib.Path(f"{CASE_TITLE}.h5").absolute()
         newSettings["startNode"] = 1
         cs = cs.modified(newSettings=newSettings)
 
-        self.td = directoryChangers.TemporaryDirectoryChanger()
+        self.td = TemporaryDirectoryChanger()
         self.td.__enter__()
 
         c = case.Case(cs)
--- armi/tests/test_plugins.py
+++ armi/tests/test_plugins.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 """Provides functionality for testing implementations of plugins"""
+import os
+import pathlib
 import unittest
 from typing import Optional
 
@@ -21,6 +23,26 @@ import yamlize
 from armi import interfaces
 from armi import plugins
 from armi import settings
+from armi.bookkeeping.tests._constants import TUTORIAL_FILES
+from armi.bookkeeping.tests.test_historyTracker import runTutorialNotebook
+from armi.cases import case
+from armi.context import ROOT
+from armi.reactor.flags import Flags
+from armi.tests import ArmiTestHelper
+from armi.utils.directoryChangers import TemporaryDirectoryChanger
+
+CASE_TITLE = "anl-afci-177"
+TUTORIAL_DIR = os.path.join(ROOT, "tests", "tutorials")
+
+
+class TestPluginAfterLoadDB(plugins.ArmiPlugin):
+    """Toy Plugin, used to test the afterLoadDB hook"""
+
+    @staticmethod
+    @plugins.HOOKIMPL
+    def afterLoadDB(core, cs):
+        for b in core.getBlocks(Flags.FUEL):
+            b.p.power += 1.0
 
 
 class TestPlugin(unittest.TestCase):
@@ -53,11 +75,12 @@ class TestPlugin(unittest.TestCase):
         """Make sure that the exposeInterfaces hook is properly implemented"""
         if self.plugin is None:
             return
-        if not hasattr(self.plugin, "exposeInterfaces"):
-            return
 
         cs = settings.getMasterCs()
         results = self.plugin.exposeInterfaces(cs)
+        if results is None or not len(results):
+            return
+
         # each plugin should return a list
         self.assertIsInstance(results, list)
         for result in results:
@@ -71,6 +94,76 @@ class TestPlugin(unittest.TestCase):
             self.assertIsInstance(order, (int, float))
             self.assertTrue(issubclass(interface, interfaces.Interface))
             self.assertIsInstance(kwargs, dict)
+
+
+class TestPluginsNeedDB(ArmiTestHelper):
+    """This test class provides test plugins a realistic ARMI DB file to act upon."""
+
+    @classmethod
+    def setUpClass(cls):
+        # We need to be in the TUTORIAL_DIR so that for `filesToMove` to work right.
+        os.chdir(TUTORIAL_DIR)
+
+        # Do this work in a temp dir, to avoid race conditions.
+        cls.dirChanger = TemporaryDirectoryChanger(filesToMove=TUTORIAL_FILES)
+        cls.dirChanger.__enter__()
+        runTutorialNotebook()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.dirChanger.__exit__(None, None, None)
+
+    def setUp(self):
+        cs = settings.Settings(f"{CASE_TITLE}.yaml")
+        newSettings = {}
+        newSettings["db"] = True
+        newSettings["detailAssemLocationsBOL"] = ["001-001"]
+        newSettings["loadStyle"] = "fromDB"
+        newSettings["reloadDBName"] = pathlib.Path(f"{CASE_TITLE}.h5").absolute()
+        newSettings["startNode"] = 1
+        cs = cs.modified(newSettings=newSettings)
+
+        self.td = TemporaryDirectoryChanger()
+        self.td.__enter__()
+
+        c = case.Case(cs)
+        case2 = c.clone(title="armiRun")
+        settings.setMasterCs(case2.cs)
+        self.o = case2.initializeOperator()
+        self.r = self.o.r
+
+        self.o.getInterface("main").interactBOL()
+
+    def tearDown(self):
+        self.o.getInterface("database").database.close()
+        self.r = None
+        self.o = None
+        self.td.__exit__(None, None, None)
+
+    def test_afterLoadDB(self):
+        r"""
+        Test the afterLoadDB hook works
+
+        Notes
+        -----
+        This test *assumes* the ``afterLoadDB()`` hook takes place at the end of
+        the ``DBI.loadState()`` method. We are not here to test that plugin hooks
+        work, but that this one in particular can affect the ``Reactor`` state.
+        """
+        # Get to the database state at the end of stack of time node 1.
+        dbi = self.o.getInterface("database")
+        dbi.loadState(0, 1)
+
+        bFuels = self.r.core.getBlocks(Flags.FUEL)
+        power = sum([b.p.power for b in bFuels])
+        self.assertEqual(power, 0.0)
+
+        plugin = TestPluginAfterLoadDB()
+        plugin.afterLoadDB(self.r.core, self.o.cs)
+
+        bFuels = self.r.core.getBlocks(Flags.FUEL)
+        powers = [b.p.power for b in bFuels]
+        self.assertEqual(sum(powers), len(powers))
 
 
 if __name__ == "__main__":
