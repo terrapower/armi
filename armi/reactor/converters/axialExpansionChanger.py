f7321919ca4c7f1b4dd47541d581407d51df6dcf
aalberti <c-aalberti@terrapower.com>
2022-07-27 07:29:18 -0700

    cleaning up runLog.warning
    - removing first person and replacing with f string to fix bug (before the componentA and componentB were never going to make the log statement)

--- armi/reactor/converters/axialExpansionChanger.py
+++ armi/reactor/converters/axialExpansionChanger.py
@@ -525,8 +525,9 @@ def _determineLinked(componentA, componentB):
                 linked = True
         except NotImplementedError:
             runLog.warning(
-                "Components {0} and {1} do not have a 'getCircleInnerDiameter' and/or getBoundingCircleOuterDiameter method. "
-                "Instead of crashing and raising an error, I'm just going to assume they aren't linked.",
+                f"Components {componentA} and {componentB} do not have "
+                "a 'getCircleInnerDiameter' and/or getBoundingCircleOuterDiameter method. "
+                "Instead of crashing and raising an error, they are going to be assumed to not be linked.",
                 single=True,
             )
             linked = False
