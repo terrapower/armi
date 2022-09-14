eb2803eec28c685590966ee53375fd8022db1899
Tommy Cisneros <tcisneros@terrapower.com>
2022-09-14 14:19:31 -0700

    black

--- armi/materials/uraniumOxide.py
+++ armi/materials/uraniumOxide.py
@@ -102,10 +102,10 @@ class UraniumOxide(material.FuelMaterial, material.SimpleSolid):
         1.847,
         1.718,
     ]
-    
+
     def __init__(self):
         material.SimpleSolid.__init__(self)
-    
+
     def adjustTD(self, val: float) -> None:
         self.theoreticalDensityFrac = val
 
