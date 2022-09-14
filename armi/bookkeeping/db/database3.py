5c9a99f8e29be6e2a9e90107ceaa98034294aed0
aalberti <c-aalberti@terrapower.com>
2022-09-14 08:17:59 -0700

    putting optional "bp" param back on db load
    - There are some cases where a database may not have blueprints, so a user will need to provide them.
    - However, for snapshot runs, we still do not allow user defined blueprints.

--- armi/bookkeeping/db/database3.py
+++ armi/bookkeeping/db/database3.py
@@ -1079,16 +1079,18 @@ class Database3:
         cycle,
         node,
         cs=None,
+        bp=None,
         statePointName=None,
         allowMissing=False,
         updateGlobalAssemNum=True,
     ):
         """Load a new reactor from (cycle, node).
 
-        Case settings can be provided by the client, or read from the database itself.
+        Case settings and blueprints can be provided by the client, or read from the database itself.
         Providing these from the client could be useful when performing snapshot runs
         or where it is expected to use results from a run using different settings and
-        continue with new settings. Blueprints and geom are read from the database itself.
+        continue with new settings (or if blueprints are not on the database).
+        Geom is read from the database itself.
 
         Parameters
         ----------
@@ -1097,6 +1099,8 @@ class Database3:
         node : int
             time node
         cs : armi.settings.Settings (optional)
+            if not provided one is read from the database
+        bp : armi.reactor.Blueprints (optional)
             if not provided one is read from the database
         statePointName : str
             Optional arbitrary statepoint name (e.g., "special" for "c00n00-special/")
@@ -1117,7 +1121,7 @@ class Database3:
         cs = cs or self.loadCS()
         # apply to avoid defaults in getMasterCs calls
         settings.setMasterCs(cs)
-        bp = self.loadBlueprints()
+        bp = bp or self.loadBlueprints()
 
         h5group = self.h5db[getH5GroupName(cycle, node, statePointName)]
 
