2821e47fbf2f104898273073fe3674fdd9b5fdb5
Michael Jarrett <mjarrett@terrapower.com>
2022-11-28 11:51:48 -0800

    Black formatting.

--- armi/physics/neutronics/parameters.py
+++ armi/physics/neutronics/parameters.py
@@ -130,7 +130,10 @@ def _getNeutronicsBlockParams():
             description="multigroup gamma source",
             location=ParamLocation.AVERAGE,
             saveToDB=True,
-            categories=[parameters.Category.multiGroupQuantities, parameters.Category.gamma],
+            categories=[
+                parameters.Category.multiGroupQuantities,
+                parameters.Category.gamma,
+            ],
             default=None,
         )
 
@@ -538,7 +541,9 @@ def _getNeutronicsBlockParams():
         pb.defParam("arealPd", units="MW/m^2", description="Power divided by XY area")
 
         pb.defParam(
-            "arealPdGamma", units="MW/m^2", description="Areal gamma power density",
+            "arealPdGamma",
+            units="MW/m^2",
+            description="Areal gamma power density",
             categories=[parameters.Category.gamma],
         )
 
@@ -632,7 +637,12 @@ def _getNeutronicsBlockParams():
 
         pb.defParam("ppdens", units="W/cm^3", description="Peak power density")
 
-        pb.defParam("ppdensGamma", units="W/cm^3", description="Peak gamma density", categories=[parameters.Category.gamma])
+        pb.defParam(
+            "ppdensGamma",
+            units="W/cm^3",
+            description="Peak gamma density",
+            categories=[parameters.Category.gamma],
+        )
 
     # rx rate params that are derived during mesh conversion.
     # We'd like all things that can be derived from flux and XS to be
@@ -686,9 +696,19 @@ def _getNeutronicsBlockParams():
 
         pb.defParam("powerDecay", units="W", description="Total decay power")
 
-        pb.defParam("powerGamma", units="W", description="Total gamma power", categories=[parameters.Category.gamma])
+        pb.defParam(
+            "powerGamma",
+            units="W",
+            description="Total gamma power",
+            categories=[parameters.Category.gamma],
+        )
 
-        pb.defParam("powerNeutron", units="W", description="Total neutron power", categories=[parameters.Category.gamma])
+        pb.defParam(
+            "powerNeutron",
+            units="W",
+            description="Total neutron power",
+            categories=[parameters.Category.gamma],
+        )
 
     with pDefs.createBuilder(default=0.0) as pb:
         pb.defParam(
