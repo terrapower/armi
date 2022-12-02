7cf32f5ea7f2158ee34f52815a68df6baa28b970
Michael Jarrett <mjarrett@terrapower.com>
2022-12-01 16:03:42 -0800

    Explain when neutron parameters in gamma category

--- armi/physics/neutronics/parameters.py
+++ armi/physics/neutronics/parameters.py
@@ -294,6 +294,7 @@ def _getNeutronicsBlockParams():
             else:
                 self._p_linPowByPinNeutron = numpy.array(value)
 
+        # gamma category because linPowByPin is only split by neutron/gamma when gamma is activated
         pb.defParam(
             "linPowByPinNeutron",
             setter=linPowByPinNeutron,
@@ -497,6 +498,7 @@ def _getNeutronicsBlockParams():
             categories=[parameters.Category.gamma],
         )
 
+        # gamma category because pdens is only split by neutron/gamma when gamma is activated
         pb.defParam(
             "pdensNeutron",
             units="W/cm^3",
@@ -572,6 +574,7 @@ def _getNeutronicsBlockParams():
             categories=[parameters.Category.gamma],
         )
 
+        # gamma category because power is only split by neutron/gamma when gamma is activated
         pb.defParam(
             "powerNeutron",
             units="W",
