28e4cd3012a65e3cf397b168cfac2fb60c57119b
Michael Jarrett <mjarrett@terrapower.com>
2022-12-20 08:12:34 -0800

    Speed up composite.getNuclideNumberDensities()

--- armi/reactor/composites.py
+++ armi/reactor/composites.py
@@ -1249,12 +1249,14 @@ class ArmiObject(metaclass=CompositeModelType):
             # there are no children so no volume or number density
             return [0.0] * len(nucNames)
 
-        nucDensForEachComp = numpy.array(
-            [
-                [c.getNumberDensity(nuc) for nuc in nucNames]
-                for c in self.iterComponents()
-            ]
-        )  # c x n
+        densListForEachComp = []
+        for c in self.iterComponents():
+            numberDensityDict = c.getNumberDensities()
+            densListForEachComp.append(
+                [numberDensityDict.get(nuc, 0.0) for nuc in nucNames]
+            )
+        nucDensForEachComp = numpy.array(densListForEachComp)  # c x n
+
         return volumes.dot(nucDensForEachComp) / totalVol
 
     def _getNdensHelper(self):
