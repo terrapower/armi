25d83e4d03635dc1d048392e5a10a3c02b4a602d
jstilley <jstilley@terrapower.com>
2023-02-03 15:23:52 -0800

    Backfilling the code coverage

--- armi/materials/tests/test_materials.py
+++ armi/materials/tests/test_materials.py
@@ -910,25 +910,28 @@ class LeadBismuth_TestCase(_Material_Test, unittest.TestCase):
 class Copper_TestCase(_Material_Test, unittest.TestCase):
     MAT_CLASS = materials.Cu
 
-    # def test_setDefaultMassFracs(self):
-    #    cur = self.mat.massFrac
-    #    ref = {"CU63": 0.6915, "CU65": 0.3085}
-    #    self.assertEqual(cur, ref)
+    def test_setDefaultMassFracs(self):
+        cur = self.mat.massFrac
+        ref = {"CU63": 0.6915, "CU65": 0.3085}
+        self.assertEqual(cur, ref)
 
     def test_densityNeverChanges(self):
         for tk in [200.0, 400.0, 800.0, 1111.1]:
             cur = self.mat.density3(tk)
             self.assertAlmostEqual(cur, 8.913, 4)
 
-
-"""
     def test_linearExpansionPercent(self):
         temps = [100.0, 200.0, 600.0]
         expansions = [-0.2955, -0.1500, 0.5326]
         for i, temp in enumerate(temps):
             cur = self.mat.linearExpansionPercent(Tk=temp)
             self.assertAlmostEqual(cur, expansions[i], 4)
-"""
+
+    def test_getChildren(self):
+        self.assertEqual(len(self.mat.getChildren()), 0)
+
+    def test_getChildrenWithFlags(self):
+        self.assertEqual(len(self.mat.getChildrenWithFlags("anything")), 0)
 
 
 class Sulfur_TestCase(_Material_Test, unittest.TestCase):
