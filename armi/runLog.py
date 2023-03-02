8d3c64ff5899f40a03c3ac56ba6412c552eb4a34
Arrielle Opotowsky <c-aopotowsky@terrapower.com>
2023-03-02 17:09:20 -0600

    Only run os.path.exists if logging level is applicable

--- armi/runLog.py
+++ armi/runLog.py
@@ -519,12 +519,14 @@ class RunLogger(logging.Logger):
         In this situation, we do the mangling needed to get the log level to the correct number.
         And we do some custom string manipulation so we can handle de-duplicating warnings.
         """
-        # If the log dir hasn't been created yet, create it.
-        if not os.path.exists(LOG_DIR):
-            createLogDir(LOG_DIR)
-
         # Determine the log level: users can optionally pass in custom strings ("debug")
         msgLevel = msgType if isinstance(msgType, int) else LOG.logLevels[msgType][0]
+
+        # os.path.exists is slow, only call this if writing is needed
+        if self.isEnabledFor(msgLevel):
+            # If the log dir hasn't been created yet, create it.
+            if not os.path.exists(LOG_DIR):
+                createLogDir(LOG_DIR)
 
         # Do the actual logging
         logging.Logger.log(
