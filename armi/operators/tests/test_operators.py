3ff63beabb548bf7b630a388145bdda5e909213d
Michael Jarrett <mjarrett@terrapower.com>
2023-06-08 10:10:58 -0700

    Add unit tests for snapshotRequest.

--- armi/operators/tests/test_operators.py
+++ armi/operators/tests/test_operators.py
@@ -17,6 +17,7 @@
 # pylint: disable=missing-function-docstring,missing-class-docstring,protected-access,invalid-name,no-method-argument,import-outside-toplevel
 import os
 import unittest
+from unittest.mock import patch
 import collections
 
 from armi import settings
@@ -123,17 +124,20 @@ class OperatorTests(unittest.TestCase):
         cs = self.o.setStateToDefault(self.o.cs)
         self.assertEqual(cs[CONF_RUN_TYPE], "Standard")
 
-    def test_snapshotRequest(self):
+    @patch("shutil.copy")
+    @patch("os.listdir")
+    def test_snapshotRequest(self, fakeDirList, fakeCopy):
+        fakeDirList.return_value = ["mccAA.inp"]
         with TemporaryDirectoryChanger():
             with mockRunLogs.BufferLog() as mock:
                 self.o.snapshotRequest(0, 1)
                 self.assertIn("ISOTXS-c0", mock.getStdout())
                 self.assertIn(
-                    "DIF3D input for snapshot from armiRun-flux-c0n1.inp",
+                    "DIF3D input for snapshot: armiRun-flux-c0n1.inp",
                     mock.getStdout(),
                 )
                 self.assertIn(
-                    "DIF3D output for snapshot from armiRun-flux-c0n1.out",
+                    "DIF3D output for snapshot: armiRun-flux-c0n1.out",
                     mock.getStdout(),
                 )
                 self.assertIn("Shuffle logic for snapshot", mock.getStdout())
@@ -142,6 +146,25 @@ class OperatorTests(unittest.TestCase):
                 self.assertIn("Flow history for snapshot", mock.getStdout())
                 self.assertIn("Pressure history for snapshot", mock.getStdout())
             self.assertTrue(os.path.exists("snapShot0_1"))
+
+        with TemporaryDirectoryChanger():
+            with mockRunLogs.BufferLog() as mock:
+                self.o.snapshotRequest(0, 2, iteration=1)
+                self.assertIn("ISOTXS-c0", mock.getStdout())
+                self.assertIn(
+                    "DIF3D input for snapshot: armiRun-flux-c0n2i1.inp",
+                    mock.getStdout(),
+                )
+                self.assertIn(
+                    "DIF3D output for snapshot: armiRun-flux-c0n2i1.out",
+                    mock.getStdout(),
+                )
+                self.assertIn("Shuffle logic for snapshot", mock.getStdout())
+                self.assertIn("Geometry file for snapshot", mock.getStdout())
+                self.assertIn("Loading definition for snapshot", mock.getStdout())
+                self.assertIn("Flow history for snapshot", mock.getStdout())
+                self.assertIn("Pressure history for snapshot", mock.getStdout())
+            self.assertTrue(os.path.exists("snapShot0_2"))
 
 
 class TestTightCoupling(unittest.TestCase):
