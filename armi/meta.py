3e17b8c193efd598f77c38abb3d7a823a98e478d
jstilley <jstilley@terrapower.com>
2023-06-16 10:31:53 -0700

    ARMI version bump to 0.2.8

--- armi/meta.py
+++ armi/meta.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-Metadata describing an ARMI distribution.
-"""
+"""Metadata describing an ARMI distribution."""
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
