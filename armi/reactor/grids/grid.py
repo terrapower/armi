5e76197d01941d35d909179376b656604c254dd8
Drew Johnson <83778714+drewj-usnctech@users.noreply.github.com>
2023-08-25 09:44:19 -0700

    Refactor armi.reactor.grids to wider submodule; Grids as ABC (#1373)
    * Move grids.py to own module armi.reactor.grids
    
    Goal is to introduce separate files for each grid type, but still
    make those importable from armi.reactor.grids
    
    * Add armi.reactor.grids.constants
    
    Intended to share some constants and type hints across the
    `armi.reactor.grids` submodule. Intentionally sparse.
    
    Items will be imported into `armi.reactor.grids` to help with
    back compatibility.
    
    * Add armi.reactor.grids.locations
    
    Provides abstract `LocationBase` as the interface defining base class.
    Concrete subclasses `IndexLocation`, `MultiIndexLocation`, and
    `CoordinateLocation` are also defined.
    
    There is little to no implementation differences in this change. Only
    that `LocationBase` is a proper abstract base class, so subclasses that
    don't override the necessary abstract methods will need to change. Some
    other key changes
    
    - `LocationBase.__eq__` returns `NotImplemented` if the RHS argument
      is not a tuple nor a `LocationBase`
    
    The four main classes defined are imported into `armi.reactor.grids`
    to maintain back compatibility.
    
    * Move reactor/tests/test_grids to grids/tests
    
    * Add armi.reactor.grids.grid for Grid base class
    
    Imported into `armi.reactor.grids` so still back compatible
    
    * Add armi.reactor.grids.cartesian for CartesianGrid
    
    * Add armi.reactor.grids.hexagonal
    
    * Add armi.reactor.grids.thetarz
    
    * Remove six.cPickle -> pickle in test_grids.py
    
    * Add armi.reactor.grids.axial with AxialGrid class
    
    Replaces grids.axialUnitGrid
    
    * Move duplicated grids.addingIsValid to grids.locations
    
    * Refactor Grid to be ABC, intermediate StructuredGrid
    
    Heavier lift, but the classes defined in `armi.reactor.grids`
    follow this inheritance pattern
    
    - `Grid(abc.ABC)` :: abstract interface for _a_ grid, or things that reside in
      space
      - `StructuredGrid(Grid)` :: still abstract, but for things that have
        some structure, known to ARMI as unit steps and bounds
        - `HexGrid(StructuredGrid)`
        - `CartesianGrid(StructuredGrid)
        - `ThetaRZGrid(StructuredGrid)` - though how "structured" is this
          really?
        - `AxialGrid(StructuredGrid)`
    
    In some cases, the shared interface between structured grids falls
    apart. For example, `HexGrid.pitch` returns a single float, while
    `CartesianGrid.pitch` is a 2-ple of float, one for x and one for y.
    And the concept of ring and position appears to be stressed since the
    inception of cartesian grids. But parts of ARMI require an interface
    and so an attempt was made to define that interface.
    
    The decision was made to not have `StructuredGrid` the base class, and
    to introduce `Grid` as the base intentionally. There are cases for some
    multiphysics codes where a grid may still be "structured" but not with
    the level of structure the previous `Grid` implementations expect. One
    example would be an irregular cartesian grid, where the unit cell
    spacing in one or more dimensions may change. So the cell spacing is a
    function of the cell position in that dimension.

---
+++ armi/reactor/grids/grid.py
@@ -0,0 +1,248 @@
+# Copyright 2023 TerraPower, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from abc import ABC, abstractmethod
+from typing import Union, Optional, Hashable, TYPE_CHECKING, Dict, Iterable, Tuple, List
+
+import numpy
+
+from armi.reactor import geometry
+
+from .locations import LocationBase, IndexLocation, IJType, IJKType
+
+if TYPE_CHECKING:
+    from armi.reactor.composites import ArmiObject
+
+
+class Grid(ABC):
+    """Base class that defines the interface for grids
+
+    Most work will be done with structured grids, e.g., hexagonal grid, Cartesian grids,
+    but some physics codes accept irregular or unstructured grids. Consider
+    a Cartesian grid but with variable stepping between cells, where ``dx`` may not be
+    constant.
+
+    So here, we define an interface so things that rely on grids can worry less
+    about how the location data are stored.
+
+    Parameters
+    ----------
+    geomType : str or armi.reactor.geometry.GeomType
+        Underlying geometric representation
+    symmetry : str or armi.reactor.geometry.SymmetryType
+        Symmetry conditions
+    armiObject : optional, armi.reactor.composites.ArmiObject
+        If given, what is this grid attached to or what does it describe?
+        Something like a :class:`armi.reactor.Core`
+
+    """
+
+    _geomType: str
+    _symmetry: str
+    armiObject: Optional["ArmiObject"]
+
+    def __init__(
+        self,
+        geomType: Union[str, geometry.GeomType] = "",
+        symmetry: Union[str, geometry.SymmetryType] = "",
+        armiObject: Optional["ArmiObject"] = None,
+    ):
+        # geometric metadata encapsulated here because it's related to the grid.
+        # They do not impact the grid object itself.
+        # Notice that these are stored using their string representations, rather than
+        # the GridType enum. This avoids the danger of deserializing an enum value from
+        # an old version of the code that may have had different numeric values.
+        self.geomType = geomType
+        self.symmetry = symmetry
+        self.armiObject = armiObject
+        self._backup = None
+
+    @property
+    def geomType(self) -> geometry.GeomType:
+        """Geometric representation"""
+        return geometry.GeomType.fromStr(self._geomType)
+
+    @geomType.setter
+    def geomType(self, geomType: Union[str, geometry.GeomType]):
+        if geomType:
+            self._geomType = str(geometry.GeomType.fromAny(geomType))
+        else:
+            self._geomType = ""
+
+    @property
+    def symmetry(self) -> str:
+        """Symmetry applied to the grid"""
+        return geometry.SymmetryType.fromStr(self._symmetry)
+
+    @symmetry.setter
+    def symmetry(self, symmetry: Union[str, geometry.SymmetryType]):
+        if symmetry:
+            self._symmetry = str(geometry.SymmetryType.fromAny(symmetry))
+        else:
+            self._symmetry = ""
+
+    def __getstate__(self) -> Dict:
+        """
+        Pickling removes reference to ``armiObject``
+
+        Removing the ``armiObject`` allows us to pickle an assembly without pickling
+        the entire reactor. An ``Assembly.spatialLocator.grid.armiObject`` is the
+        reactor, by removing the link here, we still have spatial orientation, but are
+        not required to pickle the entire reactor to pickle an assembly.
+
+        This relies on the ``armiObject.__setstate__`` to assign itself.
+        """
+        state = self.__dict__.copy()
+        state["armiObject"] = None
+
+        return state
+
+    def __setstate__(self, state: Dict):
+        """
+        Pickling removes reference to ``armiObject``
+
+        This relies on the ``ArmiObject.__setstate__`` to assign itself.
+        """
+        self.__dict__.update(state)
+
+        for _index, locator in self.items():
+            locator._grid = self
+
+    @property
+    @abstractmethod
+    def isAxialOnly(self) -> bool:
+        """Indicate to parts of ARMI if this Grid handles only axial cells"""
+
+    @abstractmethod
+    def __len__(self) -> int:
+        """Number of items in the grid"""
+
+    @abstractmethod
+    def items(self) -> Iterable[Tuple[IJKType, IndexLocation]]:
+        """Return list of ((i, j, k), IndexLocation) tuples."""
+
+    @abstractmethod
+    def locatorInDomain(
+        self, locator: LocationBase, symmetryOverlap: Optional[bool] = False
+    ) -> bool:
+        """
+        Return whether the passed locator is in the domain represented by the Grid.
+
+        For instance, if we have a 1/3rd core hex grid, this would return False for
+        locators that are outside of the first third of the grid.
+
+        Parameters
+        ----------
+        locator : LocationBase
+            The location to test
+        symmetryOverlap : bool, optional
+            Whether grid locations along the symmetry line should be considered "in the
+            represented domain". This can be useful when assemblies are split along the
+            domain boundary, with fractions of the assembly on either side.
+
+        Returns
+        -------
+        bool
+            If the given locator is within the given grid
+        """
+
+    @abstractmethod
+    def getSymmetricEquivalents(self, indices: IJType) -> List[IJType]:
+        """
+        Return a list of grid indices that contain matching contents based on symmetry.
+
+        The length of the list will depend on the type of symmetry being used, and
+        potentially the location of the requested indices. E.g.,
+        third-core will return the two sets of indices at the matching location in the
+        other two thirds of the grid, unless it is the central location, in which case
+        no indices will be returned.
+        """
+
+    @abstractmethod
+    def overlapsWhichSymmetryLine(self, indices: IJType) -> Optional[int]:
+        """Return lines of symmetry position at a given index can be found
+
+        Parameters
+        ----------
+        indices : tuple of [int, int]
+            Indices for the requested object
+
+        Returns
+        -------
+        None or int
+            None if not line of symmetry goes through the object at the
+            requested index. Otherwise, some grid constants like ``BOUNDARY_CENTER``
+            will be returned.
+
+        """
+
+    @abstractmethod
+    def getCoordinates(
+        self,
+        indices: Union[IJKType, List[IJKType]],
+        nativeCoords: bool = False,
+    ) -> numpy.ndarray:
+        pass
+
+    @abstractmethod
+    def backUp(self):
+        """Subclasses should modify the internal backup variable"""
+
+    @abstractmethod
+    def restoreBackup(self):
+        """Restore state from backup"""
+
+    @abstractmethod
+    def getCellBase(self, indices: IJKType) -> numpy.ndarray:
+        """Return the lower left case of this cell in cm"""
+
+    @abstractmethod
+    def getCellTop(self, indices: IJKType) -> numpy.ndarray:
+        """Get the upper right of this cell in cm"""
+
+    @staticmethod
+    def getLabel(indices):
+        """
+        Get a string label from a 0-based spatial locator.
+
+        Returns a string representing i, j, and k indices of the locator
+        """
+        i, j = indices[:2]
+        label = f"{i:03d}-{j:03d}"
+        if len(indices) == 3:
+            label += f"-{indices[2]:03d}"
+        return label
+
+    @abstractmethod
+    def reduce(self) -> Tuple[Hashable, ...]:
+        """
+        Return the set of arguments used to create this Grid.
+
+        This is very much like the argument tuple from ``__reduce__``, but we do not
+        implement ``__reduce__`` for real, because we are generally happy with
+        ``__getstate__`` and ``__setstate__`` for pickling purposes. However, getting
+        these arguments to ``__init__`` is useful for storing Grids to the database, as
+        they are more stable (less likely to change) than the actual internal state of
+        the objects.
+
+        The return value should be hashable, such that a set of these can be created.
+
+        The return type should be symmetric such that a similar grid can be
+        created just with the outputs of ``Grid.reduce``, e.g.,
+        ``type(grid)(*grid.reduce())``
+
+        Notes
+        -----
+        For consistency, the second to last argument **must** be the geomType
+
+        """
