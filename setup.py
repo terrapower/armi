673f279a38924ce948a6e1f192600d2ea585e9dc
jstilley <jstilley@terrapower.com>
2023-09-15 08:48:54 -0700

    Supporting the new matplotlib API change

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         "future",
         "h5py>=3.0",
         "htmltree",
-        "matplotlib<3.8",
+        "matplotlib",
         "numpy>=1.21,<=1.23.5",
         "ordered-set",
         "pillow",
