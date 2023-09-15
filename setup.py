ea50b2f83dc37f78578644fac18e56e6eee2d0f2
jstilley <jstilley@terrapower.com>
2023-09-15 08:36:15 -0700

    Version pinning matplotlib

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         "future",
         "h5py>=3.0",
         "htmltree",
-        "matplotlib",
+        "matplotlib<3.8",
         "numpy>=1.21,<=1.23.5",
         "ordered-set",
         "pillow",
