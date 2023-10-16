acd932bc13ebb161a0a3f590e50f5e39ebd862a6
aalberti <c-aalberti@terrapower.com>
2023-10-16 16:13:10 -0700

    fix license headers

--- armi/reactor/converters/axialExpansion/__init__.py
+++ armi/reactor/converters/axialExpansion/__init__.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Particles! Expand axially!."""
 
 from armi.materials import material
