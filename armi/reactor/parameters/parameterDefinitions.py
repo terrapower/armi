e1aa81545fbfaf0ed61004a4c589a4066c8410b5
Brandon LaFleur <brandon.lafleur@ge.com>
2023-10-31 09:16:22 -0400

    Remove "impl" tag in doc string

--- armi/reactor/parameters/parameterDefinitions.py
+++ armi/reactor/parameters/parameterDefinitions.py
@@ -575,10 +575,6 @@ class ParameterDefinitionCollection:
         """
         Get a list of acceptable parameters to store to the database for a level of the data model.
 
-        .. impl:: Implementation for restricting some parameters from being written to the database.
-            :id: I_ARMI_RESTRICT_DB_WRITE
-            :links: R_ARMI_RESTRICT_DB_WRITE
-
         Parameters
         ----------
         assignedMask : int
