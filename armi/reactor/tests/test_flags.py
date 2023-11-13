2af81e9946bc77b869151fb2d294277378fe8ca8
bdlafleur <brandon.lafleur@ge.com>
2023-11-13 13:57:24 -0500

    Adding Flag/Flags impl and test tags (#1470)

--- armi/reactor/tests/test_flags.py
+++ armi/reactor/tests/test_flags.py
@@ -26,9 +26,17 @@ class TestFlags(unittest.TestCase):
         self._help_fromString(flags.Flags.fromStringIgnoreErrors)
         self.assertEqual(flags.Flags.fromStringIgnoreErrors("invalid"), flags.Flags(0))
 
-    def test_toString(self):
+    def test_flagsToAndFromString(self):
+        """
+        Convert flag to and from string for serialization.
+
+        .. test:: Convert flag to a string
+            :id: T_ARMI_FLAG_TO_STR
+            :tests: R_ARMI_FLAG_TO_STR
+        """
         f = flags.Flags.FUEL
         self.assertEqual(flags.Flags.toString(f), "FUEL")
+        self.assertEqual(f, flags.Flags.fromString("FUEL"))
 
     def test_fromStringStrict(self):
         self._help_fromString(flags.Flags.fromString)
