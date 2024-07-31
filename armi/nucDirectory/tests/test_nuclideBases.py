2546a3140f2ba01f165e8bdfe02eb625b4477276
jstilley <jstilley@terrapower.com>
2024-07-31 14:07:42 -0700

    Fixing a test

--- armi/nucDirectory/tests/test_nuclideBases.py
+++ armi/nucDirectory/tests/test_nuclideBases.py
@@ -127,12 +127,13 @@ class TestNuclide(unittest.TestCase):
             atomicMass = 0.0
             for natIso in natNuk.getNaturalIsotopics():
                 atomicMass += natIso.abundance * natIso.weight
-            self.assertEqual(
+            self.assertAlmostEqual(
                 atomicMass,
                 natNuk.weight,
                 "{} weight is {}, expected {}".format(
                     natNuk, natNuk.weight, atomicMass
                 ),
+                delta=0.000001,
             )
 
     def test_nucBases_labelAndNameCollsionsAreForSameNuclide(self):
