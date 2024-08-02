93a379bd5c7bad561768ae080fd81860d33b84ab
jstilley <jstilley@terrapower.com>
2024-08-02 08:49:02 -0700

    Adding tabulate unit tests

--- armi/utils/tests/test_tabulate.py
+++ armi/utils/tests/test_tabulate.py
@@ -16,6 +16,7 @@
 #       This was originally https://github.com/astanin/python-tabulate
 
 """Tests for tabulate."""
+import numpy  # TODO: as np
 import unittest
 
 from armi.utils.tabulate import tabulate, tabulate_formats
@@ -36,4 +37,180 @@ class TestTabulate(unittest.TestCase):
             ["-  -  -  -  -", "0  1  2  3  4", "5  4  3  2  1", "-  -  -  -  -"]
         )
         result = tabulate(ii)
+        self.assertEqual(expected, result)
+
+    def test_iterable_of_iterables_headers(self):
+        """Input: an interable of iterables with headers."""
+        ii = iter(map(lambda x: iter(x), [range(5), range(5, 0, -1)]))
+        expected = "\n".join(
+            [
+                "  a    b    c    d    e",
+                "---  ---  ---  ---  ---",
+                "  0    1    2    3    4",
+                "  5    4    3    2    1",
+            ]
+        )
+        result = tabulate(ii, "abcde")
+        self.assertEqual(expected, result)
+
+    def test_iterable_of_iterables_firstrow(self):
+        """Input: an interable of iterables with the first row as headers."""
+        ii = iter(map(lambda x: iter(x), ["abcde", range(5), range(5, 0, -1)]))
+        expected = "\n".join(
+            [
+                "  a    b    c    d    e",
+                "---  ---  ---  ---  ---",
+                "  0    1    2    3    4",
+                "  5    4    3    2    1",
+            ]
+        )
+        result = tabulate(ii, "firstrow")
+        self.assertEqual(expected, result)
+
+    def test_list_of_lists(self):
+        """Input: a list of lists with headers."""
+        ll = [["a", "one", 1], ["b", "two", None]]
+        expected = "\n".join(
+            [
+                "    string      number",
+                "--  --------  --------",
+                "a   one              1",
+                "b   two",
+            ]
+        )
+        result = tabulate(ll, headers=["string", "number"])
+        self.assertEqual(expected, result)
+
+    def test_list_of_lists_firstrow(self):
+        """Input: a list of lists with the first row as headers."""
+        ll = [["string", "number"], ["a", "one", 1], ["b", "two", None]]
+        expected = "\n".join(
+            [
+                "    string      number",
+                "--  --------  --------",
+                "a   one              1",
+                "b   two",
+            ]
+        )
+        result = tabulate(ll, headers="firstrow")
+        self.assertEqual(expected, result)
+
+    def test_list_of_lists_keys(self):
+        """Input: a list of lists with column indices as headers."""
+        ll = [["a", "one", 1], ["b", "two", None]]
+        expected = "\n".join(
+            ["0    1      2", "---  ---  ---", "a    one    1", "b    two"]
+        )
+        result = tabulate(ll, headers="keys")
+        self.assertEqual(expected, result)
+
+    def testDictLike(self):
+        """Input: a dict of iterables with keys as headers."""
+        # columns should be padded with None, keys should be used as headers
+        dd = {"a": range(3), "b": range(101, 105)}
+        # keys' order (hence columns' order) is not deterministic in Python 3
+        # => we have to consider both possible results as valid
+        expected1 = "\n".join(
+            ["  a    b", "---  ---", "  0  101", "  1  102", "  2  103", "     104"]
+        )
+        result = tabulate(dd, "keys")
+        self.assertEqual(result, expected1)
+
+    def test_numpy_2d(self):
+        """Input: a 2D NumPy array with headers."""
+        na = (numpy.arange(1, 10, dtype=numpy.float32).reshape((3, 3)) ** 3) * 0.5
+        expected = "\n".join(
+            [
+                "    a      b      c",
+                "-----  -----  -----",
+                "  0.5    4     13.5",
+                " 32     62.5  108",
+                "171.5  256    364.5",
+            ]
+        )
+        result = tabulate(na, ["a", "b", "c"])
+        self.assertEqual(expected, result)
+
+    def test_numpy_2d_firstrow(self):
+        """Input: a 2D NumPy array with the first row as headers."""
+        na = numpy.arange(1, 10, dtype=numpy.int32).reshape((3, 3)) ** 3
+        expected = "\n".join(
+            ["  1    8    27", "---  ---  ----", " 64  125   216", "343  512   729"]
+        )
+        result = tabulate(na, headers="firstrow")
+        self.assertEqual(expected, result)
+
+    def test_numpy_2d_keys(self):
+        """Input: a 2D NumPy array with column indices as headers."""
+        na = (numpy.arange(1, 10, dtype=numpy.float32).reshape((3, 3)) ** 3) * 0.5
+        expected = "\n".join(
+            [
+                "    0      1      2",
+                "-----  -----  -----",
+                "  0.5    4     13.5",
+                " 32     62.5  108",
+                "171.5  256    364.5",
+            ]
+        )
+        result = tabulate(na, headers="keys")
+        self.assertEqual(expected, result)
+
+    def test_numpy_record_array(self):
+        """Input: a 2D NumPy record array without header."""
+        na = numpy.asarray(
+            [("Alice", 23, 169.5), ("Bob", 27, 175.0)],
+            dtype={
+                "names": ["name", "age", "height"],
+                "formats": ["a32", "uint8", "float32"],
+            },
+        )
+        expected = "\n".join(
+            [
+                "-----  --  -----",
+                "Alice  23  169.5",
+                "Bob    27  175",
+                "-----  --  -----",
+            ]
+        )
+        result = tabulate(na)
+        self.assertEqual(expected, result)
+
+    def test_numpy_record_array_keys(self):
+        """Input: a 2D NumPy record array with column names as headers."""
+        na = numpy.asarray(
+            [("Alice", 23, 169.5), ("Bob", 27, 175.0)],
+            dtype={
+                "names": ["name", "age", "height"],
+                "formats": ["a32", "uint8", "float32"],
+            },
+        )
+        expected = "\n".join(
+            [
+                "name      age    height",
+                "------  -----  --------",
+                "Alice      23     169.5",
+                "Bob        27     175",
+            ]
+        )
+        result = tabulate(na, headers="keys")
+        self.assertEqual(expected, result)
+
+    def test_numpy_record_array_headers(self):
+        """Input: a 2D NumPy record array with user-supplied headers."""
+        na = numpy.asarray(
+            [("Alice", 23, 169.5), ("Bob", 27, 175.0)],
+            dtype={
+                "names": ["name", "age", "height"],
+                "formats": ["a32", "uint8", "float32"],
+            },
+        )
+        expected = "\n".join(
+            [
+                "person      years     cm",
+                "--------  -------  -----",
+                "Alice          23  169.5",
+                "Bob            27  175",
+            ]
+        )
+        result = tabulate(na, headers=["person", "years", "cm"])
         self.assertEqual(expected, result)
