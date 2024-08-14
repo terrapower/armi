d0824b5d4da1ba1228b04b621082f5b72d0027e1
Arrielle Opotowsky <aopotowsky@terrapower.com>
2024-08-14 06:03:04 +0000

    ruff it

--- armi/reactor/grids/axial.py
+++ armi/reactor/grids/axial.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import List, Optional, TYPE_CHECKING, NoReturn
-import warnings
 
 import numpy
 
