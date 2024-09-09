4a300c59fbde3bed3d324309380dc15804ff724b
Andrew Johnson <anjohnson@terrapower.com>
2024-09-09 10:25:36 -0700

    Fix an incorrect test docstring

--- armi/materials/tests/test_materials.py
+++ armi/materials/tests/test_materials.py
@@ -46,7 +46,7 @@ class _Material_Test:
         )
 
     def test_density(self):
-        """Test that all materials produce a zero density from density."""
+        """Test that all materials produce a non-zero density from density."""
         self.assertNotEqual(self.mat.density(500), 0)
 
     def test_TD(self):
