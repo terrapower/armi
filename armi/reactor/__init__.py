190f8157bdbb4e66b73951021bb24055ff864393
Andrew Johnson <anjohnson@terrapower.com>
2024-09-09 14:21:16 -0700

    Remove self from staticmethod hookspec defineSystemBuilders
    Was in the area so make a minor cleanup

--- armi/reactor/__init__.py
+++ armi/reactor/__init__.py
@@ -94,9 +94,9 @@ class ReactorPlugin(plugins.ArmiPlugin):
 
     @staticmethod
     @plugins.HOOKIMPL(trylast=True)
-    def defineSystemBuilders() -> Dict[
-        str, Callable[[str], Union["Core", "SpentFuelPool"]]
-    ]:
+    def defineSystemBuilders() -> (
+        Dict[str, Callable[[str], Union["Core", "SpentFuelPool"]]]
+    ):
         from armi.reactor.reactors import Core
         from armi.reactor.assemblyLists import SpentFuelPool
 
