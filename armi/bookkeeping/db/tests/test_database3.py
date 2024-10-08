9e41e7f3deb55fb90e637e4d01f17066ab2622af
Michael Jarrett <mjarrett@terrapower.com>
2024-10-08 11:22:37 -0700

    Address review comment and simplify unit test.

--- armi/bookkeeping/db/tests/test_database3.py
+++ armi/bookkeeping/db/tests/test_database3.py
@@ -843,21 +843,18 @@ grids:
             self.assertEqual(b1.p.power, 12345.6)
 
     def test_badData(self):
-        # put some data in the DB, for timenode 1
-        self.r.p.timeNode = 1
-        self.r.core.p.keff = 1.01
+        # create a DB to be modified
         self.db.writeToDB(self.r)
-        # close the DB
         self.db.close()
 
         # modify the HDF5 file to corrupt a dataset
         with h5py.File(self.db.fileName, "r+") as hf:
-            circleGroup = hf["c00n01"]["Circle"]
+            circleGroup = hf["c00n00"]["Circle"]
             circleMass = np.array(circleGroup["massHmBOL"][()])
             badData = circleMass[:-1]
             del circleGroup["massHmBOL"]
             circleGroup.create_dataset("massHmBOL", data=badData)
 
         with self.assertRaises(ValueError):
-            with database3.Database3(self._testMethodName + ".h5", "r") as db:
-                _r = db.load(0, 1, allowMissing=True)
+            with database3.Database3(self.db.fileName, "r") as db:
+                _r = db.load(0, 0, allowMissing=True)
