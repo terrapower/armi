3520838ea5fdb699ab72e36401d46c8790885a4e
Andrew Johnson <anjohnson@terrapower.com>
2024-10-11 15:45:16 -0700

    Expand test_oppositeRotation to all six configurations

--- armi/physics/fuelCycle/tests/test_assemblyRotationAlgorithms.py
+++ armi/physics/fuelCycle/tests/test_assemblyRotationAlgorithms.py
@@ -19,12 +19,72 @@ Notes
 These algorithms are defined in assemblyRotationAlgorithms.py, but they are used in:
 ``FuelHandler.outage()``.
 """
+from unittest import mock
+import numpy as np
+
 from armi.physics.fuelCycle import assemblyRotationAlgorithms as rotAlgos
+from armi.physics.fuelCycle.hexAssemblyFuelMgmtUtils import (
+    getOptimalAssemblyOrientation,
+)
 from armi.physics.fuelCycle import fuelHandlers
 from armi.physics.fuelCycle.settings import CONF_ASSEM_ROTATION_STATIONARY
 from armi.physics.fuelCycle.tests.test_fuelHandlers import addSomeDetailAssemblies
 from armi.physics.fuelCycle.tests.test_fuelHandlers import FuelHandlerTestHelper
 from armi.reactor.flags import Flags
+
+
+class TestOptimalAssemblyRotation(FuelHandlerTestHelper):
+    N_PINS = 271
+
+    def prepBlocks(self, percentBuMaxPinLocation: int, pinPowers: list[float]):
+        """Assign basic information to the blocks so we can rotate them.
+
+        Parameters
+        ----------
+        percentBuMaxPinLocation : int
+            ARMI pin number, 1-indexed, for the pin with the highest burnup
+        pinPowers : list[float]
+            Powers in each pin
+        """
+        for b in self.assembly.getBlocks(Flags.FUEL):
+            b.p.percentBuMax = 5
+            # Fake enough behavior so we can make a spatial grid
+            b.getPinPitch = mock.Mock(return_value=1.1)
+            b.autoCreateSpatialGrids()
+            b.p.percentBuMaxPinLocation = percentBuMaxPinLocation
+            b.p.linPowByPin = pinPowers
+
+
+    def test_flatPowerNoRotation(self):
+        """If all pin powers are identical, no rotation is suggested."""
+        powers = np.ones(self.N_PINS)
+        # Identical powers but _some_ non-central "max" burnup pin
+        self.prepBlocks(percentBuMaxPinLocation=8, pinPowers=powers)
+        rot = getOptimalAssemblyOrientation(self.assembly, self.assembly)
+        self.assertEqual(rot, 0)
+
+    def test_maxBurnupAtCenterNoRotation(self):
+        """If max burnup pin is at the center, no rotation is suggested."""
+        # Fake a higher power towards the center
+        powers = np.arange(self.N_PINS)[::-1]
+        self.prepBlocks(percentBuMaxPinLocation=1, pinPowers=powers)
+        rot = getOptimalAssemblyOrientation(self.assembly, self.assembly)
+        self.assertEqual(rot, 0)
+
+    def test_oppositeRotation(self):
+        """Test a 180 degree rotation is suggested when the max burnup pin is opposite the lowest power pin.
+
+        Use the second ring of the hexagon because it's easier to write out pin locations
+        and check work.
+        """
+        for startPin, oppositePin in ((2, 5), (3, 6), (4, 7), (5, 2), (6, 3), (7, 4)):
+            powers = np.ones(self.N_PINS)
+            powers[startPin - 1] *= 2
+            powers[oppositePin - 1] = 0
+            self.prepBlocks(percentBuMaxPinLocation=startPin, pinPowers=powers)
+            rot = getOptimalAssemblyOrientation(self.assembly, self.assembly)
+            # 180 degrees is three 60 degree rotations
+            self.assertEqual(rot, 3, msg=f"{startPin=} :: {oppositePin=}")
 
 
 class TestFuelHandlerMgmtTools(FuelHandlerTestHelper):
