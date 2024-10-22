1b182041bee5dbaecb562e3db6c3e686482678e4
Andrew Johnson <anjohnson@terrapower.com>
2024-10-22 15:22:33 -0700

    Check against Component.p.pinPercentBu for burnup equalizing rotation

--- armi/physics/fuelCycle/tests/test_utils.py
+++ armi/physics/fuelCycle/tests/test_utils.py
@@ -19,7 +19,7 @@ import numpy as np
 from armi.reactor.blocks import Block
 from armi.reactor.components import Circle
 from armi.reactor.flags import Flags
-from armi.reactor.grids import IndexLocation
+from armi.reactor.grids import IndexLocation, MultiIndexLocation
 from armi.physics.fuelCycle import utils
 
 
@@ -54,6 +54,35 @@ class FuelCycleUtilsTests(TestCase):
         actual = utils.maxBurnupFuelPinLocation(self.block)
         self.assertIs(actual, expected)
 
+    def test_maxBurnupLocationFromComponents(self):
+        """Test that the ``Component.p.pinPercentBu`` parameter can reveal max burnup location."""
+        self.fuel.spatialLocator = MultiIndexLocation(None)
+        locations = []
+        for i in range(self.N_PINS):
+            loc = IndexLocation(i, 0, 0, None)
+            self.fuel.spatialLocator.append(loc)
+            locations.append(loc)
+        self.fuel.p.pinPercentBu = np.ones(self.N_PINS, dtype=float)
+
+        # Pick an arbitrary index for the pin with the most burnup
+        maxBuIndex = self.N_PINS // 3
+        self.fuel.p.pinPercentBu[maxBuIndex] *= 2
+        expectedLoc = locations[maxBuIndex]
+        actual = utils.maxBurnupFuelPinLocation(self.block)
+        self.assertEqual(actual, expectedLoc)
+
+    def test_singleLocatorWithBurnup(self):
+        """Test that a single component with burnup can be used to find the highest burnup."""
+        freeComp = Circle(
+            "free fuel", material="UO2", Tinput=200, Thot=200, id=0, od=1, mult=1
+        )
+        freeComp.spatialLocator = IndexLocation(2, 4, 0, None)
+        freeComp.p.pinPercentBu = [
+            0.01,
+        ]
+        loc = utils.getMaxBurnupLocationFromChildren([freeComp])
+        self.assertIs(loc, freeComp.spatialLocator)
+
     def test_assemblyHasPinPower(self):
         """Test the ability to check if an assembly has fuel pin powers."""
         fakeAssem = [self.block]
@@ -71,3 +100,28 @@ class FuelCycleUtilsTests(TestCase):
         # Yes fuel blocks, yes pin power assigned but all zeros => no pin powers
         self.block.p.linPowByPin = np.zeros(self.N_PINS, dtype=float)
         self.assertFalse(utils.assemblyHasFuelPinPowers(fakeAssem))
+
+    def test_assemblyHasPinBurnups(self):
+        """Test the ability to check if an assembly has fuel pin burnup."""
+        fakeAssem = [self.block]
+        # No fuel components => no assembly burnups
+        self.assertFalse(self.block.getChildrenWithFlags(Flags.FUEL))
+        self.assertFalse(utils.assemblyHasFuelPinBurnup(fakeAssem))
+        # No fuel with burnup => no assembly burnups
+        self.block.p.flags |= Flags.FUEL
+        self.fuel.p.flags |= Flags.FUEL
+        self.assertFalse(utils.assemblyHasFuelPinBurnup(fakeAssem))
+        # Fuel pin has burnup => yes assembly burnup
+        self.fuel.p.pinPercentBu = np.arange(self.N_PINS, dtype=float)
+        self.assertTrue(utils.assemblyHasFuelPinBurnup(fakeAssem))
+        # Fuel pin has empty burnup => no assembly burnup
+        self.fuel.p.pinPercentBu = np.zeros(self.N_PINS)
+        self.assertFalse(utils.assemblyHasFuelPinBurnup(fakeAssem))
+        # Yes burnup but no fuel flags => no assembly burnup
+        self.fuel.p.flags ^= Flags.FUEL
+        self.assertFalse(self.fuel.hasFlags(Flags.FUEL))
+        self.fuel.p.pinPercentBu = np.arange(self.N_PINS, dtype=float)
+        self.assertFalse(utils.assemblyHasFuelPinBurnup(fakeAssem))
+        # No pin component burnup, but a pin burnup location parameter => yes assembly burnup
+        self.block.p.percentBuMaxPinLocation = 3
+        self.assertTrue(utils.assemblyHasFuelPinBurnup(fakeAssem))
