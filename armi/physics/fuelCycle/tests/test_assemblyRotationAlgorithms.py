bb1a8da3c6a722f0b14a5bab7a44964d153c5fad
Andrew Johnson <anjohnson@terrapower.com>
2024-11-05 16:15:28 -0800

    Refactor burnup rotation test with a shuffled queue case
    Adds a test where we involve three assemblies `A -> B -> C` mimicing a
    real outage event. This test is important to make sure we aren't
    rotating assemblies that we need later.
    
    Heavy refactor to make the tests a bit easier to read (hopefully) by
    adding a few things
    
    1. PinLocations enumeration to de-mystify some magic pin locations.
    2. Helper methods for getting burnup vector with some known max location
       and pin power vector with known min locaiton
    3. Comparison of the rotation called on an assembly with the integer
       number of rotations we know from the test

--- armi/physics/fuelCycle/tests/test_assemblyRotationAlgorithms.py
+++ armi/physics/fuelCycle/tests/test_assemblyRotationAlgorithms.py
@@ -20,11 +20,15 @@ These algorithms are defined in assemblyRotationAlgorithms.py, but they are used
 ``FuelHandler.outage()``.
 """
 import copy
-from unittest import mock
+import enum
+import math
+import typing
+from unittest import mock, TestCase
 
 import numpy as np
 
 from armi.reactor.assemblies import HexAssembly
+from armi.reactor.blocks import HexBlock
 from armi.physics.fuelCycle import assemblyRotationAlgorithms as rotAlgos
 from armi.physics.fuelCycle.hexAssemblyFuelMgmtUtils import (
     getOptimalAssemblyOrientation,
@@ -32,59 +36,111 @@ from armi.physics.fuelCycle.hexAssemblyFuelMgmtUtils import (
 from armi.physics.fuelCycle import fuelHandlers
 from armi.physics.fuelCycle.settings import CONF_ASSEM_ROTATION_STATIONARY
 from armi.physics.fuelCycle.tests.test_fuelHandlers import addSomeDetailAssemblies
-from armi.physics.fuelCycle.tests.test_fuelHandlers import FuelHandlerTestHelper
 from armi.reactor.flags import Flags
+from armi.reactor.tests import test_reactors
 
 
-class FullImplFuelHandler(fuelHandlers.FuelHandler):
+class MockFuelHandler(fuelHandlers.FuelHandler):
     """Implements the entire interface but with empty methods."""
 
     def chooseSwaps(self, *args, **kwargs):
         pass
 
 
-class TestOptimalAssemblyRotation(FuelHandlerTestHelper):
-    N_PINS = 271
+class PinLocations(enum.IntEnum):
+    """Zero-indexed locations for specific points of interest.
+
+    If a data vector has an entry to all ``self.N_PINS=169`` pins in the test model,
+    then ``data[PIN_LOCATIONS.UPPER_RIGHT_VERTEX]`` will access the data for the pin
+    along the upper right 60 symmetry line. Since we're dealing with rotations here, it
+    does not need to literally be the pin at the vertex. Just along the symmetry line
+    to help explain tests.
+
+    The use case here is setting the pin or burnup array to be a constant value, but
+    using a single max or minimum value to determine rotation.
+    """
+
+    CENTER = 0
+    UPPER_RIGHT_VERTEX = 1
+    UPPER_LEFT_VERTEX = 2
+    DUE_LEFT_VERTEX = 3
+    LOWER_LEFT_VERTEX = 4
+    LOWER_RIGHT_VERTEX = 5
+    DUE_RIGHT_VERTEX = 6
+
+
+class ShuffleAndRotateTestHelper(TestCase):
+    """Fixture class to assist in testing rotation of assemblies via the fuel handler."""
+
+    N_PINS = 169
+
+    def setUp(self):
+        self.o, self.r = test_reactors.loadTestReactor()
+        self.r.core.locateAllAssemblies()
 
     @staticmethod
-    def prepShuffledAssembly(a: HexAssembly, percentBuMaxPinLocation: int):
+    def ensureBlockHasSpatialGrid(b: HexBlock):
+        """If ``b`` does not have a spatial grid, auto create one."""
+        if b.spatialGrid is None:
+            b.getPinPitch = mock.Mock(return_value=1.1)
+            b.autoCreateSpatialGrids()
+
+    def setAssemblyPinBurnups(self, a: HexAssembly, burnups: np.ndarray):
         """Prepare the assembly that will be shuffled and rotated."""
         for b in a.getChildrenWithFlags(Flags.FUEL):
-            # Fake enough information to build a spatial grid
-            b.getPinPitch = mock.Mock(return_value=1.1)
-            b.autoCreateSpatialGrids()
+            self.ensureBlockHasSpatialGrid(b)
             for c in b.getChildrenWithFlags(Flags.FUEL):
-                mult = c.getDimension("mult")
-                if mult <= percentBuMaxPinLocation:
-                    continue
-                burnups = np.ones(mult, dtype=float)
-                burnups[percentBuMaxPinLocation] *= 2
                 c.p.pinPercentBu = burnups
 
-    @staticmethod
-    def prepPreviousAssembly(a: HexAssembly, pinPowers: list[float]):
+    def setAssemblyPinPowers(self, a: HexAssembly, pinPowers: np.ndarray):
         """Prep the assembly that existed at the site a shuffled assembly will occupy."""
         for b in a.getChildrenWithFlags(Flags.FUEL):
-            # Fake enough information to build a spatial grid
-            b.getPinPitch = mock.Mock(return_value=1.1)
-            b.autoCreateSpatialGrids()
+            self.ensureBlockHasSpatialGrid(b)
             b.p.linPowByPin = pinPowers
+
+    def powerWithMinValue(self, minIndex: int) -> np.ndarray:
+        """Create a vector of pin powers with a minimum value at a given index."""
+        data = np.ones(self.N_PINS)
+        data[minIndex] = 0
+        return data
+
+    def burnupWithMaxValue(self, maxIndex: int) -> np.ndarray:
+        """Create a vector of pin burnups with a maximum value at a given index."""
+        data = np.zeros(self.N_PINS)
+        data[maxIndex] = 50
+        return data
+
+    def compareMockedToExpectedRotation(
+        self, nRotations: int, mRotate: mock.Mock, msg: typing.Optional[str] = None
+    ):
+        """Helper function to check the mocked rotate and compare against expected rotation."""
+        expectedRadians = nRotations * math.pi / 3
+        (actualRadians,) = mRotate.call_args.args
+        self.assertAlmostEqual(actualRadians, expectedRadians, msg=msg)
+
+
+class TestOptimalAssemblyRotation(ShuffleAndRotateTestHelper):
+    """Test the burnup dependent assembly rotation methods."""
+
+    def setUp(self):
+        super().setUp()
+        self.assembly: HexAssembly = self.r.core.getFirstAssembly(Flags.FUEL)
 
     def test_flatPowerNoRotation(self):
         """If all pin powers are identical, no rotation is suggested."""
-        powers = np.ones(self.N_PINS)
-        # Identical powers but _some_ non-central "max" burnup pin
-        self.prepShuffledAssembly(self.assembly, percentBuMaxPinLocation=8)
-        self.prepPreviousAssembly(self.assembly, powers)
+        burnups = self.burnupWithMaxValue(PinLocations.UPPER_LEFT_VERTEX)
+        powers = np.ones_like(burnups)
+        self.setAssemblyPinBurnups(self.assembly, burnups)
+        self.setAssemblyPinPowers(self.assembly, powers)
         rot = getOptimalAssemblyOrientation(self.assembly, self.assembly)
         self.assertEqual(rot, 0)
 
     def test_maxBurnupAtCenterNoRotation(self):
         """If max burnup pin is at the center, no rotation is suggested."""
-        # Fake a higher power towards the center
-        powers = np.arange(self.N_PINS)[::-1]
-        self.prepShuffledAssembly(self.assembly, percentBuMaxPinLocation=0)
-        self.prepPreviousAssembly(self.assembly, powers)
+        burnups = self.burnupWithMaxValue(PinLocations.CENTER)
+        powers = np.zeros_like(burnups)
+        self.setAssemblyPinBurnups(self.assembly, burnups)
+        self.setAssemblyPinPowers(self.assembly, powers)
         rot = getOptimalAssemblyOrientation(self.assembly, self.assembly)
         self.assertEqual(rot, 0)
 
@@ -107,11 +163,19 @@ class TestOptimalAssemblyRotation(FuelHandlerTestHelper):
         """
         shuffledAssembly = self.assembly
         previousAssembly = copy.deepcopy(shuffledAssembly)
-        for startPin, oppositePin in ((1, 4), (2, 5), (3, 6), (4, 1), (5, 2), (6, 3)):
-            powers = np.ones(self.N_PINS)
-            powers[oppositePin] = 0
-            self.prepShuffledAssembly(shuffledAssembly, startPin)
-            self.prepPreviousAssembly(previousAssembly, powers)
+        pairs = (
+            (PinLocations.DUE_RIGHT_VERTEX, PinLocations.DUE_LEFT_VERTEX),
+            (PinLocations.UPPER_LEFT_VERTEX, PinLocations.LOWER_RIGHT_VERTEX),
+            (PinLocations.UPPER_RIGHT_VERTEX, PinLocations.LOWER_LEFT_VERTEX),
+            (PinLocations.DUE_LEFT_VERTEX, PinLocations.DUE_RIGHT_VERTEX),
+            (PinLocations.LOWER_RIGHT_VERTEX, PinLocations.UPPER_LEFT_VERTEX),
+            (PinLocations.LOWER_LEFT_VERTEX, PinLocations.UPPER_RIGHT_VERTEX),
+        )
+        for startPin, oppositePin in pairs:
+            powers = self.powerWithMinValue(oppositePin)
+            burnups = self.burnupWithMaxValue(startPin)
+            self.setAssemblyPinBurnups(shuffledAssembly, burnups)
+            self.setAssemblyPinPowers(previousAssembly, powers)
             rot = getOptimalAssemblyOrientation(shuffledAssembly, previousAssembly)
             # 180 degrees is three 60 degree rotations
             self.assertEqual(rot, 3, msg=f"{startPin=} :: {oppositePin=}")
@@ -124,60 +188,16 @@ class TestOptimalAssemblyRotation(FuelHandlerTestHelper):
     def test_mismatchPinPowersAndLocations(self):
         """Require pin powers and locations to be have the same length."""
         powers = np.arange(self.N_PINS + 1)
-        self.prepShuffledAssembly(self.assembly, percentBuMaxPinLocation=4)
-        self.prepPreviousAssembly(self.assembly, powers)
+        burnups = np.arange(self.N_PINS)
+        self.setAssemblyPinBurnups(self.assembly, burnups)
+        self.setAssemblyPinPowers(self.assembly, powers)
         with self.assertRaisesRegex(
             ValueError, "Inconsistent pin powers and number of pins"
         ):
             getOptimalAssemblyOrientation(self.assembly, self.assembly)
 
 
-class TestFuelHandlerMgmtTools(FuelHandlerTestHelper):
-    @staticmethod
-    def prepareAssemForBurnupRotation(assem: HexAssembly):
-        """Set necessary data in order for the burnup rotation to be performed."""
-        for b in assem.getBlocks(Flags.FUEL):
-            b.initializePinLocations()
-            fuel = b.getChildrenWithFlags(Flags.FUEL)[0]
-            mult = fuel.getDimension("mult")
-            fuel.p.pinPercentBu = np.arange(mult, dtype=float)[::-1]
-            b.p.linPowByPin = reversed(range(b.getNumPins()))
-
-    def test_buReducingAssemblyRotation(self):
-        """Test that the fuel handler supports the burnup reducing assembly rotation."""
-        newSettings = {
-            CONF_ASSEM_ROTATION_STATIONARY: False,
-            "fluxRecon": True,
-            "assemblyRotationAlgorithm": "buReducingAssemblyRotation",
-        }
-        self.o.cs = self.o.cs.modified(newSettings=newSettings)
-
-        # Grab two assemblies and make them look like they were part of a shuffling operation
-        rotatedAssem, shuffledAway = self.o.r.core.getChildrenWithFlags(Flags.FUEL)[:2]
-        self.prepareAssemForBurnupRotation(rotatedAssem)
-        self.prepareAssemForBurnupRotation(shuffledAway)
-
-        # Mimic a shuffling where shuffledAway used to be where rotatedAssem is now
-        shuffledAway.lastLocationLabel = rotatedAssem.getLocation()
-
-        # Show that we call the optimal assembly orientation function.
-        # This function is tested seperately and more extensively elsewhere.
-        fh = FullImplFuelHandler(self.o)
-        # Mocked swap function updates the moved assemblies collection
-        fh.chooseSwaps = mock.Mock(side_effect=lambda _: fh.moved.append(shuffledAway))
-        with mock.patch(
-            "armi.physics.fuelCycle.assemblyRotationAlgorithms.getOptimalAssemblyOrientation",
-            return_value=4,
-        ) as p:
-            fh.outage(1)
-        # fh.outage clears fh.moved at the end. But if it was called, we know our mock function added
-        # our shuffled assembly to the moved assemblies.
-        fh.chooseSwaps.assert_called_once()
-        p.assert_called_once_with(rotatedAssem, shuffledAway)
-        for b in rotatedAssem.getBlocks(Flags.FUEL):
-            # Four rotations is 240 degrees
-            self.assertEqual(b.p.orientation[2], 240)
-
+class TestFuelHandlerMgmtTools(ShuffleAndRotateTestHelper):
     def test_buRotationWithFreshFeed(self):
         """Test that rotation works if a new assembly is swapped with fresh fuel.
 
@@ -192,7 +212,7 @@ class TestFuelHandlerMgmtTools(FuelHandlerTestHelper):
 
         fresh = self.r.core.createFreshFeed(self.o.cs)
         self.assertEqual(fresh.lastLocationLabel, HexAssembly.LOAD_QUEUE)
-        fh = FullImplFuelHandler(self.o)
+        fh = MockFuelHandler(self.o)
         fh.chooseSwaps = mock.Mock(side_effect=lambda _: fh.moved.append(fresh))
 
         with mock.patch(
@@ -216,11 +236,12 @@ class TestFuelHandlerMgmtTools(FuelHandlerTestHelper):
         # Grab two assemblies that were not moved. One of which will have the detailed information
         # needed for rotation
         detailedAssem, coarseAssem = self.o.r.core.getChildrenWithFlags(Flags.FUEL)[:2]
-        self.prepareAssemForBurnupRotation(detailedAssem)
+        self.setAssemblyPinBurnups(detailedAssem, burnups=np.arange(self.N_PINS))
+        self.setAssemblyPinPowers(detailedAssem, pinPowers=np.arange(self.N_PINS))
         detailedAssem.rotate = mock.Mock()
         coarseAssem.rotate = mock.Mock()
 
-        fh = FullImplFuelHandler(self.o)
+        fh = MockFuelHandler(self.o)
 
         with mock.patch(
             "armi.physics.fuelCycle.assemblyRotationAlgorithms.getOptimalAssemblyOrientation",
@@ -228,11 +249,83 @@ class TestFuelHandlerMgmtTools(FuelHandlerTestHelper):
         ) as p:
             fh.outage()
         p.assert_called_once_with(detailedAssem, detailedAssem)
+        # Assembly with detailed pin powers and pin burnups will be rotated
         detailedAssem.rotate.assert_called_once()
+        self.compareMockedToExpectedRotation(5, detailedAssem.rotate)
+        # Assembly without pin level data will not be rotated
         coarseAssem.rotate.assert_not_called()
 
+    def test_rotateInShuffleQueue(self):
+        """Test for expected behavior when multiple assemblies are shuffled and rotated in one outage.
+
+        Examine the behavior of three assemblies: ``first -> second -> third``
+
+        1. ``first`` is moved to the location of ``second`` and rotated by comparing
+           ``first`` burnup against ``second`` pin powers.
+        2. ``second`` is moved to the location of ``third`` and rotated by comparing
+           ``second`` burnup against ``third`` pin powers.
+
+        where:
+
+        * ``first`` burnup is maximized in the upper left direction.
+        * ``second`` pin power is minimized along the lower left direction.
+        * ``second`` burnup is maximized in the upper right direction.
+        * ``third`` pin power is minimized in the direct right direction.
+
+        We should expect:
+
+        1. ``first`` is rotated from upper left to lower left => two 60 degree CCW rotations.
+        2. ``second`` is rotated from upper right to direct right => five 60 degree CCW rotations.
+        """
+        newSettings = {
+            CONF_ASSEM_ROTATION_STATIONARY: False,
+            "fluxRecon": True,
+            "assemblyRotationAlgorithm": "buReducingAssemblyRotation",
+        }
+        self.o.cs = self.o.cs.modified(newSettings=newSettings)
+
+        first, second, third = self.r.core.getChildrenWithFlags(Flags.FUEL)[:3]
+
+        firstBurnups = self.burnupWithMaxValue(PinLocations.UPPER_LEFT_VERTEX)
+        self.setAssemblyPinBurnups(first, firstBurnups)
+
+        secondPowers = self.powerWithMinValue(PinLocations.LOWER_LEFT_VERTEX)
+        self.setAssemblyPinPowers(second, pinPowers=secondPowers)
+
+        secondBurnups = self.burnupWithMaxValue(PinLocations.UPPER_RIGHT_VERTEX)
+        self.setAssemblyPinBurnups(second, burnups=secondBurnups)
+
+        thirdPowers = self.powerWithMinValue(PinLocations.DUE_RIGHT_VERTEX)
+        self.setAssemblyPinPowers(third, thirdPowers)
+
+        # Set the shuffling sequence
+        # first -> second
+        # second -> third
+        second.lastLocationLabel = first.getLocation()
+        third.lastLocationLabel = second.getLocation()
+
+        first.rotate = mock.Mock()
+        second.rotate = mock.Mock()
+        third.rotate = mock.Mock()
+
+        fh = MockFuelHandler(self.o)
+        fh.chooseSwaps = mock.Mock(
+            side_effect=lambda _: fh.moved.extend([second, third])
+        )
+        fh.outage()
+
+        first.rotate.assert_called_once()
+        self.compareMockedToExpectedRotation(2, first.rotate, "First")
+        second.rotate.assert_called_once()
+        self.compareMockedToExpectedRotation(5, second.rotate, "Second")
+        third.rotate.assert_not_called()
+
+
+class SimpleRotationTests(ShuffleAndRotateTestHelper):
+    """Test the simple rotation where assemblies are rotated a fixed amount."""
+
     def test_simpleAssemblyRotation(self):
-        """Test rotating assemblies 120 degrees."""
+        """Test rotating assemblies 120 degrees with two rotation events."""
         fh = fuelHandlers.FuelHandler(self.o)
         newSettings = {CONF_ASSEM_ROTATION_STATIONARY: True}
         self.o.cs = self.o.cs.modified(newSettings=newSettings)
