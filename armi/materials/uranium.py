9f2ba4601182a8b5e4785b234577cc47abfd855d
jstilley <jstilley@terrapower.com>
2024-12-05 16:12:19 -0800

    Progress on materials

--- armi/materials/uranium.py
+++ armi/materials/uranium.py
@@ -24,7 +24,6 @@ from numpy import interp
 
 from armi import runLog
 from armi.materials.material import FuelMaterial
-from armi.nucDirectory import nuclideBases as nb
 from armi.utils.units import getTk
 
 
@@ -233,16 +232,15 @@ class Uranium(FuelMaterial):
         return interp(Tk, self._heatCapacityTableK, self._heatCapacityTable)
 
     def setDefaultMassFracs(self) -> None:
-        u235 = nb.byLabel["U235"]
-        u238 = nb.byLabel["U238"]
+        u235Abundance = 0.007204
+        u235Weight = 235.043929425
+        u238Abundance = 1.0 - u235Abundance
+        u238Weight = 238.050788298
 
-        u238Abundance = (
-            1.0 - u235.abundance
-        )  # neglect U234 and keep U235 at natural level
-        gramsIn1Mol = u235.abundance * u235.weight + u238Abundance * u238.weight
+        gramsIn1Mol = u235Abundance * u235Weight + u238Abundance * u238Weight
 
-        self.setMassFrac("U235", u235.weight * u235.abundance / gramsIn1Mol)
-        self.setMassFrac("U238", u238.weight * u238Abundance / gramsIn1Mol)
+        self.setMassFrac("U235", u235Weight * u235Abundance / gramsIn1Mol)
+        self.setMassFrac("U238", u238Weight * u238Abundance / gramsIn1Mol)
 
     def applyInputParams(
         self, U235_wt_frac: float = None, TD_frac: float = None, *args, **kwargs
@@ -254,8 +252,7 @@ class Uranium(FuelMaterial):
         if td is not None:
             if td > 1.0:
                 runLog.warning(
-                    "Theoretical density frac for {0} is {1}, which is >1"
-                    "".format(self, td),
+                    f"Theoretical density frac for {self} is {td}, which is >1",
                     single=True,
                     label="Large theoretical density",
                 )
