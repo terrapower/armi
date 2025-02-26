8974c4250492054a94fe8185b53c28c8f1cc1e37
Michael Jarrett <mjarrett@terrapower.com>
2025-02-26 02:51:57 +0000

    Account for symmetry factor in getMgFlux

--- armi/reactor/components/component.py
+++ armi/reactor/components/component.py
@@ -1221,6 +1221,46 @@ class Component(composites.Composite, metaclass=ComponentType):
             )
         self.setMassFracs(adjustedMassFracs)
 
+    def getMgFlux(self, adjoint=False, average=False, volume=None, gamma=False):
+        """
+        Return the multigroup neutron flux in [n/cm^2/s].
+
+        The first entry is the first energy group (fastest neutrons). Each additional
+        group is the next energy group, as set in the ISOTXS library.
+
+        On blocks, it is stored integrated over volume on <block>.p.mgFlux
+
+        Parameters
+        ----------
+        adjoint : bool, optional
+            Return adjoint flux instead of real
+
+        average : bool, optional
+            If true, will return average flux between latest and previous. Doesn't work
+            for pin detailed yet
+
+        volume: float, optional
+            If average=True, the volume-integrated flux is divided by volume before
+            being returned. The user may specify a volume here, or the function will
+            obtain the block volume directly.
+
+        gamma : bool, optional
+            Whether to return the neutron flux or the gamma flux.
+
+        Returns
+        -------
+        flux : np.ndarray
+            multigroup neutron flux in [n/cm^2/s]
+        """
+        if average:
+            raise NotImplementedError(
+                "{} class has no method for producing average MG flux -- try"
+                "using blocks".format(self.__class__)
+            )
+
+        volume = volume or self.getVolume() / self.parent.getSymmetryFactor()
+        return self.getIntegratedMgFlux(adjoint=adjoint, gamma=gamma) / volume
+
     def getIntegratedMgFlux(self, adjoint=False, gamma=False):
         """
         Return the multigroup neutron tracklength in [n-cm/s].
