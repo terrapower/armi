cb548f0978dcc065bce69bc7644c5b5c5f100c02
jstilley <jstilley@terrapower.com>
2025-03-24 13:11:06 -0700

    Removing writeHTML methods

--- armi/bookkeeping/report/data.py
+++ armi/bookkeeping/report/data.py
@@ -18,12 +18,8 @@ import copy
 import re
 
 from armi import runLog
-from armi.bookkeeping.report import html
 
 
-# --------------------------------------------
-#                REPORTS
-# --------------------------------------------
 class Report:
     """Storage for data separated out for a particular kind of user."""
 
@@ -77,75 +73,7 @@ class Report:
             )
             return None
 
-    def writeHTML(self):
-        """Renders this report as a standalone HTML file."""
-        filename = "{}.html".format(self.title)
-        runLog.debug("Writing HTML document {}.".format(filename))
 
-        with html.HTMLFile(filename, "w") as f:
-            html.writeStandardReportTemplate(f, self, self.title)
-
-        runLog.info("HTML document {} written".format(filename))
-
-    def writeGroupsHTML(self, f):
-        """A helper method to the writeHTML method process.
-
-        Composes the group html content, intended for use in the midst of the html document generation
-        """
-        Image.count = 0  # reset the count for this report's figure enumeration
-
-        # this implementation is hardcoded as 2 equal width layout
-        # easy enough to change if so desired
-        leftCol = []
-        leftNumRows, leftNumImgs = 0, 0
-        rightCol = []
-        rightNumRows, rightNumImgs = 0, 0
-
-        # roughly compose the groups into equalish length columns
-        for global_group in self._groupRenderOrder:
-            group = self.groups[global_group]
-            if isinstance(group, Table):
-                tableEstimateLength = len(group.data.keys())
-                if leftNumRows <= rightNumRows:
-                    leftCol.append(group)
-                    leftNumRows += tableEstimateLength
-                else:
-                    rightCol.append(group)
-                    rightNumRows += tableEstimateLength
-            elif isinstance(group, Image):
-                if leftNumImgs <= rightNumImgs:
-                    leftCol.append(group)
-                    leftNumImgs += len(group.data.keys())
-                else:
-                    rightCol.append(group)
-                    rightNumImgs += len(group.data.keys())
-            else:
-                raise NotImplementedError(
-                    "Please update this method for handling of new group classes."
-                )
-
-        # write html columns and place content inside
-        with html.Div(f, attrs={"class": "row"}):
-            with html.Div(f, attrs={"class": "col-md-6"}):
-                for group in leftCol:
-                    group.writeHTML(f)
-            with html.Div(f, attrs={"class": "col-md-6"}):
-                for group in rightCol:
-                    group.writeHTML(f)
-
-        # specially gathered groups constructed into own subsection
-        presentComponentSpecials = [
-            group for group in self.componentWellGroups if group in self.groups
-        ]
-        if any(presentComponentSpecials):
-            with html.Div(f, attrs={"class": "well"}):
-                for global_group in presentComponentSpecials:
-                    self.groups[global_group].writeHTML(f)
-
-
-# --------------------------------------------
-#                GROUPS
-# --------------------------------------------
 class Group:
     """Abstract class, when extended is used for storage for data within a report.
 
@@ -230,30 +158,6 @@ class Table(Group):
 
         Group.__setitem__(self, name, value)
 
-    def writeHTML(self, f):
-        with html.Table(f, attrs={"class": "table table-striped table-hover "}):
-            with html.H4(f, attrs={"style": self.titleStyle}):
-                f.writeEscaped(self.title)
-                with html.Span(f, attrs={"style": self.descStyle}):
-                    f.writeEscaped(self.description)
-
-            if self.header:
-                with html.THead(f):
-                    with html.TR(f):
-                        for colname in self.header:
-                            with html.TH(f):
-                                with html.Span(f):
-                                    f.writeEscaped(colname)
-
-            with html.TBody(f):
-                for name, value in self.data.items():
-                    with html.TR(f):
-                        with html.TD(f, attrs={"class": "lalign"}):
-                            f.writeEscaped(name)
-                        for col in value:
-                            with html.TD(f):
-                                f.writeEscaped(col)
-
 
 class Image(Group):
 
@@ -262,81 +166,3 @@ class Image(Group):
     def __init__(self, title, description=""):
         Group.__init__(self, title, description=description)
         self._shortformTitle = title.replace(" ", "").lower()
-
-    def writeHTML(self, f):
-
-        if len(self.data.keys()) == 1:
-
-            # single images don't get the standard Header as the same information is moved to it's Figure Caption
-            with html.Img(
-                f,
-                attrs={
-                    "src": html.encode64(self.data[list(self.data.keys())[0]]),
-                    "width": "100%",
-                    "height": "100%",
-                    "padding-top": "15px",
-                },
-            ):
-                with html.Div(f, attrs={"class": "alert alert-dismissible alert-info"}):
-                    with html.B(f):
-                        Image.count += 1
-                        f.writeEscaped("Figure {} - {}".format(Image.count, self.title))
-                    f.writeEscaped(self.description)
-
-        elif len(self.data.keys()) > 1:
-
-            with html.H4(f, attrs={"style": self.titleStyle}):
-                f.writeEscaped(self.title)
-                with html.Span(f, attrs={"style": self.descStyle}):
-                    f.writeEscaped(self.description)
-
-            with html.UL(f, attrs={"class": "nav nav-tabs", "data-tabs": "tabs"}):
-                first = True
-                first_attrs = {"class": "active"}
-                later_attrs = {"class": ""}
-                for i in range(len(self.data.keys())):
-                    with html.LI(f, attrs=first_attrs if first else later_attrs):
-                        with html.A(
-                            f,
-                            attrs={
-                                "href": "#{}{}".format(self._shortformTitle, i),
-                                "data-toggle": "tab",
-                            },
-                        ):
-                            f.writeEscaped(str(i + 1))
-                    first = False
-
-            with html.Div(f, attrs={"class": "tab-content"}):
-                Image.count += 1
-                first = True
-                for i, (name, filepath) in enumerate(self.data.items()):
-                    pane_attrs = {}
-                    pane_attrs["class"] = (
-                        "tab-pane fade active in" if first else "tab-pane fade"
-                    )
-                    pane_attrs["id"] = "{}{}".format(self._shortformTitle, i)
-                    first = False
-
-                    with html.Div(f, attrs=pane_attrs):
-                        with html.Img(
-                            f,
-                            attrs={
-                                "src": html.encode64(filepath),
-                                "width": "100%",
-                                "height": "100%",
-                                "padding-top": "15px",
-                            },
-                        ):
-                            with html.Div(
-                                f, attrs={"class": "alert alert-dismissible alert-info"}
-                            ):
-                                with html.B(f):
-                                    f.writeEscaped(
-                                        "Figure {} - {}".format(Image.count, name)
-                                    )
-                                f.writeEscaped(filepath)
-
-        else:
-            runLog.warning(
-                "Empty Image group {} has nothing to report to HTML".format(self.title)
-            )
