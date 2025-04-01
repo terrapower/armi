38af70fcab079082ac6c3d8506a035a8f0f0d2e6
John Stilley <1831479+john-science@users.noreply.github.com>
2025-04-01 14:21:02 -0700

    Fixing module-level docstring

--- armi/reactor/tests/test_cores.py
+++ armi/reactor/tests/test_cores.py
@@ -1,4 +1,4 @@
-# Copyright 2024 TerraPower, LLC
+# Copyright 2025 TerraPower, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
