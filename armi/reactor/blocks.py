7d7ccab15ef8cfa3cec80cb6b0a688118a837866
jstilley <jstilley@terrapower.com>
2025-04-09 17:19:19 -0700

    Reducing the warnings from Block.autoCreateSpatialGrids

--- armi/reactor/blocks.py
+++ armi/reactor/blocks.py
@@ -2186,7 +2186,7 @@ class HexBlock(Block):
 
         In this case, a simple block would be one that has either multiplicity of components equal
         to 1 or N but no other multiplicities. Also, this should only happen when N fits exactly
-        into a given number of hex rings.  Otherwise, do not create a grid for this block.
+        into a given number of hex rings. Otherwise, do not create a grid for this block.
 
         Parameters
         ----------
@@ -2211,12 +2211,21 @@ class HexBlock(Block):
         # Check multiplicities
         mults = {c.getDimension("mult") for c in self.iterComponents()}
 
-        if len(mults) != 2 or 1 not in mults:
-            raise ValueError(
-                "Could not create a spatialGrid for block {}, multiplicities are not 1 or N they are {}".format(
-                    self.p.type, mults
-                )
+        # Do some validation: Should we try to create a spatial grid?
+        multz = {float(m) for m in mults}
+        if len(multz) == 1 and list(multz)[0] == 1.0:
+            runLog.extra(
+                f"Will not create a spatialGrid for block {self.p.type}, multiplicities are are all 1.",
+                single=True,
             )
+            return
+        elif len(multz) != 2 or 1.0 not in multz:
+            runLog.extra(
+                f"Could not create a spatialGrid for block {self.p.type}, multiplicities are not 1 "
+                f"or N they are {mults}",
+                single=True,
+            )
+            return
 
         # build the grid, from pitch and orientation
         if isinstance(systemSpatialGrid, grids.HexGrid):
