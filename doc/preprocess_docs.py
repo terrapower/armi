11771f9e66d30e01d9a0329c190a85b5a6bc184c
jstilley <jstilley@terrapower.com>
2025-06-03 09:13:19 -0700

    Removing everything but the skip STR logic

--- doc/preprocess_docs.py
+++ doc/preprocess_docs.py
@@ -12,44 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-TODO.
-
-Then this:
-
-pytest --junit-xml=test_results.xml -v -n 4 armi > pytest_verbose.log
-mpiexec -n 2 --use-hwthread-cpus pytest --junit-xml=test_results_mpi1.xml
-    armi/tests/test_mpiFeatures.py > pytest_verbose_mpi1.log
-mpiexec -n 2 --use-hwthread-cpus pytest --junit-xml=test_results_mpi2.xml
-    armi/tests/test_mpiParameters.py > pytest_verbose_mpi2.log
-mpiexec -n 2 --use-hwthread-cpus pytest --junit-xml=test_results_mpi3.xml
-    armi/utils/tests/test_directoryChangersMpi.py > pytest_verbose_mpi3.log
-python doc/.static/cleanup_test_results.py test_results.xml
-"""
-import os
+"""TODO."""
 import subprocess
-import sys
 
 
 def main():
-    if len(sys.argv) > 1 and sys.argv[1].lower() in ["--cleanup", "-c"]:
-        # clean up previous data file, if they exist
-        for fileName in ["pytest_verbose.log", "test_results.xml"]:
-            if os.path.exists(fileName):
-                print(f"Removing {fileName}")
-                os.remove(fileName)
+    # skip build the STR, if you are running locally
+    cmd = 'echo "skipping STR"'
+    fileName = "pytest_verbose.log"
+    _pipeCmdToFile(cmd, fileName, True)
 
-        return
-
-    if len(sys.argv) > 1 and sys.argv[1].lower() in ["--skip-str", "-s"]:
-        # skip build the STR, if you are running locally
-        cmd = 'echo "skipping STR"'
-        fileName = "pytest_verbose.log"
-        _pipeCmdToFile(cmd, fileName, True)
-
-        cmd = 'echo "<metadata></metadata>"'
-        fileName = "test_results.xml"
-        _pipeCmdToFile(cmd, fileName, True)
+    cmd = 'echo "<metadata></metadata>"'
+    fileName = "test_results.xml"
+    _pipeCmdToFile(cmd, fileName, True)
 
 
 def _pipeCmdToFile(cmd, fileName, append=False):
