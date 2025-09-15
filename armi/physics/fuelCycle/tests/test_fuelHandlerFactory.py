eecc576431ce1b7e98fff15fbd784676fed923fa
Hunter Smith <hsmith@terrapower.com>
2025-09-15 18:18:37 +0000

    add copyright

--- armi/physics/fuelCycle/tests/test_fuelHandlerFactory.py
+++ armi/physics/fuelCycle/tests/test_fuelHandlerFactory.py
@@ -1,5 +1,17 @@
-# USE AND DISTRIBUTION OF THIS CODE IS GOVERNED BY EXPORT CONTROL LAWS AND THE LICENSE IN LICENSE.txt.
-# @LICENSE:
+# Copyright 2025 TerraPower, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Tests for :mod:`armi.physics.fuelCycle.fuelHandlerFactory`."""
 
 import unittest
@@ -29,7 +41,7 @@ class FuelHandlerFactoryTests(unittest.TestCase):
         self.settings.inputDirectory = str(self.inputDirectory)
         self.operator = _DummyOperator(self.settings)
 
-    def test_file_path(self):
+    def test_filePath(self):
         """Custom handlers can still be loaded from explicit file paths."""
         modulePath = Path(__file__).resolve().with_name("_customFuelHandlerModule.py")
         self.settings.update(
@@ -43,7 +55,7 @@ class FuelHandlerFactoryTests(unittest.TestCase):
 
         self.assertEqual(handler.__class__.__name__, "FileFuelHandler")
 
-    def test_module_path(self):
+    def test_modulePath(self):
         """Module-style paths are imported using :mod:`importlib`."""
         moduleName = "armi.physics.fuelCycle.tests._customFuelHandlerModule"
         self.settings.update(
