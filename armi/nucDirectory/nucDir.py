fb12b3703eeedcd25f31804dcd4a8fe3d2fcf5dd
John Stilley <jstilley@terrapower.com>
2025-10-08 15:37:17 -0700

    cleaning up test_nucDir.py

--- armi/nucDirectory/nucDir.py
+++ armi/nucDirectory/nucDir.py
@@ -24,6 +24,7 @@ This may be deprecated. Consider using the appropriate instance methods availabl
 
 import re
 
+# TODO: JOHN A lot of global stuff here.
 from armi.nucDirectory import elements, nuclideBases
 
 nuclidePattern = re.compile(r"([A-Za-z]+)-?(\d{0,3})(\d*)(\S*)")
