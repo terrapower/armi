ab1f503942f48876824a6396acd21b97ddfe0b84
John Stilley <jstilley@terrapower.com>
2026-01-14 17:01:49 -0800

    Moving unused TH params out

--- armi/physics/thermalHydraulics/parameters.py
+++ armi/physics/thermalHydraulics/parameters.py
@@ -43,130 +43,7 @@ def _getAssemblyParams():
 def _getBlockParams():
     pDefs = parameters.ParameterDefinitionCollection()
 
-    with pDefs.createBuilder(default=0.0, location=ParamLocation.AVERAGE, categories=["thermal hydraulics"]) as pb:
-        pb.defParam(
-            "THhotChannelHeatTransferCoeff",
-            units=f"{units.WATTS}/{units.METERS}^2/{units.DEGK}",
-            saveToDB=True,
-            description="Film heat transfer coefficient for hot channel in the assembly.",
-        )
-
-    with pDefs.createBuilder(default=None, saveToDB=True, categories=["thermal hydraulics"]) as pb:
-        pb.defParam(
-            "THhotChannelCladODT",
-            units=units.DEGC,
-            description="Nominal clad outer diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "THhotChannelCladIDT",
-            units=units.DEGC,
-            description="Nominal clad inner diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "THhotChannelFuelODT",
-            units=units.DEGC,
-            description="Temperature of the fuel outer diameter",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "THhotChannelFuelCenterlineT",
-            units=units.DEGC,
-            description="Nominal hot channel fuel centerline temperature",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "TH0SigmaCladODT",
-            units=units.DEGC,
-            description="0-sigma clad outer diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "TH2SigmaCladODT",
-            units=units.DEGC,
-            description="2-sigma clad outer diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "TH3SigmaCladODT",
-            units=units.DEGC,
-            description="3-sigma clad outer diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "TH0SigmaCladIDT",
-            units=units.DEGC,
-            description="0-sigma clad inner diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "TH2SigmaCladIDT",
-            units=units.DEGC,
-            description="2-sigma clad inner diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
-        pb.defParam(
-            "TH3SigmaCladIDT",
-            units=units.DEGC,
-            description="3-sigma clad inner diameter temperature of the hot pin",
-            categories=["thInterface"],
-        )
-
     with pDefs.createBuilder(default=0.0, categories=["thInterface"], saveToDB=True) as pb:
-        pb.defParam(
-            "THTfuelCL",
-            units=units.DEGC,
-            description="Average temperature of the fuel centerline used for neutronic coupling",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THTfuelOD",
-            units=units.DEGC,
-            description="Average temperature of the fuel outer diameter used for neutronic coupling",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THaverageCladODT",
-            units=units.DEGC,
-            description="Block average of the outer clad temperature.",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THaverageCladTemp",
-            units=units.DEGC,
-            description="The nominal average clad temperature in the block, which should be used "
-            "for neutronic and TH feedback.",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THaverageDuctTemp",
-            units=units.DEGC,
-            description="The nominal average duct temperature in the block, which should be used "
-            "for neutronic and TH feedback.",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THcoolantInletT",
-            units=units.DEGC,
-            description="The nominal average bulk coolant inlet temperature into the block.",
-            location=ParamLocation.BOTTOM,
-        )
-
         pb.defParam(
             "THcoolantOutletT",
             units=units.DEGC,
@@ -175,52 +52,10 @@ def _getBlockParams():
         )
 
         pb.defParam(
-            "THcoolantStaticT",
-            units=units.DEGC,
-            description="Volume-based average coolant temperature, recommended for neutronics",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THdeltaPTotal",
-            units=units.PASCALS,
-            description="Total pressure difference in a block",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THhotChannelOutletT",
-            units=units.DEGC,
-            description="Nominal hot channel outlet temperature",
-            location=ParamLocation.TOP,
-        )
-
-        pb.defParam(
-            "THlocalDTout",
-            units=units.DEGC,
-            description="Maximum delta-Ts for outlet temperatures of all assemblies",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
-            "THlocalDToutFuel",
-            units=units.DEGC,
-            description="Maximum delta-Ts for outlet temperatures of fuel assemblies",
-            location=ParamLocation.AVERAGE,
-        )
-
-        pb.defParam(
             "THmassFlowRate",
             units=f"{units.KG}/{units.SECONDS}",
             description="Mass flow rate",
             location=ParamLocation.VOLUME_INTEGRATED,
-        )
-
-        pb.defParam(
-            "THorificeZone",
-            units=units.UNITLESS,
-            description="A list of orificing zones corresponding to the assembly list",
-            location=ParamLocation.AVERAGE,
         )
 
     with pDefs.createBuilder(default=None, categories=["thermal hydraulics", "mongoose"], saveToDB=True) as pb:
@@ -236,14 +71,6 @@ def _getBlockParams():
             units=units.DEGC,
             description="Mid-wall duct temperature for assembly edges",
             location=ParamLocation.BOTTOM | ParamLocation.EDGES,
-        )
-
-    with pDefs.createBuilder(default=0, categories=["thermal hydraulics", "mongoose"], saveToDB=True) as pb:
-        pb.defParam(
-            "THhotChannel",
-            units=units.UNITLESS,
-            description="Hot channel (highest coolant dT) identifier",
-            location=ParamLocation.AVERAGE,
         )
 
     return pDefs
