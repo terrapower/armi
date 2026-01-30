1cad139a93c904554115feec8dc285ed88ff87fb
John Stilley <jstilley@terrapower.com>
2026-01-30 15:08:19 -0800

    Oh, is it working now?

--- armi/nuclearDataIO/tests/test_xsLibraries.py
+++ armi/nuclearDataIO/tests/test_xsLibraries.py
@@ -354,7 +354,6 @@ class AbstractTestXSlibraryMerging(TempFileMixin):
         emptyXSLib.merge(self.libAA)
         self.libAA = None
         self.getWriteFunc()(emptyXSLib, self.testFileName)
-        print(filecmp.cmp(self.getLibAAPath(), self.testFileName))
         self.assertTrue(filecmp.cmp(self.getLibAAPath(), self.testFileName))
 
     def test_mergeTwoXSLibFiles(self):
