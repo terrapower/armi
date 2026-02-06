d8135b324462619992d7db573f0f2f476b3b7e20
John Stilley <jstilley@terrapower.com>
2026-02-06 15:36:39 -0800

    Moving setting xsBucklingConvergence out of ARMI

--- armi/physics/neutronics/settings.py
+++ armi/physics/neutronics/settings.py
@@ -68,7 +68,6 @@ CONF_MINIMUM_FISSILE_FRACTION = "minimumFissileFraction"
 CONF_MINIMUM_NUCLIDE_DENSITY = "minimumNuclideDensity"
 CONF_TOLERATE_BURNUP_CHANGE = "tolerateBurnupChange"
 CONF_XS_BLOCK_REPRESENTATION = "xsBlockRepresentation"
-CONF_XS_BUCKLING_CONVERGENCE = "xsBucklingConvergence"
 CONF_XS_EIGENVALUE_CONVERGENCE = "xsEigenvalueConvergence"
 CONF_XS_KERNEL = "xsKernel"
 CONF_XS_SCATTERING_ORDER = "xsScatteringOrder"
@@ -311,17 +310,6 @@ def defineSettings():
             default=3,
             label="Scattering Order",
             description="Scattering order for the lattice physics calculation",
-        ),
-        setting.Setting(
-            CONF_XS_BUCKLING_CONVERGENCE,
-            default=1e-05,
-            label="Buckling Convergence Criteria",
-            description="Convergence criteria for the buckling iteration if it is "
-            "available in the lattice physics solver",
-            oldNames=[
-                ("mc2BucklingConvergence", None),
-                ("bucklingConvergence", None),
-            ],
         ),
         setting.Setting(
             CONF_XS_EIGENVALUE_CONVERGENCE,
