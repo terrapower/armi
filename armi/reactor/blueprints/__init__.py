1fe6d0b00f5ed9a990fa4146e2f22d489d8f9b7d
Arrielle Opotowsky <c-aopotowsky@terrapower.com>
2026-02-09 10:55:00 -0600

    This worked before, why is it not working now?

--- armi/reactor/blueprints/__init__.py
+++ armi/reactor/blueprints/__init__.py
@@ -526,8 +526,7 @@ class Blueprints(yamlize.Object, metaclass=_BlueprintsPluginCollector):
         # AttributeError: 'RoundTripLoader' object has no attribute 'max_depth'
         # Setting that attribute to `None` solved the issue. However, it would be prudent to rework blueprints loading
         # to side step the issue entirely.
-        if roundTrip:
-            RoundTripLoader.max_depth = None
+        RoundTripLoader.max_depth = None
         loader = RoundTripLoader if roundTrip else CLoader
         return super().load(stream, Loader=loader)
 
