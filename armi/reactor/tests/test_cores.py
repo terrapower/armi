832ddbea3e4dbf2b3ad1aa2fea9cb247eeec61ee
John Stilley <jstilley@terrapower.com>
2026-05-18 14:20:47 -0700

    reverting dev testing changes

---
+++ armi/reactor/tests/test_cores.py
@@ -0,0 +1,180 @@
+# Copyright 2025 TerraPower, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import itertools
+import random
+import typing
+import unittest
+from unittest import mock
+from unittest.mock import patch
+
+from armi.nuclearDataIO.xsLibraries import IsotxsLibrary
+from armi.reactor.assemblies import HexAssembly
+from armi.reactor.blocks import Block
+from armi.reactor.flags import Flags
+from armi.reactor.tests.test_reactors import loadTestReactor
+from armi.testing import TESTING_ROOT
+from armi.tests import ISOAA_PATH
+
+
+class HexCoreTests(unittest.TestCase):
+    """Tests on a hex reactor core."""
+
+    @classmethod
+    def setUpClass(cls):
+        r = loadTestReactor(
+            inputFilePath=TESTING_ROOT, inputFileName="reactors/thirdSmallHexReactor/thirdSmallHexReactor.yaml"
+        )[1]
+        cls.core = r.core
+
+    def assertAllIs(self, actuals: typing.Iterable[typing.Any], expecteds: typing.Iterable[typing.Any], fill=None):
+        """Assert that all items in two iterables are the same objects."""
+        for actual, expected in itertools.zip_longest(actuals, expecteds, fillvalue=fill):
+            self.assertIs(actual, expected)
+
+    def test_getAllAssem(self):
+        """Test the ability to produce all assemblies."""
+        expectedAll = list(self.core)
+        actualAll = self.core.getAssemblies()
+        self.assertAllIs(actualAll, expectedAll)
+
+    def test_getAllAssemWithFlag(self):
+        """Test the ability to produce assemblies with a flag."""
+        for spec in (Flags.FUEL, Flags.CONTROL):
+            expected = self.core.getChildrenWithFlags(spec)
+            actual = self.core.getAssemblies(typeSpec=spec)
+            for a in actual:
+                self.assertIsInstance(a, HexAssembly)
+                self.assertTrue(a.hasFlags(spec))
+            self.assertAllIs(actual, expected)
+
+    def test_iterChildrenWithFlags(self):
+        aa = list(self.core.iterChildrenWithFlags(Flags.BOOSTER))
+        self.assertEqual(len(aa), 0)
+
+        aa = list(self.core.iterChildrenWithFlags(Flags.FUEL))
+        self.assertTrue(1 < len(aa) < 10)
+
+        aa = list(self.core.iterChildrenWithFlags(Flags.CONTROL))
+        self.assertEqual(len(aa), 0)
+
+    def test_getAssemsInZones(self):
+        """Test the ability to produce assemblies in a zone."""
+        # Grab a few assemblies and add their locations to those in the zones
+        selection = random.choices(self.core.getAssemblies(), k=5)
+        locations = [a.getLocation() for a in selection]
+        fakeZones = ["hot", "cold"]
+        with mock.patch.object(self.core.zones, "getZoneLocations", mock.Mock(return_value=locations)):
+            actuals = self.core.getAssemblies(zones=fakeZones)
+        for a in actuals:
+            self.assertIn(a.getLocation(), locations, msg=str(a))
+
+    def test_getBlocks(self):
+        """Test the ability to get all blocks in the core."""
+        blocks = []
+        for a in self.core:
+            blocks.extend(a)
+        actual = self.core.iterBlocks()
+        self.assertAllIs(actual, blocks)
+
+    def test_getBlocksWithFlag(self):
+        """Test the ability to get all blocks with a flag in the core."""
+        blocks = []
+        for a in self.core:
+            blocks.extend(filter(lambda b: b.hasFlags(Flags.FUEL), a))
+        actual = self.core.getBlocks(Flags.FUEL)
+        self.assertAllIs(actual, blocks)
+
+    def test_traverseAllBlocks(self):
+        """Test the ability to iterate over all blocks in the core."""
+        blocks = []
+        for a in self.core:
+            blocks.extend(a)
+        actual = self.core.iterBlocks()
+        self.assertAllIs(actual, blocks)
+
+    def test_traverseAllBlocksWithFlag(self):
+        """Test the ability to traverse blocks in the core with a flag."""
+        blocks: list[Block] = []
+        for a in self.core:
+            blocks.extend(a)
+        for spec in (Flags.FUEL, Flags.CONTROL, Flags.FUEL | Flags.CONTROL):
+            expected = list(filter(lambda b: b.hasFlags(spec), blocks))
+            actual = self.core.iterBlocks(spec)
+            self.assertAllIs(actual, expected)
+            # Fake the flag check with hasFlags as predicate
+            actual = self.core.iterBlocks(predicate=lambda b: b.hasFlags(spec))
+            self.assertAllIs(actual, expected)
+
+    def test_traverseBlocksWithPredicate(self):
+        """Test the ability to traverse blocks that meet some criteria with a flag."""
+        fuelBlocks: list[Block] = []
+        for a in self.core:
+            fuelBlocks.extend(filter(lambda b: b.hasFlags(Flags.FUEL), a))
+        # Make some contrived condition to exclude some blocks
+        meanElevation = sum(b.p.z for b in fuelBlocks) / len(fuelBlocks)
+        checker = lambda b: b.p.z >= meanElevation
+        expected = list(filter(checker, fuelBlocks))
+        actual = self.core.iterBlocks(Flags.FUEL, predicate=checker)
+        self.assertAllIs(actual, expected)
+
+    @patch("armi.nuclearDataIO.getExpectedISOTXSFileName")
+    def test_lib(self, mockFileName):
+        # the default case will look something like this
+        mockFileName.return_value = "ISOTXS-c0n0"
+        self.assertIsNone(self.core.lib)
+        self.assertFalse(self.core.hasLib())
+
+        # we can inject some mock data, and retrieve it
+        mockFileName.return_value = ISOAA_PATH
+        self.assertTrue(isinstance(self.core.lib, IsotxsLibrary))
+        self.assertTrue(self.core.hasLib())
+
+    def test_getAssembliesInRing(self):
+        assems = self.core.getAssembliesInRing(0)
+        self.assertEqual(len(assems), 0)
+
+        assems = self.core.getAssembliesInRing(1)
+        self.assertEqual(len(assems), 1)
+        self.assertIsInstance(assems[0], HexAssembly)
+
+    def test_getAssembliesInSquareOrHexRing(self):
+        assems = self.core.getAssembliesInSquareOrHexRing(0)
+        self.assertEqual(len(assems), 0)
+
+        assems = self.core.getAssembliesInSquareOrHexRing(1)
+        self.assertEqual(len(assems), 1)
+        self.assertIsInstance(assems[0], HexAssembly)
+
+    def test_getAssembliesInCircularRing(self):
+        assems = self.core.getAssembliesInCircularRing(0)
+        self.assertEqual(len(assems), 0)
+
+        assems = self.core.getAssembliesInCircularRing(1)
+        self.assertEqual(len(assems), 5)
+        self.assertIsInstance(assems[0], HexAssembly)
+
+    def test_getBlockByName(self):
+        with self.assertRaises(KeyError):
+            self.core.getBlockByName("badName")
+
+        b = self.core.getBlockByName("B0004-000")
+        self.assertIsInstance(b, Block)
+
+    def test_getFirstBlock(self):
+        b = self.core.getFirstBlock()
+        self.assertIsInstance(b, Block)
+
+    def test_getFirstAssembly(self):
+        a = self.core.getFirstAssembly()
+        self.assertIsInstance(a, HexAssembly)
---
+++ armi/reactor/tests/test_zones.py
@@ -0,0 +1,368 @@
+# Copyright 2019 TerraPower, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Test for Zones."""
+
+import logging
+import os
+import unittest
+
+from armi import runLog
+from armi.reactor import (
+    assemblies,
+    blocks,
+    blueprints,
+    geometry,
+    grids,
+    reactors,
+    zones,
+)
+from armi.testing import TESTING_ROOT, loadTestReactor, mockRunLogs
+
+THIS_DIR = os.path.dirname(__file__)
+
+
+class TestZone(unittest.TestCase):
+    def setUp(self):
+        # set up a Reactor, for the spatialLocator
+        bp = blueprints.Blueprints()
+        r = reactors.Reactor("zonetest", bp)
+        r.add(reactors.Core("Core"))
+        r.core.spatialGrid = grids.HexGrid.fromPitch(1.0)
+        r.core.spatialGrid._bounds = (
+            [0, 1, 2, 3, 4],
+            [0, 10, 20, 30, 40],
+            [0, 20, 40, 60, 80],
+        )
+        r.core.spatialGrid.symmetry = geometry.SymmetryType(
+            geometry.DomainType.THIRD_CORE, geometry.BoundaryType.PERIODIC
+        )
+        r.core.spatialGrid.geomType = geometry.HEX
+
+        # some testing constants
+        self.numAssems = 5
+        self.numBlocks = 5
+
+        # build a list of Assemblies
+        self.aList = []
+        for ring in range(self.numAssems):
+            a = assemblies.HexAssembly("fuel")
+            a.spatialGrid = r.core.spatialGrid
+            a.spatialLocator = r.core.spatialGrid[ring, 1, 0]
+            a.parent = r.core
+            self.aList.append(a)
+
+        # build a list of Blocks
+        self.bList = []
+        for _ in range(self.numBlocks):
+            b = blocks.HexBlock("TestHexBlock")
+            b.setType("defaultType")
+            b.p.nPins = 3
+            b.setHeight(3.0)
+            self.aList[0].add(b)
+            self.bList.append(b)
+
+    def test_addItem(self):
+        """
+        Test adding an item.
+
+        .. test:: Add item to a zone.
+            :id: T_ARMI_ZONE0
+            :tests: R_ARMI_ZONE
+        """
+        zone = zones.Zone("test_addItem")
+        zone.addItem(self.aList[0])
+        self.assertIn(self.aList[0].getLocation(), zone)
+
+        self.assertRaises(AssertionError, zone.addItem, "nope")
+
+    def test_removeItem(self):
+        zone = zones.Zone("test_removeItem", [a.getLocation() for a in self.aList])
+        zone.removeItem(self.aList[0])
+        self.assertNotIn(self.aList[0].getLocation(), zone)
+
+        self.assertRaises(AssertionError, zone.removeItem, "also nope")
+
+    def test_addItems(self):
+        """
+        Test adding items.
+
+        .. test:: Add multiple items to a zone.
+            :id: T_ARMI_ZONE1
+            :tests: R_ARMI_ZONE
+        """
+        zone = zones.Zone("test_addItems")
+        zone.addItems(self.aList)
+        for a in self.aList:
+            self.assertIn(a.getLocation(), zone)
+
+    def test_removeItems(self):
+        zone = zones.Zone("test_removeItems", [a.getLocation() for a in self.aList])
+        zone.removeItems(self.aList)
+        for a in self.aList:
+            self.assertNotIn(a.getLocation(), zone)
+
+    def test_addLoc(self):
+        """
+        Test adding a location.
+
+        .. test:: Add location to a zone.
+            :id: T_ARMI_ZONE2
+            :tests: R_ARMI_ZONE
+        """
+        zone = zones.Zone("test_addLoc")
+        zone.addLoc(self.aList[0].getLocation())
+        self.assertIn(self.aList[0].getLocation(), zone)
+
+        self.assertRaises(AssertionError, zone.addLoc, 1234)
+
+    def test_removeLoc(self):
+        zone = zones.Zone("test_removeLoc", [a.getLocation() for a in self.aList])
+        zone.removeLoc(self.aList[0].getLocation())
+        self.assertNotIn(self.aList[0].getLocation(), zone)
+
+        self.assertRaises(AssertionError, zone.removeLoc, 1234)
+
+    def test_addLocs(self):
+        """
+        Test adding locations.
+
+        .. test:: Add multiple locations to a zone.
+            :id: T_ARMI_ZONE3
+            :tests: R_ARMI_ZONE
+        """
+        zone = zones.Zone("test_addLocs")
+        zone.addLocs([a.getLocation() for a in self.aList])
+        for a in self.aList:
+            self.assertIn(a.getLocation(), zone)
+
+    def test_removeLocs(self):
+        zone = zones.Zone("test_removeLocs", [a.getLocation() for a in self.aList])
+        zone.removeLocs([a.getLocation() for a in self.aList])
+        for a in self.aList:
+            self.assertNotIn(a.getLocation(), zone)
+
+    def test_iteration(self):
+        locs = [a.getLocation() for a in self.aList]
+        zone = zones.Zone("test_iteration")
+
+        # BONUS TEST: Zone.__len__()
+        self.assertEqual(len(zone), 0)
+        zone.addLocs(locs)
+        self.assertEqual(len(zone), self.numAssems)
+
+        # loop once to prove looping works
+        for aLoc in zone:
+            self.assertIn(aLoc, locs)
+            self.assertTrue(aLoc in zone)  # Tests Zone.__contains__()
+
+        # loop twice to make sure it iterates nicely.
+        for aLoc in zone:
+            self.assertIn(aLoc, locs)
+            self.assertTrue(aLoc in zone)  # Tests Zone.__contains__()
+
+    def test_repr(self):
+        zone = zones.Zone("test_repr")
+        zone.addItems(self.aList)
+        zStr = "Zone test_repr with 5 Assemblies"
+        self.assertIn(zStr, str(zone))
+
+    def test_blocks(self):
+        zone = zones.Zone("test_blocks", zoneType=blocks.Block)
+
+        # test the blocks were correctly added
+        self.assertEqual(len(zone), 0)
+        zone.addItems(self.bList)
+        self.assertEqual(len(zone), self.numBlocks)
+
+        # loop once to prove looping works
+        for aLoc in zone:
+            self.assertIn(aLoc, zone.locs)
+            self.assertTrue(aLoc in zone)  # test Zone.__contains__()
+
+
+class TestZones(unittest.TestCase):
+    def setUp(self):
+        # spin up the test reactor
+        self.o, self.r = loadTestReactor(
+            inputFilePath=TESTING_ROOT, inputFileName="reactors/thirdSmallHexReactor/thirdSmallHexReactor.yaml"
+        )
+
+        # build some generic test zones to get started with
+        newSettings = {}
+        newSettings["zoneDefinitions"] = [
+            "ring-1: 001-001",
+            "ring-2: 002-001, 002-002",
+            "ring-3: 003-001, 003-002, 003-003",
+        ]
+        cs = self.o.cs.modified(newSettings=newSettings)
+        self.r.core.buildManualZones(cs)
+        self.zonez = self.r.core.zones
+
+    def test_dictionaryInterface(self):
+        """
+        Test creating and interacting with the Zones object.
+
+        .. test:: Create collection of Zones.
+            :id: T_ARMI_ZONE4
+            :tests: R_ARMI_ZONE
+        """
+        zs = zones.Zones()
+
+        # validate the addZone() and __len__() work
+        self.assertEqual(len(zs.names), 0)
+        zs.addZone(self.zonez["ring-2"])
+        self.assertEqual(len(zs.names), 1)
+
+        # validate that __contains__() works
+        self.assertFalse("ring-1" in zs)
+        self.assertTrue("ring-2" in zs)
+        self.assertFalse("ring-3" in zs)
+
+        # validate that __remove__() works
+        del zs["ring-2"]
+        self.assertEqual(len(zs.names), 0)
+
+        # validate that addZones() works
+        zs.addZones(self.zonez)
+        self.assertEqual(len(zs.names), 3)
+        self.assertTrue("ring-1" in zs)
+        self.assertTrue("ring-2" in zs)
+        self.assertTrue("ring-3" in zs)
+
+        # validate that get() works
+        ring3 = zs["ring-3"]
+        self.assertEqual(len(ring3), 3)
+        self.assertIn("003-002", ring3)
+
+        # validate that removeZones() works
+        zonesToRemove = [z.name for z in self.zonez]
+        zs.removeZones(zonesToRemove)
+        self.assertEqual(len(zs.names), 0)
+        self.assertFalse("ring-1" in zs)
+        self.assertFalse("ring-2" in zs)
+        self.assertFalse("ring-3" in zs)
+
+    def test_findZoneItIsIn(self):
+        # customize settings for this test
+        newSettings = {}
+        newSettings["zoneDefinitions"] = [
+            "ring-1: 001-001",
+            "ring-2: 002-001, 002-002",
+        ]
+        cs = self.o.cs.modified(newSettings=newSettings)
+
+        self.r.core.buildManualZones(cs)
+        daZones = self.r.core.zones
+        for zone in daZones:
+            a = self.r.core.getAssemblyWithStringLocation(sorted(zone.locs)[0])
+            aZone = daZones.findZoneItIsIn(a)
+            self.assertEqual(aZone, zone)
+
+        # get assem from first zone
+        a = self.r.core.getAssemblyWithStringLocation(sorted(daZones[daZones.names[0]].locs)[0])
+        # remove the zone
+        daZones.removeZone(daZones.names[0])
+
+        # ensure that we can no longer find the assembly in the zone
+        self.assertEqual(daZones.findZoneItIsIn(a), None)
+
+    def test_getZoneLocations(self):
+        # customize settings for this test
+        newSettings = {}
+        newSettings["zoneDefinitions"] = [
+            "ring-1: 001-001",
+            "ring-2: 002-001, 002-002",
+        ]
+        cs = self.o.cs.modified(newSettings=newSettings)
+        self.r.core.buildManualZones(cs)
+
+        # test the retrieval of zone locations
+        self.assertEqual(set(["002-001", "002-002"]), self.r.core.zones.getZoneLocations("ring-2"))
+
+    def test_getAllLocations(self):
+        # customize settings for this test
+        newSettings = {}
+        newSettings["zoneDefinitions"] = [
+            "ring-1: 001-001",
+            "ring-2: 002-001, 002-002",
+        ]
+        cs = self.o.cs.modified(newSettings=newSettings)
+        self.r.core.buildManualZones(cs)
+
+        # test the retrieval of zone locations
+        self.assertEqual(set(["001-001", "002-001", "002-002"]), self.r.core.zones.getAllLocations())
+
+    def test_summary(self):
+        # make sure we have a couple of zones to test on
+        for name0 in ["ring-1", "ring-2", "ring-3"]:
+            self.assertIn(name0, self.zonez.names)
+
+        # test the summary (in the log)
+        with mockRunLogs.BufferLog() as mock:
+            runLog.LOG.startLog("test_summary")
+            runLog.LOG.setVerbosity(logging.INFO)
+            self.assertEqual("", mock.getStdout())
+
+            self.zonez.summary()
+
+            self.assertIn("zoneDefinitions:", mock.getStdout())
+            self.assertIn("- ring-1: ", mock.getStdout())
+            self.assertIn("- ring-2: ", mock.getStdout())
+            self.assertIn("- ring-3: ", mock.getStdout())
+            self.assertIn("003-001, 003-002, 003-003", mock.getStdout())
+
+    def test_sortZones(self):
+        # create some zones in non-alphabetical order
+        zs = zones.Zones()
+        zs.addZone(self.zonez["ring-3"])
+        zs.addZone(self.zonez["ring-1"])
+        zs.addZone(self.zonez["ring-2"])
+
+        # check the initial order of the zones
+        self.assertEqual(list(zs._zones.keys())[0], "ring-3")
+        self.assertEqual(list(zs._zones.keys())[1], "ring-1")
+        self.assertEqual(list(zs._zones.keys())[2], "ring-2")
+
+        # sort the zones
+        zs.sortZones()
+
+        # check the final order of the zones
+        self.assertEqual(list(zs._zones.keys())[0], "ring-1")
+        self.assertEqual(list(zs._zones.keys())[1], "ring-2")
+        self.assertEqual(list(zs._zones.keys())[2], "ring-3")
+
+
+class TestZonesFile(unittest.TestCase):
+    def setUp(self):
+        # spin up the test reactor
+        self.o, self.r = loadTestReactor()
+
+        # build zones based on a file
+        newSettings = {}
+        newSettings["zonesFile"] = os.path.join(THIS_DIR, "zonesFile.yaml")
+        cs = self.o.cs.modified(newSettings=newSettings)
+        self.r.core.buildManualZones(cs)
+        self.zonez = self.r.core.zones
+
+    def test_zonesFile(self):
+        """
+        Test creating and interacting with a zones file.
+
+        .. test:: Create collection of Zones based on a yaml file.
+            :id: T_ARMI_ZONE5
+            :tests: R_ARMI_ZONE
+        """
+        self.assertEqual(set(["001-001"]), self.r.core.zones.getZoneLocations("a_zone"))
+        self.assertEqual(set(["002-001"]), self.r.core.zones.getZoneLocations("a_different_zone"))
