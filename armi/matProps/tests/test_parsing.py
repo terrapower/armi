c63c10665b9482a26fc64f386a462207289a2e89
Olek Yardas <olek.yardin@gmail.com>
2026-06-22 16:24:49 -0500

    add loadAll test to cover material directory exception

--- armi/matProps/tests/test_parsing.py
+++ armi/matProps/tests/test_parsing.py
@@ -15,6 +15,7 @@
 """Test YAML parsers for all files in the matProps data directory to ensure that there are no parsing errors."""
 
 import os
+import sysconfig
 import tempfile
 import unittest
 from os import path
@@ -75,6 +76,18 @@ class TestParsing(unittest.TestCase):
 
         armi.matProps.clear()
         self.assertEqual(0, len(armi.matProps.loadedMaterials()))
+
+    def test_loadAllBadRootDir(self):
+        defaultRootDir = os.path.join(sysconfig.get_paths()["purelib"], "materials_data")
+        tempRootDir = os.path.join(sysconfig.get_paths()["purelib"], "materials_data_temp")
+        if os.path.exists(defaultRootDir):
+            os.rename(defaultRootDir, tempRootDir)
+
+        with self.assertRaisesRegex(OSError, "No material directory provided"):
+            armi.matProps.loadAll()
+
+        if os.path.exists(tempRootDir):
+            os.rename(tempRootDir, defaultRootDir)
 
     def test_loadSafe(self):
         armi.matProps.clear()
