6ffa5ad556dab3519765a59c023703b2ea0e85b1
Olek Yardas <olek.yardin@gmail.com>
2026-07-22 10:52:22 -0500

    typo fix

--- armi/testing/__init__.py
+++ armi/testing/__init__.py
@@ -29,12 +29,12 @@ from armi import getPluginManagerOrFail, materials, operators, runLog, settings
 from armi.materials import uZr
 from armi.reactor import assemblies, blocks, geometry, grids, reactors
 from armi.reactor.components import Hexagon, Rectangle
-from armi.testing.testAssemblies import (
+from armi.testing.testAssemblies import (  # noqa: F401
     buildHexAssemblyFiveUZrUTh,
-    buildHexAssemblyFourUZrUT,
+    buildHexAssemblyFourUZrUTh,
     buildHexAssemblySingleUZr,
     buildHexAssemblySingleUZrUTh,
-)  # noqa: F401
+)
 
 TEST_ROOT = os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "tests"))
 TESTING_ROOT = os.path.dirname(os.path.abspath(__file__))
