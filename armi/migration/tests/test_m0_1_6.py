a5228ee6d3a3ee33343f511f60f8a2e1ccd7b127
John Stilley <jstilley@terrapower.com>
2026-08-03 10:51:42 -0700

    ruf

--- armi/migration/tests/test_m0_1_6.py
+++ armi/migration/tests/test_m0_1_6.py
@@ -42,7 +42,7 @@ class TestMigration(unittest.TestCase):
     def test_noMigrationIfVersion(self):
         """
         Ensure that no migration happens if the version of the DB is newer than the migration 'toVersion'.
-        
+
         This test checks the specific case of `ConvertAlphanumLocationSettingsToNum`, which should only
         be applied until v0.1.7.
         """
