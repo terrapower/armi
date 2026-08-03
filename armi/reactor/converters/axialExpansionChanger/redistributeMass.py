f34aee9ed3ed30825aea444f8c0f19d9ec4bea89
Andrew Johnson <anjohnson@terrapower.com>
2026-08-03 15:18:00 -0700

    Don't use setattr for ndens update

--- armi/reactor/converters/axialExpansionChanger/redistributeMass.py
+++ armi/reactor/converters/axialExpansionChanger/redistributeMass.py
@@ -283,7 +283,7 @@ class RedistributeMass:
             )
             runLog.error(msg)
             raise
-        setattr(self.toComp.p, paramName, toData)
+        self.toComp.p[paramName] = toData
         return True
 
     def _adjustPinNDens(self):
