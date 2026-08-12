80eecdc2cc0c22732c55d136fa8dcfc899be1a43
John Stilley <jstilley@terrapower.com>
2026-08-12 11:03:27 -0700

    Making Composite.changeNDensByFactor safe for Components

--- armi/reactor/composites.py
+++ armi/reactor/composites.py
@@ -1505,11 +1505,13 @@ class ArmiObject(metaclass=CompositeModelType):
         """Change the number density of all nuclides within the object by a multiplicative factor."""
         densitiesScaled = {nuc: val * factor for nuc, val in self.getNumberDensities().items()}
         self.setNumberDensities(densitiesScaled)
+
         # Update detailedNDens
-        if self.p.detailedNDens is not None:
+        if self.p.get("detailedNDens", None) is not None:
             self.p.detailedNDens *= factor
+
         # Update pinNDens
-        if self.p.pinNDens is not None:
+        if self.p.get("pinNDens", None) is not None:
             self.p.pinNDens *= factor
 
     def clearNumberDensities(self):
