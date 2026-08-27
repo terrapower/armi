0a71e884e463161525d58a61c0e6b58ccd2f31f5
Michael Jarrett <mjarrett@terrapower.com>
2026-08-27 17:52:16 +0000

    Fix a bug.

--- armi/physics/neutronics/crossSectionGroupManager.py
+++ armi/physics/neutronics/crossSectionGroupManager.py
@@ -451,13 +451,13 @@ class AverageBlockCollection(BlockCollection):
         weights = np.array([self.getWeight(b) / b.getHeight() for b in blocks])
         weights /= weights.sum()  # normalize by total weight
         components = [sorted(b.getComponents())[compIndex] for b in blocks]
-        weightedAvgComponentMass = sum(w * c.getMass() for w, c in zip(weights, components) if c.getMass() > 0.0)
+        weightedAvgComponentMass = sum(w * c.getMass() if c.getMass() > 0.0 else 0.0 for w, c in zip(weights, components))
         if weightedAvgComponentMass == 0.0:
             # if there is no component mass (e.g., gap), do a regular average
             return np.mean(np.array([c.temperatureInC for c in components]))
         else:
             return (
-                weights.dot(np.array([c.temperatureInC * c.getMass() for c in components if c.getMass() > 0.0]))
+                weights.dot(np.array([c.temperatureInC * c.getMass() if c.getMass() > 0.0 else 0.0 for c in components]))
                 / weightedAvgComponentMass
             )
 
