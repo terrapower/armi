1f1ef1c23d1068c4cf27ca70cd0014fd1022e316
Michael Jarrett <mjarrett@terrapower.com>
2026-09-25 18:08:50 +0000

    Remove print statement.

--- armi/runLog.py
+++ armi/runLog.py
@@ -560,7 +560,6 @@ class RunLogger(logging.Logger):
                 handler = handlers.WatchedFileHandler(filePath, delay=True)
             handler.setLevel(logging.WARNING)
             self.setLevel(logging.WARNING)
-        print(mpiRank, handler)
 
         form = logging.Formatter(RunLogger.FMT)
         handler.setFormatter(form)
